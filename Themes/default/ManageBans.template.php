15590545903119cf282cf153e876577e434c28c5
emanuele <emanuele45@interfree.it>
2012-02-05 20:34:03 +0800

    ! Really fixed xhtml validation in ManageBans.template.php

--- Themes/default/ManageBans.template.php
+++ Themes/default/ManageBans.template.php
@@ -184,10 +184,10 @@ function template_ban_edit()
 				<table class="table_grid" width="100%">
 					<thead>
 						<tr class="catbg">
-							<th width="65%" align="left">', $txt['ban_banned_entity'], '</td>
-							<th width="15%" align="center">', $txt['ban_hits'], '</td>
-							<th width="15%" align="center">', $txt['ban_actions'], '</td>
-							<th width="5%" align="center"><input type="checkbox" onclick="invertAll(this, this.form, \'ban_items\');" class="input_check" /></td>
+							<th width="65%" align="left">', $txt['ban_banned_entity'], '</th>
+							<th width="15%" align="center">', $txt['ban_hits'], '</th>
+							<th width="15%" align="center">', $txt['ban_actions'], '</th>
+							<th width="5%" align="center"><input type="checkbox" onclick="invertAll(this, this.form, \'ban_items\');" class="input_check" /></th>
 						</tr>
 					</thead>
 					<tbody>';
