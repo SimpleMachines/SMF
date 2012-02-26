SimpleMachines/SMF

commit b2951f6ddf869a3c646db96d884966389a525bbd

! Updates to read_zip_data moving from Magic Number based parsing to a File Structure / Local Header parsing. ! CSS files were being called as Javascript ! Load theme variants for the init template

Sources/Subs-Package.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * This file's central purpose of existence is that of making the package 
+ * This file's central purpose of existence is that of making the package
  * manager work nicely.  It contains functions for handling tar.gz and zip
  * files, as well as a simple xml parser to handle the xml package stuff.
  * Not to mention a few functions to make file handling easier.
- * 
+ *
  * Simple Machines Forum (SMF)
  *
  * @package SMF
@@ -60,10 +60,10 @@ function read_tgz_file($gzfilename, $destination, $single_file = false, $overwri
  * requires zlib support be built into PHP.
  * returns an array of the files extracted.
  * if files_to_extract is not equal to null only extracts file within this array.
- * @param string data, 
- * @param string destination, 
- * @param bool single_file = false, 
- * @param bool overwrite = false, 
+ * @param string data,
+ * @param string destination,
+ * @param bool single_file = false,
+ * @param bool overwrite = false,
  * @param array files_to_extract = null
  * @return array
  */
@@ -225,47 +225,52 @@ function read_zip_data($data, $destination, $single_file = false, $overwrite = f
 	if ($destination !== null && !file_exists($destination) && !$single_file)
 		mktree($destination, 0777);
 
-	// Look for the PK header...
-	if (strpos($data, 'PK') !== 0)
+	// Look for the end of directory signature 0x06054b50
+	$data_ecr = explode("\x50\x4b\x05\x06", $data);
+	if (!isset($data_ecr[1]))
 		return false;
 
-	// Find the central whosamawhatsit at the end; if there's a comment it's a pain.
-	if (substr($data, -22, 4) == 'PK' . chr(5) . chr(6))
-		$p = -22;
-	else
-	{
-		// Have to find where the comment begins, ugh.
-		for ($p = -22; $p > -strlen($data); $p--)
-		{
-			if (substr($data, $p, 4) == 'PK' . chr(5) . chr(6))
-				break;
-		}
-	}
 
 	$return = array();
 
-	// Get the basic zip file info.
-	$zip_info = unpack('vfiles/Vsize/Voffset', substr($data, $p + 10, 10));
+	// Get all the basic zip file info since we are here
+	$zip_info = unpack('vdisks/vrecords/vfiles/Vsize/Voffset/vcomment_length/', $data_ecr[1]);
 
-	$p = $zip_info['offset'];
-	for ($i = 0; $i < $zip_info['files']; $i++)
+	// Cut file at the central directory file header signature -- 0x02014b50, use unpack if you want any of the data, we don't
+	$file_sections = explode("\x50\x4b\x01\x02", $data);
+
+	// Cut the result on each local file header -- 0x04034b50 so we have each file in the archive as an element.
+	$file_sections = explode("\x50\x4b\x03\x04", $file_sections[0]);
+	array_shift($file_sections);
+
+	// sections and count from the signature must match or the zip file is bad
+	if (count($file_sections) != $zip_info['files'])
+		return false;
+
+	// go though each file in the archive
+	foreach ($file_sections as $data)
 	{
-		// Make sure this is a file entry...
-		if (substr($data, $p, 4) != 'PK' . chr(1) . chr(2))
-			return false;
-
 		// Get all the important file information.
-		$file_info = unpack('Vcrc/Vcompressed_size/Vsize/vfilename_len/vextra_len/vcomment_len/vdisk/vinternal/Vexternal/Voffset', substr($data, $p + 16, 30));
-		$file_info['filename'] = substr($data, $p + 46, $file_info['filename_len']);
+		$file_info = unpack("vversion/vgeneral_purpose/vcompress_method/vfile_time/vfile_date/Vcrc/Vcompressed_size/Vsize/vfilename_length/vextrafield_length", $data);
+		$file_info['filename'] = substr($data, 26, $file_info['filename_length']);
+		$file_info['dir'] = $destination . '/' . dirname($file_info['filename']);
 
-		// Skip all the information we don't care about anyway.
-		$p += 46 + $file_info['filename_len'] + $file_info['extra_len'] + $file_info['comment_len'];
+		// If bit 3 (0x08) of the general-purpose flag is set, then the CRC and file size were not available when the header was written
+		// In this case the CRC and size are instead appended in a 12-byte structure immediately after the compressed data
+		if ($file_info['general_purpose'] & 0x0008)
+		{
+			$unzipped2 = unpack("Vcrc/Vcompressed_size/Vsize", substr($$data, -12));
+			$unzipped['crc'] = $unzipped2['crc'];
+			$unzipped['compressed_size'] = $unzipped2['compressed_size'];
+			$unzipped['size'] = $unzipped2['size'];
+			unset($unzipped2);
+		}
 
 		// If this is a file, and it doesn't exist.... happy days!
-		if (substr($file_info['filename'], -1, 1) != '/' && !file_exists($destination . '/' . $file_info['filename']))
+		if (substr($file_info['filename'], -1) != '/' && !file_exists($destination . '/' . $file_info['filename']))
 			$write_this = true;
 		// If the file exists, we may not want to overwrite it.
-		elseif (substr($file_info['filename'], -1, 1) != '/')
+		elseif (substr($file_info['filename'], -1) != '/')
 			$write_this = $overwrite;
 		// This is a directory, so we're gonna want to create it. (probably...)
 		elseif ($destination !== null && !$single_file)
@@ -280,22 +285,19 @@ function read_zip_data($data, $destination, $single_file = false, $overwrite = f
 		else
 			$write_this = false;
 
-		// Check that the data is there and does exist.
-		if (substr($data, $file_info['offset'], 4) != 'PK' . chr(3) . chr(4))
-			return false;
 
 		// Get the actual compressed data.
-		$file_info['data'] = substr($data, $file_info['offset'] + 30 + $file_info['filename_len'], $file_info['compressed_size']);
+		$file_info['data'] = substr($data, 26 + $file_info['filename_length'] + $file_info['extrafield_length']);
 
-		// Only inflate it if we need to ;).
+		// Only inflate it if we need to ;)
 		if ($file_info['compressed_size'] != $file_info['size'])
-			$file_info['data'] = @gzinflate($file_info['data']);
+			$file_info['data'] = gzinflate($file_info['data']);
 
 		// Okay!  We can write this file, looks good from here...
 		if ($write_this && $destination !== null)
 		{
-			if (strpos($file_info['filename'], '/') !== false && !$single_file)
-				mktree($destination . '/' . dirname($file_info['filename']), 0777);
+			if ((strpos($file_info['filename'], '/') !== false && !$single_file) || (!is_dir($file_info['dir'])))
+				mktree($file_info['dir'], 0777);
 
 			// If we're looking for a specific file, and this is it... ka-bam, baby.
 			if ($single_file && ($destination == $file_info['filename'] || $destination == '*/' . basename($file_info['filename'])))
@@ -1366,7 +1368,7 @@ function parsePackageInfo(&$packageXML, $testing_only = true, $method = 'install
  * supports comma separated version numbers, with or without whitespace.
  * supports lower and upper bounds. (1.0-1.2)
  * returns true if the version matched.
- * @param string $version 
+ * @param string $version
  * @param string $versions
  * @return bool
  */
@@ -2898,7 +2900,7 @@ function fetch_web_data($url, $post_data = '', $keep_alive = false, $redirection
 		// I want this, from there, and I'm not going to be bothering you for more (probably.)
 		if (empty($post_data))
 		{
-			fwrite($fp, 'GET ' . $match[6] . ' HTTP/1.0' . "\r\n");
+			fwrite($fp, 'GET ' . ($match[6] !== '/' ? $match[6] : '') . ' HTTP/1.0' . "\r\n");
 			fwrite($fp, 'Host: ' . $match[3] . (empty($match[5]) ? ($match[2] ? ':443' : '') : ':' . $match[5]) . "\r\n");
 			fwrite($fp, 'User-Agent: PHP/SMF' . "\r\n");
 			if ($keep_alive)
@@ -2908,7 +2910,7 @@ function fetch_web_data($url, $post_data = '', $keep_alive = false, $redirection
 		}
 		else
 		{
-			fwrite($fp, 'POST ' . $match[6] . ' HTTP/1.0' . "\r\n");
+			fwrite($fp, 'POST ' . ($match[6] !== '/' ? $match[6] : '') . ' HTTP/1.0' . "\r\n");
 			fwrite($fp, 'Host: ' . $match[3] . (empty($match[5]) ? ($match[2] ? ':443' : '') : ':' . $match[5]) . "\r\n");
 			fwrite($fp, 'User-Agent: PHP/SMF' . "\r\n");
 			if ($keep_alive)

commit 41d94294ad9d4f43fc0b6bcc08b54a724e6cb595

+ Add <requires> option to package manager with optional version tag <requires id="" [version=""] /> + Add <license> option to package manager with optional lang tag <license type="file" [parsebbc="true"] [lang=""]>license.txt</license> ! Update <hook> tag  <hook function="function_name" hook="hook_name" /> ! Fix error introduced with Commit:b2951f6ddf869a3c646db96d884966389a525bbd (creating directories when none was requested)

Sources/Packages.php
@@ -313,21 +313,22 @@ function PackageInstallTest()
 			$chmod_files[] = $action['filename'];
 			continue;
 		}
-		elseif ($action['type'] == 'readme')
+		elseif ($action['type'] == 'readme' || $action['type'] == 'license')
 		{
+			$type = 'package_' . $action['type'];
 			if (file_exists($boarddir . '/Packages/temp/' . $context['base_path'] . $action['filename']))
-				$context['package_readme'] = htmlspecialchars(trim(file_get_contents($boarddir . '/Packages/temp/' . $context['base_path'] . $action['filename']), "\n\r"));
+				$context[$type] = htmlspecialchars(trim(file_get_contents($boarddir . '/Packages/temp/' . $context['base_path'] . $action['filename']), "\n\r"));
 			elseif (file_exists($action['filename']))
-				$context['package_readme'] = htmlspecialchars(trim(file_get_contents($action['filename']), "\n\r"));
+				$context[$type] = htmlspecialchars(trim(file_get_contents($action['filename']), "\n\r"));
 
 			if (!empty($action['parse_bbc']))
 			{
 				require_once($sourcedir . '/Subs-Post.php');
-				preparsecode($context['package_readme']);
-				$context['package_readme'] = parse_bbc($context['package_readme']);
+				preparsecode($context[$type]);
+				$context[$type] = parse_bbc($context[$type]);
 			}
 			else
-				$context['package_readme'] = nl2br($context['package_readme']);
+				$context[$type] = nl2br($context[$type]);
 
 			continue;
 		}
@@ -505,7 +506,7 @@ function PackageInstallTest()
 				'type' => $txt['package_create'] . ' ' . ($action['type'] == 'create-dir' ? $txt['package_tree'] : $txt['package_file']),
 				'action' => $smcFunc['htmlspecialchars'](strtr($action['destination'], array($boarddir => '.')))
 			);
-		elseif (in_array($action['type'], array('hook')))
+		elseif ($action['type'] == 'hook')
 		{
 			$action['description'] = !isset($action['hook'], $action['function']) ? $txt['package_action_failure'] : $txt['package_action_success'];
 
@@ -515,6 +516,47 @@ function PackageInstallTest()
 			$thisAction = array(
 				'type' => $action['reverse'] ? $txt['execute_hook_remove'] : $txt['execute_hook_add'],
 				'action' => sprintf($txt['execute_hook_action'],  $smcFunc['htmlspecialchars']($action['hook'])),
+			);
+		}
+		elseif ($action['type'] == 'requires')
+		{
+			$installed = false;
+			$version = true;
+
+			// package missing required values?
+			if (!isset($action['id']))
+				$context['has_failure'] = true;
+			else
+			{
+				// See if this dependancy is installed
+				$request = $smcFunc['db_query']('', '
+					SELECT version
+					FROM {db_prefix}log_packages
+					WHERE package_id = {string:current_package}
+						AND install_state != {int:not_installed}
+					ORDER BY time_installed DESC
+					LIMIT 1',
+					array(
+						'not_installed'	=> 0,
+						'current_package' => $action['id'],
+					)
+				);
+				$installed = ($smcFunc['db_num_rows']($request) !== 0);
+				if ($installed)
+					list($version) = $smcFunc['db_fetch_row']($request);
+				$smcFunc['db_free_result']($request);
+
+				// do a version level check (if requested) in the most basic way
+				$version = (isset($action['version']) ? $version == $action['version'] : true);
+			}
+
+			// Set success or failure information
+			$action['description'] = ($installed && $version) ? $txt['package_action_success'] : $txt['package_action_failure'];
+			$context['has_failure'] = !($installed && $version);
+
+			$thisAction = array(
+				'type' => $txt['package_requires'],
+				'action' => $txt['package_check_for'] . ' ' . $action['id'] . (isset($action['version']) ? (' / ' . ($version ? $action['version'] : '<span class="error">' . $action['version'] . '</span>')) : ''),
 			);
 		}
 		elseif (in_array($action['type'], array('require-dir', 'require-file')))
@@ -921,7 +963,7 @@ function PackageInstall()
 				// Now include the file and be done with it ;).
 				require($boarddir . '/Packages/temp/' . $context['base_path'] . $action['filename']);
 			}
-			elseif ($action['type'] == 'hook' && isset($action['hook'], $action['name']))
+			elseif ($action['type'] == 'hook' && isset($action['hook'], $action['function']))
 			{
 				if ($action['reverse'])
 					remove_integration_function($action['hook'], $action['function']);

Sources/Subs-Package.php
@@ -50,16 +50,23 @@ function read_tgz_file($gzfilename, $destination, $single_file = false, $overwri
 
 /**
  * Extracts a file or files from the .tar.gz contained in data.
+ *
  * detects if the file is really a .zip file, and if so returns the result of read_zip_data
- * if destination is null, returns a list of files in the archive.
- * if single_file is true, returns the contents of the file specified by destination, if it exists, or false.
- * if single_file is true, destination can start with * and / to signify that the file may come from any directory.
- * destination should not begin with a / if single_file is true.
+ *
+ * if destination is null
+ *	- returns a list of files in the archive.
+ *
+ * if single_file is true
+ * - returns the contents of the file specified by destination, if it exists, or false.
+ * - destination can start with * and / to signify that the file may come from any directory.
+ * - destination should not begin with a / if single_file is true.
+ *
  * overwrites existing files with newer modification times if and only if overwrite is true.
  * creates the destination directory if it doesn't exist, and is is specified.
  * requires zlib support be built into PHP.
  * returns an array of the files extracted.
  * if files_to_extract is not equal to null only extracts file within this array.
+ *
  * @param string data,
  * @param string destination,
  * @param bool single_file = false,
@@ -296,7 +303,7 @@ function read_zip_data($data, $destination, $single_file = false, $overwrite = f
 		// Okay!  We can write this file, looks good from here...
 		if ($write_this && $destination !== null)
 		{
-			if ((strpos($file_info['filename'], '/') !== false && !$single_file) || (!is_dir($file_info['dir'])))
+			if ((strpos($file_info['filename'], '/') !== false && !$single_file) || (!$single_file && !is_dir($file_info['dir'])))
 				mktree($file_info['dir'], 0777);
 
 			// If we're looking for a specific file, and this is it... ka-bam, baby.
@@ -970,12 +977,14 @@ function packageRequireFTP($destination_url, $files = null, $return = false)
 }
 
 /**
- * Parses the actions in package-info.xml files from packages.
- * package should be an xmlArray with package-info as its base.
- * testing_only should be true if the package should not actually be applied.
- * method is upgrade, install, or uninstall.  Its default is install.
- * previous_version should be set to the previous installed version of this package, if any.
- * does not handle failure terribly well; testing first is always better.
+ * Parses the actions in package-info.xml file from packages.
+ *
+ * - Package should be an xmlArray with package-info as its base.
+ * - Testing_only should be true if the package should not actually be applied.
+ * - Method can be upgrade, install, or uninstall.  Its default is install.
+ * - Previous_version should be set to the previous installed version of this package, if any.
+ * - Does not handle failure terribly well; testing first is always better.
+ *
  * @param xmlArray &$package
  * @param bool $testing_only = true
  * @param string $method = 'install' ('install', 'upgrade', or 'uninstall')
@@ -1035,53 +1044,55 @@ function parsePackageInfo(&$packageXML, $testing_only = true, $method = 'install
 	$temp_path = $boarddir . '/Packages/temp/' . (isset($context['base_path']) ? $context['base_path'] : '');
 
 	$context['readmes'] = array();
+	$context['licences'] = array();
+
 	// This is the testing phase... nothing shall be done yet.
 	foreach ($actions as $action)
 	{
 		$actionType = $action->name();
 
-		if (in_array($actionType, array('readme', 'code', 'database', 'modification', 'redirect')))
+		if (in_array($actionType, array('readme', 'code', 'database', 'modification', 'redirect', 'license')))
 		{
-			// Allow for translated readme files.
-			if ($actionType == 'readme')
+			// Allow for translated readme and license files.
+			if ($actionType == 'readme' || $actionType == 'license')
 			{
+				$type = $actionType . 's';
 				if ($action->exists('@lang'))
 				{
-					// Auto-select a readme language based on either request variable or current language.
-					if ((isset($_REQUEST['readme']) && $action->fetch('@lang') == $_REQUEST['readme']) || (!isset($_REQUEST['readme']) && $action->fetch('@lang') == $language))
+					// Auto-select the language based on either request variable or current language.
+					if ((isset($_REQUEST['readme']) && $action->fetch('@lang') == $_REQUEST['readme']) || (isset($_REQUEST['license']) && $action->fetch('@lang') == $_REQUEST['license']) || (!isset($_REQUEST['readme']) && $action->fetch('@lang') == $language)	|| (!isset($_REQUEST['license']) && $action->fetch('@lang') == $language))
 					{
-						// In case the user put the readme blocks in the wrong order.
-						if (isset($context['readmes']['selected']) && $context['readmes']['selected'] == 'default')
-							$context['readmes'][] = 'default';
+						// In case the user put the blocks in the wrong order.
+						if (isset($context[$type]['selected']) && $context[$type]['selected'] == 'default')
+							$context[$type][] = 'default';
 
-						$context['readmes']['selected'] = htmlspecialchars($action->fetch('@lang'));
+						$context[$type]['selected'] = htmlspecialchars($action->fetch('@lang'));
 					}
 					else
 					{
-						// We don't want this readme now, but we'll allow the user to select to read it.
-						$context['readmes'][] = htmlspecialchars($action->fetch('@lang'));
+						// We don't want this now, but we'll allow the user to select to read it.
+						$context[$type][] = htmlspecialchars($action->fetch('@lang'));
 						continue;
 					}
 				}
-				// Fallback readme. Without lang parameter.
+				// Fallback when we have no lang parameter.
 				else
 				{
-
-					// Already selected a readme.
-					if (isset($context['readmes']['selected']))
+					// Already selected one for use?
+					if (isset($context[$type]['selected']))
 					{
-						$context['readmes'][] = 'default';
+						$context[$type][] = 'default';
 						continue;
 					}
 					else
-						$context['readmes']['selected'] = 'default';
+						$context[$type]['selected'] = 'default';
 				}
 			}
 
 			// @todo Make sure the file actually exists?  Might not work when testing?
 			if ($action->exists('@type') && $action->fetch('@type') == 'inline')
 			{
-				$filename = $temp_path . '$auto_' . $temp_auto++ . ($actionType == 'readme' || $actionType == 'redirect' ? '.txt' : ($actionType == 'code' || $actionType == 'database' ? '.php' : '.mod'));
+				$filename = $temp_path . '$auto_' . $temp_auto++ . (in_array($actionType, array('readme', 'redirect', 'license')) ? '.txt' : ($actionType == 'code' || $actionType == 'database' ? '.php' : '.mod'));
 				package_put_contents($filename, $action->fetch('.'));
 				$filename = strtr($filename, array($temp_path => ''));
 			}
@@ -1097,7 +1108,7 @@ function parsePackageInfo(&$packageXML, $testing_only = true, $method = 'install
 				'redirect_url' => $action->exists('@url') ? $action->fetch('@url') : '',
 				'redirect_timeout' => $action->exists('@timeout') ? (int) $action->fetch('@timeout') : '',
 				'parse_bbc' => $action->exists('@parsebbc') && $action->fetch('@parsebbc') == 'true',
-				'language' => ($actionType == 'readme' && $action->exists('@lang') && $action->fetch('@lang') == $language) ? $language : '',
+				'language' => (($actionType == 'readme' || $actionType == 'license')  && $action->exists('@lang') && $action->fetch('@lang') == $language) ? $language : '',
 			);
 
 			continue;
@@ -1109,6 +1120,16 @@ function parsePackageInfo(&$packageXML, $testing_only = true, $method = 'install
 				'function' => $action->exists('@function') ? $action->fetch('@function') : '',
 				'hook' => $action->exists('@hook') ? $action->fetch('@hook') : $action->fetch('.'),
 				'reverse' => $action->exists('@reverse') && $action->fetch('@reverse') == 'true' ? true : false,
+				'description' => '',
+			);
+			continue;
+		}
+		elseif ($actionType == 'requires')
+		{
+			$return[] = array(
+				'type' => $actionType,
+				'id' => $action->exists('@id') ? $action->fetch('@id') : '',
+				'version' => $action->exists('@version') ? $action->fetch('@version') : $action->fetch('.'),
 				'description' => '',
 			);
 			continue;
@@ -1268,7 +1289,7 @@ function parsePackageInfo(&$packageXML, $testing_only = true, $method = 'install
 	$not_done = array(array('type' => '!'));
 	foreach ($return as $action)
 	{
-		if ($action['type'] == 'modification' || $action['type'] == 'code' || $action['type'] == 'database' || $action['type'] == 'redirect')
+		if (in_array($action['type'], array('modification', 'code', 'database', 'redirect', 'hook')))
 			$not_done[] = $action;
 
 		if ($action['type'] == 'create-dir')
@@ -2518,16 +2539,21 @@ function package_flush_cache($trash = false)
 		elseif (!file_exists($filename))
 			@touch($filename);
 
-		package_chmod($filename);
+		$result = package_chmod($filename);
 
-		$fp = fopen($filename, 'r+');
-		if (!$fp && !$trash)
+		// if we are not doing our test pass, then lets do a full write check
+		if (!$trash)
 		{
-			// We should have package_chmod()'d them before, no?!
-			trigger_error('package_flush_cache(): some files are still not writable', E_USER_WARNING);
-			return;
+			// acid test, can we really open this file for writing?
+			$fp = ($result) ? fopen($filename, 'r+') : $result;
+			if (!$fp)
+			{
+				// We should have package_chmod()'d them before, no?!
+				trigger_error('package_flush_cache(): some files are still not writable', E_USER_WARNING);
+				return;
+			}
+			fclose($fp);
 		}
-		fclose($fp);
 	}
 
 	if ($trash)
@@ -2548,6 +2574,7 @@ function package_flush_cache($trash = false)
 
 /**
  * Try to make a file writable.
+ *
  * @param string $filename
  * @param string $perm_state = 'writable'
  * @param bool $track_change = false
@@ -2681,6 +2708,8 @@ function package_chmod($filename, $perm_state = 'writable', $track_change = fals
 }
 
 /**
+ * Used to crypt the supplied ftp password in this session
+ *
  * @param string $pass
  * @return string The encrypted password
  */
@@ -2833,6 +2862,12 @@ function package_create_backup($id = 'backup')
 
 /**
  * Get the contents of a URL, irrespective of allow_url_fopen.
+ *
+ * - reads the contents of an http or ftp address and retruns the page in a string
+ * - will accept up to 3 page redirections (redirectio_level in the function call is private)
+ * - if post_data is supplied, the value and lenght is posted to the given url as form data
+ * - URL must be supplied in lowercase
+ *
  * @param string $url
  * @param string $post_data = ''
  * @param bool $keep_alive = false

Themes/default/Packages.template.php
@@ -46,6 +46,7 @@ function template_view_package()
 		</div>';
 	}
 
+	// Display the package readme if one exists
 	if (isset($context['package_readme']))
 	{
 		echo '
@@ -57,7 +58,7 @@ function template_view_package()
 				<div class="content">
 					', $context['package_readme'], '
 					<span class="floatright">', $txt['package_available_readme_language'], '
-						<select name="readme_language" id="readme_language" onchange="if (this.options[this.selectedIndex].value) window.location.href = smf_prepareScriptUrl(smf_scripturl + \'', '?action=admin;area=packages;sa=', $context['uninstalling'] ? 'uninstall' : 'install', ';package=', $context['filename'], ';readme=\' + this.options[this.selectedIndex].value);">';
+						<select name="readme_language" id="readme_language" onchange="if (this.options[this.selectedIndex].value) window.location.href = smf_prepareScriptUrl(smf_scripturl + \'', '?action=admin;area=packages;sa=', $context['uninstalling'] ? 'uninstall' : 'install', ';package=', $context['filename'], ';readme=\' + this.options[this.selectedIndex].value + \';license=\' + get_selected(\'license_language\'));">';
 							foreach ($context['readmes'] as $a => $b)
 								echo '<option value="', $b, '"', $a === 'selected' ? ' selected="selected"' : '', '>', $b == 'default' ? $txt['package_readme_default'] : ucfirst($b), '</option>';
 			echo '
@@ -68,7 +69,31 @@ function template_view_package()
 			</div>
 			<br />';
 	}
-
+	
+	// Did they specify a license to display?
+	if (isset($context['package_license']))
+	{
+		echo '
+			<div class="cat_bar">
+				<h3 class="catbg">', $txt['package_install_license'], '</h3>
+			</div>
+			<div class="windowbg2">
+				<span class="topslice"><span></span></span>
+				<div class="content">
+					', $context['package_license'], '
+					<span class="floatright">', $txt['package_available_license_language'], '
+						<select name="license_language" id="license_language" onchange="if (this.options[this.selectedIndex].value) window.location.href = smf_prepareScriptUrl(smf_scripturl + \'', '?action=admin;area=packages;sa=install', ';package=', $context['filename'], ';license=\' + this.options[this.selectedIndex].value + \';readme=\' + get_selected(\'readme_language\'));">';
+							foreach ($context['licenses'] as $a => $b)
+								echo '<option value="', $b, '"', $a === 'selected' ? ' selected="selected"' : '', '>', $b == 'default' ? $txt['package_license_default'] : ucfirst($b), '</option>';
+			echo '
+						</select>
+					</span>
+				</div>
+				<span class="botslice"><span></span></span>
+			</div>
+			<br />';
+	}
+	
 	echo '
 		<form action="', $scripturl, '?action=admin;area=packages;sa=', $context['uninstalling'] ? 'uninstall' : 'install', $context['ftp_needed'] ? '' : '2', ';package=', $context['filename'], ';pid=', $context['install_id'], '" onsubmit="submitonce(this);" method="post" accept-charset="', $context['character_set'], '">
 			<div class="cat_bar">
@@ -342,6 +367,21 @@ function template_view_package()
 		}
 
 	echo '
+	// ]]></script>';
+	
+	// Get the currently selected item from a select list
+	echo '
+	<script type="text/javascript"><!-- // --><![CDATA[
+	function get_selected(id)
+	{
+		var aSelected = document.getElementById(id);
+		for (var i = 0; i < aSelected.options.length; i++)
+		{
+			if (aSelected.options[i].selected == true)
+				return aSelected.options[i].value;
+		}
+		return aSelected.options[0];
+	}
 	// ]]></script>';
 
 	// And a bit more for database changes.

Themes/default/languages/Packages.english.php
@@ -79,6 +79,7 @@ $txt['packages_latest_fetch'] = 'Attempting to fetch the most popular and recent
 $txt['package_upgrade'] = 'Upgrade';
 $txt['package_uninstall_readme'] = 'Uninstallation Readme';
 $txt['package_install_readme'] = 'Installation Readme';
+$txt['package_install_license'] = 'License';
 $txt['package_install_type'] = 'Type';
 $txt['package_install_action'] = 'Action';
 $txt['package_install_desc'] = 'Description';
@@ -97,6 +98,8 @@ $txt['execute_database_changes'] = 'Adapt Database';
 $txt['execute_hook_add'] = 'Add Hook';
 $txt['execute_hook_remove'] = 'Remove Hook';
 $txt['execute_hook_action'] = 'Adapting hook %1$s';
+$txt['package_requires'] = 'Requires Modification';
+$txt['package_check_for'] = 'Check for installation:';
 
 $txt['package_install_actions'] = 'Installations actions for';
 $txt['package_will_fail_title'] = 'Error in Package Installation';
@@ -263,5 +266,8 @@ $txt['package_confirm_go_back'] = 'Go back';
 
 $txt['package_readme_default'] = 'Default';
 $txt['package_available_readme_language'] = 'Available Readme Languages:';
+$txt['package_license_default'] = 'Default';
+$txt['package_available_license_language'] = 'Available License Languages:';
+
 
 ?>