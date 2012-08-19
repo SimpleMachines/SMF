02667d6d57651c1abf5dcd126f595710b3a189fe
emanuele <emanuele45@gmail.com>
2012-08-19 14:06:38 +0200

    Copyrights changed to 2012
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- Sources/Admin.php
+++ Sources/Admin.php
@@ -8,7 +8,7 @@
  * @package SMF
  * @author Simple Machines
  *
- * @copyright 2011 Simple Machines
+ * @copyright 2012 Simple Machines
  * @license http://www.simplemachines.org/about/smf/license.php BSD
  *
  * @version 2.1 Alpha 1
