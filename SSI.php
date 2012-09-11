75bffbff672d9d5729f6ae4b3bd6bb8cfcb2c0af
emanuele <emanuele45@gmail.com>
2012-09-12 00:48:13 +0200

    Useless space in SSI.php - thanks AngelinaBelle for the fix
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- SSI.php
+++ SSI.php
@@ -749,7 +749,7 @@ function ssi_latestMember($output_method = 'echo')
 
 	if ($output_method == 'echo')
 		echo '
-	', sprintf($txt['welcome_newest_member'], ' ' . $context['common_stats']['latest_member']['link']), '<br />';
+	', sprintf($txt['welcome_newest_member'], $context['common_stats']['latest_member']['link']), '<br />';
 	else
 		return $context['common_stats']['latest_member'];
 }
