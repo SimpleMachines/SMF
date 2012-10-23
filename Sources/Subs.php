f88b8bc1c56a85fdef5acfe145d3dc5627f9b3c2
Spuds <spuds@simplemachines.org>
2012-10-22 19:17:50 -0500

    ! revert the ===0 back to =='0' Thanks Josh
    Signed-off-by: Spuds <spuds@simplemachines.org>

--- Sources/Subs.php
+++ Sources/Subs.php
@@ -2035,7 +2035,7 @@ function parse_bbc($message, $smileys = true, $cache_id = '', $parse_tags = arra
 		// Item codes are complicated buggers... they are implicit [li]s and can make [list]s!
 		if ($smileys !== false && $tag === null && isset($itemcodes[$message[$pos + 1]]) && $message[$pos + 2] == ']' && !isset($disabled['list']) && !isset($disabled['li']))
 		{
-			if ($message[$pos + 1] === 0 && !in_array($message[$pos - 1], array(';', ' ', "\t", '>')))
+			if ($message[$pos + 1] == '0' && !in_array($message[$pos - 1], array(';', ' ', "\t", '>')))
 				continue;
 			
 			$tag = $itemcodes[$message[$pos + 1]];
