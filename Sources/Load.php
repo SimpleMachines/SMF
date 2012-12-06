90217526986788fbcea43562d483a56e161b43a2
Suki <missallsunday@simplemachines.org>
2012-12-06 14:25:03 -0600

    Pass the $set var to the hook too
    Signed-off-by: Suki <missallsunday@simplemachines.org>

--- Sources/Load.php
+++ Sources/Load.php
@@ -953,7 +953,7 @@ function loadMemberData($users, $is_name = false, $set = 'normal')
 		trigger_error('loadMemberData(): Invalid member data set \'' . $set . '\'', E_USER_WARNING);
 
 	// Allow mods to easily add to the selected member data
-	call_integration_hook('integrate_load_member_data', array($select_columns, $select_tables));
+	call_integration_hook('integrate_load_member_data', array($select_columns, $select_tables, $set));
 
 	if (!empty($users))
 	{
