56e2a5a463e8248a3887c3bedca458d92ea2902a
Suki <suki@missallsunday.com>
2012-12-12 15:45:12 -0600

    Avatar.php
    Signed-off-by: Suki <suki@missallsunday.com>

---
+++ Sources/Avatar.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file handles the avatar requests.
+ *
+ * Simple Machines Forum (SMF)
+ *
+ * @package SMF
+ * @author Simple Machines
+ *
+ * @copyright 2012 Simple Machines
+ * @license http://www.simplemachines.org/about/smf/license.php BSD
+ *
+ * @version 2.1 Alpha 1
+ */
+
+if (!defined('SMF'))
+	die('Hacking attempt...');
