4d023d357d846168880b48cd1764b3f429d96e8b
Matthew Kerle <creativewebdsign@gmail.com>
2012-12-13 01:24:14 -0600

    Figuring it all out...
    Working on figuring out GitHub so I can go through the workflow more
    easily with PR's and such.
    
    Moving this back to separate my two PR's.

--- Sources/PersonalMessage.php
+++ Sources/PersonalMessage.php
@@ -837,18 +837,6 @@ function MessageFolder()
 				'id_member' => $context['folder'] == 'sent' ? 'pmr.id_member' : 'pm.id_member_from',
 			)
 		);
-
-		// Build the conversation button array.
-		if ($context['display_mode'] == 2)
-		{
-			$context['conversation_buttons'] = array(
-				'reply' => array('text' => 'reply_to_all', 'image' => 'reply.png', 'lang' => true, 'url' => $scripturl . '?action=pm;sa=send;f=' . $context['folder'] . ($context['current_label_id'] != -1 ? ';l=' . $context['current_label_id'] : '') . ';pmsg=' . $context['current_pm'] . ';u=all', 'active' => true),
-				'delete' => array('text' => 'delete_conversation', 'image' => 'delete.png', 'lang' => true, 'url' => $scripturl . '?action=pm;sa=pmactions;pm_actions[' . $context['current_pm'] . ']=delete;conversation;f=' . $context['folder'] . ';start=' . $context['start'] . ($context['current_label_id'] != -1 ? ';l=' . $context['current_label_id'] : '') . ';' . $context['session_var'] . '=' . $context['session_id'], 'custom' => 'onclick="return confirm(\'' . addslashes($txt['remove_message']) . '?\');"'),
-			);
-	
-			// Allow mods to add additional buttons here
-			call_integration_hook('integrate_conversation_buttons');
-		}
 	}
 	else
 		$messages_request = false;
@@ -868,6 +856,18 @@ function MessageFolder()
 		// Otherwise do just the current one!
 		elseif (!empty($context['current_pm']))
 			markMessages($display_pms, $context['current_label_id']);
+	}
+
+	// Build the conversation button array.
+	if ($context['display_mode'] == 2)
+	{
+		$context['conversation_buttons'] = array(
+			'reply' => array('text' => 'reply_to_all', 'image' => 'reply.png', 'lang' => true, 'url' => $scripturl . '?action=pm;sa=send;f=' . $context['folder'] . ($context['current_label_id'] != -1 ? ';l=' . $context['current_label_id'] : '') . ';pmsg=' . $context['current_pm'] . ';u=all', 'active' => true),
+			'delete' => array('text' => 'delete_conversation', 'image' => 'delete.png', 'lang' => true, 'url' => $scripturl . '?action=pm;sa=pmactions;pm_actions[' . $context['current_pm'] . ']=delete;conversation;f=' . $context['folder'] . ';start=' . $context['start'] . ($context['current_label_id'] != -1 ? ';l=' . $context['current_label_id'] : '') . ';' . $context['session_var'] . '=' . $context['session_id'], 'custom' => 'onclick="return confirm(\'' . addslashes($txt['remove_message']) . '?\');"'),
+		);
+
+		// Allow mods to add additional buttons here
+		call_integration_hook('integrate_conversation_buttons');
 	}
 }
 
