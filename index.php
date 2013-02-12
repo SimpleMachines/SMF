2a7227227d46325716ba36b0c061f97f7d1237e3
AngelinaBelle <AngelinaBelle1@hotmail.com>
2013-02-11 19:05:50 -0500

    copyright line and license link
    Should also add a link to a contributors.txt?  If this is good, then should do the same to all the files. Acknowledge https://github.com/SimpleMachines/SMF2.1/blob/master/DCO.txt

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
  *
  * @package SMF
  * @author Simple Machines http://www.simplemachines.org
- * @copyright 2012 Simple Machines
- * @license http://www.simplemachines.org/about/smf/license.php BSD
+ * @copyright 2013 Simple Machines and Simple Machines contributors
+ * @license see license.txt in this distribution and http://www.simplemachines.org/about/smf/license.php#BSD
  *
  * @version 2.1 Alpha 1
  */
