e94b475c3509a3209fe285a36586da20648ac441
Antes <antes@simplemachines.org>
2013-05-26 22:49:39 +0300

    moderation actions when not full moderator Signed-off-by: Antes <antes@simplemachines.org>

--- Themes/default/Display.template.php
+++ Themes/default/Display.template.php
@@ -577,14 +577,15 @@ function template_main()
 		// Can the user modify the contents of this post?  Show the modify inline image.
 		if ($message['can_modify'])
 			echo '
-							<li class="quick_edit"><img src="', $settings['images_url'], '/icons/modify_inline.png" alt="', $txt['modify_msg'], '" title="', $txt['modify_msg'], '" class="modifybutton" id="modify_button_', $message['id'], '" style="cursor: pointer; margin: 0;" onclick="oQuickModify.modifyMsg(\'', $message['id'], '\')" />', $txt['quick_edit'], '</li>';
-
+							<li class="quick_edit"><img src="', $settings['images_url'], '/icons/modify_inline.png" alt="', $txt['modify_msg'], '" title="', $txt['modify_msg'], '" class="modifybutton" id="modify_button_', $message['id'], '" style="cursor: pointer; margin: 0;" onclick="oQuickModify.modifyMsg(\'', $message['id'], '\')" />', $txt['quick_edit'], '</li>';				
+		
 		// Can the user modify the contents of this post?
 		if ($message['can_modify'])
 			echo '
 							<li class="post_options"><a href="', $scripturl, '?action=post;msg=', $message['id'], ';topic=', $context['current_topic'], '.', $context['start'], '">', $txt['post_options'], '</a>';
-
-			echo '
+                    else
+                        echo '                          <li class="post_options"><a href="#">', $txt['post_options'], '</a>';
+			echo '			
 								<ul>';
 
 				// Can the user modify the contents of this post?
