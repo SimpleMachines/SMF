46c8f19517b7b1b304395797451bd8559f8df56b
Illori <illori@simplemacines.org>
2013-06-04 16:39:12 -0400

    Copyright update per BoD
    Signed-off-by: illori illori@simplemachines.org

--- Sources/Who.php
+++ Sources/Who.php
@@ -8,7 +8,7 @@
  *
  * @package SMF
  * @author Simple Machines http://www.simplemachines.org
- * @copyright 2012 Simple Machines
+ * @copyright 2013 Simple Machines and individual contributors
  * @license http://www.simplemachines.org/about/smf/license.php BSD
  *
  * @version 2.1 Alpha 1
@@ -527,12 +527,15 @@ function Credits($in_admin = false)
 	// Don't blink. Don't even blink. Blink and you're dead.
 	loadLanguage('Who');
 
-	$context[$context['admin_menu_name']]['tab_data'] = array(
-		'title' => $txt['support_credits_title'],
-		'help' => '',
-		'description' => '',
-	);
-
+	if($in_admin)
+	{
+		$context[$context['admin_menu_name']]['tab_data'] = array(
+			'title' => $txt['support_credits_title'],
+			'help' => '',
+			'description' => '',
+		);
+	}
+	
 	$context['credits'] = array(
 		array(
 			'pretext' => $txt['credits_intro'],
