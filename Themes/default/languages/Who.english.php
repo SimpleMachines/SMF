6147417c837eeb62eb4cdf657b9a1b5bdcbbc09d
Beard One <deakinbryan@gmail.com>
2013-09-12 18:23:06 +0100

    New look credits
    Signed-off-by: Beard One <deakinbryan@gmail.com>

--- Themes/default/languages/Who.english.php
+++ Themes/default/languages/Who.english.php
@@ -128,7 +128,7 @@ $txt['who_poll'] = 'Posting a new poll in <a href="' . $scripturl . '?board=%1$d
 // Credits text
 $txt['credits'] = 'Credits';
 $txt['credits_intro'] = 'Simple Machines wants to thank everyone who helped make SMF 2.0 what it is today; shaping and directing our project, all through the thick and the thin. It wouldn\'t have been possible without you. This includes our users and especially Charter Members - thanks for installing and using our software as well as providing valuable feedback, bug reports, and opinions.';
-$txt['credits_team'] = 'The Team';
+$txt['credits_team'] = 'The Developers and Github Contributors';
 $txt['credits_special'] = 'Special Thanks';
 $txt['credits_and'] = 'and';
 $txt['credits_anyone'] = 'And for anyone we may have missed, thank you!';
@@ -139,15 +139,6 @@ $txt['credits_software_graphics'] = 'Software/Graphics';
 $txt['credits_software'] = 'Software';
 $txt['credits_graphics'] = 'Graphics';
 $txt['credits_fonts'] = 'Fonts';
-$txt['credits_groups_pm'] = 'Project Manager';
-$txt['credits_groups_dev'] = 'Developers';
-$txt['credits_groups_support'] = 'Support Specialists';
-$txt['credits_groups_customize'] = 'Customizers';
-$txt['credits_groups_docs'] = 'Documentation Writers';
-$txt['credits_groups_marketing'] = 'Marketing';
-$txt['credits_groups_internationalizers'] = 'Localizers';
-$txt['credits_groups_servers'] = 'Servers Administrators';
-$txt['credits_groups_site'] = 'Site Administrators';
 $txt['credits_license'] = 'License';
 $txt['credits_copyright'] = 'Copyright';
 $txt['credits_version'] = 'Version';
@@ -155,11 +146,14 @@ $txt['credits_version'] = 'Version';
 $txt['credits_groups_translation'] = 'English Translation';
 $txt['credits_groups_translators'] = 'Language Translators';
 $txt['credits_translators_message'] = 'Thank you for your efforts which make it possible for people all around the world to use SMF.';
-$txt['credits_groups_consultants'] = 'Consulting Developers';
 $txt['credits_groups_beta'] = 'Beta Testers';
 $txt['credits_beta_message'] = 'The invaluable few who tirelessly find bugs, provide feedback, and drive the developers crazier.';
 $txt['credits_groups_founder'] = 'Founding Father of SMF';
 $txt['credits_groups_orignal_pm'] = 'Original Project Managers';
+$txt['credits_groups_team_friends'] = 'SMF Friends and Team';
+$txt['credits_team_friends'] = 'A big thank you the the current and former team members of SMF, for making it what it is today.';
+$txt['credits_simplemachines'] = 'Simple Machines';
+$txt['credits_simplemachines_group'] = 'Thanks to the Simple Machine groups, Members and the Board of Directors.';
 
 // List of people who have made more than a token contribution to this translation. (blank for English)
 $txt['translation_credits'] = array();
