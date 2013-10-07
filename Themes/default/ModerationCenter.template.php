88e6f4a8341e15139133a308e4e1fdef86e42441
Suki <suki@missallsunday.com>
2013-10-06 19:46:51 -0500

    Only show the buttons on active reports
    Signed-off-by: Suki <suki@missallsunday.com>

--- Themes/default/ModerationCenter.template.php
+++ Themes/default/ModerationCenter.template.php
@@ -237,12 +237,12 @@ function template_reported_posts()
 					<li><a href="', $scripturl, '?action=moderate;area=reports', $context['view_closed'] ? ';sa=closed' : '', ';close=', (int) !$report['closed'], ';rid=', $report['id'], ';start=', $context['start'], ';', $context['session_var'], '=', $context['session_id'], '">', $close_button, '</a></li>';
 
 		// Delete message button.
-		if (allowedTo('remove_any', $report['topic']['id_board']))
+		if (!$report['closed'] && allowedTo('remove_any', $report['topic']['id_board']))
 			echo '
 					<li><a href="', $scripturl, '?action=deletemsg;topic=', $report['topic']['id'] ,'.0;msg=', $report['topic']['id_msg'] ,';', $context['session_var'], '=', $context['session_id'], '" onclick="return confirm(\'' , $txt['mc_reportedp_delete_confirm'] , '\');">', $delete_button, '</a></li>';
 
 		// Ban this user button.
-		if (allowedTo('manage_bans', $report['topic']['id_board']))
+		if (!$report['closed'] && allowedTo('manage_bans', $report['topic']['id_board']))
 			echo '
 					<li><a href="', $scripturl, '?action=admin;area=ban;sa=add;u=', $report['author']['id'] ,';', $context['session_var'], '=', $context['session_id'], '">', $ban_button, '</a></li>';
 
