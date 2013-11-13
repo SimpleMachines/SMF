d6ba137d2f1d4cfc4e077e1cb8e77efba2896246
Suki <suki@missallsunday.com>
2013-11-13 14:15:17 -0600

    get_single_them()
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/Subs-Themes.php
+++ Sources/Subs-Themes.php
@@ -17,4 +17,41 @@
 if (!defined('SMF'))
 	die('No direct access...');
 
+
+function get_single_theme($id)
+{
+	global $smcFunc, $context;
+
+	// No data, no fun!
+	if (empty($id))
+		return false;
+
+	$single = array();
+
+	$request = $smcFunc['db_query']('', '
+		SELECT id_theme, variable, value
+		FROM {db_prefix}themes
+		WHERE variable IN ({string:theme_dir}, {string:theme_url}, {string:images_url}, {string:name}, {string:theme_layers}, {string:theme_templates}, {string:version}, {string:install_for})
+			AND id_theme = {int:id_theme}
+			AND id_member = {int:no_member}',
+		array(
+			'id_theme' => $id,
+			'no_member' => 0,
+			'theme_dir' => 'theme_dir',
+			'images_url' => 'images_url',
+			'theme_url' => 'theme_url',
+			'name' => 'name',
+			'theme_layers' => 'theme_layers',
+			'theme_templates' => 'theme_templates',
+			'version' => 'version',
+			'install_for' => 'install_for',
+		)
+	);
+
+	while ($row = $smcFunc['db_fetch_assoc']($request))
+		$single[$row['variable']] = $row['value'];
+
+	return $single;
+}
+
 ?>
