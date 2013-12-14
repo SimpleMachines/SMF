1ced6ff893d96e28f6fe4af054c6619251010017
Peter Spicer <sleeping@myperch.org>
2013-12-14 18:03:48 +0000

    ! I just realised we're not using this any more, so let's not install it (and then we won't be loading it every day)
    Signed-off-by: Peter Spicer <sleeping@myperch.org>

--- other/install_2-1_mysql.sql
+++ other/install_2-1_mysql.sql
@@ -26,8 +26,7 @@ VALUES
 	(1, 'current-version.js', '/smf/', 'version=%3$s', '', 'text/javascript'),
 	(2, 'detailed-version.js', '/smf/', 'language=%1$s&version=%3$s', '', 'text/javascript'),
 	(3, 'latest-news.js', '/smf/', 'language=%1$s&format=%2$s', '', 'text/javascript'),
-	(4, 'latest-smileys.js', '/smf/', 'language=%1$s&version=%3$s', '', 'text/javascript'),
-	(5, 'latest-themes.js', '/smf/', 'language=%1$s&version=%3$s', '', 'text/javascript');
+	(4, 'latest-smileys.js', '/smf/', 'language=%1$s&version=%3$s', '', 'text/javascript');
 # --------------------------------------------------------
 
 #
