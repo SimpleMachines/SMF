b354667715eb7952b60f8d0378d589b81eed8b89
Peter Spicer <sleeping@myperch.org>
2013-12-14 18:42:39 +0000

    ! We need to indicate a default preference for liking posts.
    Signed-off-by: Peter Spicer <sleeping@myperch.org>

--- other/install_2-1_sqlite3.sql
+++ other/install_2-1_sqlite3.sql
@@ -2301,9 +2301,8 @@ CREATE TABLE {$db_prefix}user_alerts_prefs (
 # Dumping data for table `user_alerts_prefs`
 #
 
-INSERT INTO {$db_prefix}user_alerts_prefs
-	(id_member, alert_pref, alert_value)
-VALUES (0, 'member_register', 1);
+INSERT INTO {$db_prefix}user_alerts_prefs (id_member, alert_pref, alert_value) VALUES (0, 'member_register', 1);
+INSERT INTO {$db_prefix}user_alerts_prefs (id_member, alert_pref, alert_value) VALUES (0, 'msg_like', 1);
 # --------------------------------------------------------
 
 #
