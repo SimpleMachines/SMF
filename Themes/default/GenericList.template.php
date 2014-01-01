90d87f5032e61f380dd7ca92039b40b0025c3227
Peter Spicer <sleeping@myperch.org>
2014-01-01 01:41:23 +0000

    ! And the templates
    Signed-off-by: Peter Spicer <sleeping@myperch.org>

--- Themes/default/GenericList.template.php
+++ Themes/default/GenericList.template.php
@@ -4,7 +4,7 @@
  *
  * @package SMF
  * @author Simple Machines
- * @copyright 2013 Simple Machines and individual contributors
+ * @copyright 2014 Simple Machines and individual contributors
  * @license http://www.simplemachines.org/about/smf/license.php BSD
  *
  * @version 2.1 Alpha 1
