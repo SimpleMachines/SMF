182a1c70e320333e130bd85c185ef6d1863a3846
Suki <suki@missallsunday.com>
2014-01-06 18:55:29 -0600

    Remove instance of display_quick_reply on Settings and Profile template files
    Signed-off-by: Suki <suki@missallsunday.com>

--- Themes/default/Settings.template.php
+++ Themes/default/Settings.template.php
@@ -105,16 +105,6 @@ function template_options()
 			'default' => true,
 		),
 		array(
-			'id' => 'display_quick_reply',
-			'label' => $txt['display_quick_reply'],
-			'options' => array(
-				0 => $txt['display_quick_reply1'],
-				1 => $txt['display_quick_reply2'],
-				2 => $txt['display_quick_reply3'],
-			),
-			'default' => true,
-		),
-		array(
 			'id' => 'use_editor_quick_reply',
 			'label' => $txt['use_editor_quick_reply'],
 			'default' => true,
