f0fafdfa02a2a65126780ae38aaf58b7275dbc3b
Peter Spicer <sleeping@myperch.org>
2014-01-18 06:03:42 +0000

    ! Don't use $scripturl directly in alerts. For emails it's fine because they are less likely to linger, but for alerts they can hang around a while, e.g. if a user doesn't log in before a site move, and the user then logs in. Emails are by nature a bit more transient like that.
    Signed-off-by: Peter Spicer <sleeping@myperch.org>

--- Sources/Profile-View.php
+++ Sources/Profile-View.php
@@ -322,8 +322,8 @@ function fetch_alerts($memID, $all = false)
 		if (isset($txt[$string]))
 		{
 			$extra = $alerts[$id_alert]['extra'];
-			$search = array('{member_link}');
-			$repl = array(!empty($alert['sender_id']) ? '<a href="' . $scripturl . '?action=profile;u=' . $alert['sender_id'] . '">' . $alert['sender_name'] . '</a>' : $alert['sender_name']);
+			$search = array('{member_link}', '{scripturl}');
+			$repl = array(!empty($alert['sender_id']) ? '<a href="' . $scripturl . '?action=profile;u=' . $alert['sender_id'] . '">' . $alert['sender_name'] . '</a>' : $alert['sender_name'], $scripturl);
 			foreach ($extra as $k => $v)
 			{
 				$search[] = '{' . $k . '}';
