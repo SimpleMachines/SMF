dc182d6c7cadddcf9979d0dc264f04779b1fc245
Suki <suki@missallsunday.com>
2014-01-24 14:27:26 -0600

    And a brand new file is born!
    Signed-off-by: Suki <suki@missallsunday.com>

---
+++ Sources/ReportedPosts.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Handles reported posts and moderation comments
+ *
+ * Simple Machines Forum (SMF)
+ *
+ * @package SMF
+ * @author Simple Machines http://www.simplemachines.org
+ * @copyright 2014 Simple Machines and individual contributors
+ * @license http://www.simplemachines.org/about/smf/license.php BSD
+ *
+ * @version 2.1 Alpha 1
+ */
+
+if (!defined('SMF'))
+	die('No direct access...');
+
+/**
+ * Shows both open and closed reports as well as their respective comments
+ * calls a function based on each subaction.
+ * It requires the moderate_forum permission.
+ *
+ * @uses ModerationCenter template.
+ * @uses ModerationCenter language file.
+
+ *
+ */
+function ReportedPosts()
+{
+	global $txt, $context;
+}
+?>
