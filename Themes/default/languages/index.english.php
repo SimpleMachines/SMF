b9591b9f0afde12843ac4b8ff16f72b5b31324d8
Peter Spicer <sleeping@myperch.org>
2014-01-27 23:40:01 +0000

    ! One less link in the footer, as previously discussed and approved. I'm not *entirely* happy with the way it falls right now... but that's fixable. Important thing is one less link.
    Signed-off-by: Peter Spicer <sleeping@myperch.org>

--- Themes/default/languages/index.english.php
+++ Themes/default/languages/index.english.php
@@ -485,8 +485,7 @@ $txt['status'] = 'Status';
 $txt['go_up'] = 'Go Up';
 $txt['go_down'] = 'Go Down';
 
-$forum_copyright = '<a href="' . $scripturl . '?action=credits" title="Simple Machines Forum" class="new_win">%1$s</a> |
- <a href="http://www.simplemachines.org/about/smf/license.php" title="License" target="_blank" class="new_win">SMF &copy; %2$s</a>, <a href="http://www.simplemachines.org" title="Simple Machines" target="_blank" class="new_win">Simple Machines</a>';
+$forum_copyright = '<a href="http://www.simplemachines.org/about/smf/license.php" title="License" target="_blank" class="new_win">%1$s &copy; %2$s</a>, <a href="http://www.simplemachines.org" title="Simple Machines" target="_blank" class="new_win">Simple Machines</a>';
 
 $txt['birthdays'] = 'Birthdays:';
 $txt['events'] = 'Events:';
