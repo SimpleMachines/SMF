3f7729c8ec2c42fd7dc1ee77f55e287c12b5c7fd
Suki <suki@missallsunday.com>
2014-02-20 11:30:55 -0600

    Change on $txt['avatar_directory_wrong']
    Signed-off-by: Suki <suki@missallsunday.com>

--- Themes/default/languages/Admin.english.php
+++ Themes/default/languages/Admin.english.php
@@ -444,7 +444,7 @@ $txt['attachment_transfer_progress'] = 'Please wait. Transfer in progess.';
 
 $txt['mods_cat_avatars'] = 'Avatars';
 $txt['avatar_directory'] = 'Avatars directory';
-$txt['avatar_directory_wrong'] = 'The Avatars directory is not a valid directory, this will cause several issues within your forum, please specify a valid path.';
+$txt['avatar_directory_wrong'] = 'The Avatars directory is not valid. This will cause several issues with your forum.';
 $txt['avatar_url'] = 'Avatars URL';
 $txt['avatar_max_width_external'] = 'Maximum width of external avatar';
 $txt['avatar_max_height_external'] = 'Maximum height of external avatar';
