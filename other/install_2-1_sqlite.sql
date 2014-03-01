e5cc8925f8e8f452d04e9292a65f1c364afd63cb
Underdog <github.underdog@gmail.com>
2014-03-01 02:19:17 -0500

    Update install_2-1_sqlite.sql
    ! missing required field 'location' (ref. install.php - currently line 1465)
    
    Signed-off-by: -Underdog- github.underdog@gmail.com

--- other/install_2-1_sqlite.sql
+++ other/install_2-1_sqlite.sql
@@ -1327,6 +1327,7 @@ CREATE TABLE {$db_prefix}members (
   birthdate date NOT NULL default '0001-01-01',
   website_title varchar(255) NOT NULL,
   website_url varchar(255) NOT NULL,
+  location varchar(255) NOT NULL,
   hide_email smallint NOT NULL default '0',
   show_online smallint NOT NULL default '1',
   time_format varchar(80) NOT NULL default '',
