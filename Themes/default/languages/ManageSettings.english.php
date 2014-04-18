952337e515405dcbf202707bbf33b9700eb30c71
Suki <suki@missallsunday.com>
2014-04-18 12:43:07 -0500

    Couple more places for custom profile fields The position number doesn't really follow the actual order inside template_single_post() for compatibility and stuff.
    Signed-off-by: Suki <suki@missallsunday.com>

--- Themes/default/languages/ManageSettings.english.php
+++ Themes/default/languages/ManageSettings.english.php
@@ -281,6 +281,9 @@ $txt['custom_profile_placement'] = 'Placement';
 $txt['custom_profile_placement_standard'] = 'Standard';
 $txt['custom_profile_placement_withicons'] = 'With Icons';
 $txt['custom_profile_placement_abovesignature'] = 'Above Signature';
+$txt['custom_profile_placement_belowsignature'] = 'Below Signature';
+$txt['custom_profile_placement_below_avatar'] = 'Below Avatar';
+$txt['custom_profile_placement_abovesignature'] = 'Above Username';
 
 // Use numeric entities in the string below!
 $txt['custom_edit_delete_sure'] = 'Are you sure you wish to delete this field - all related user data will be lost!';
