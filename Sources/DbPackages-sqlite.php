c69acd048cfbbbcc8db62e2b731abab61f4e14f4
John Rayes <live627@gmail.com>
2014-04-29 23:50:02 -0700

    Continue removing collapsed_categories from the sources
    Signed-off-by: John Rayes <live627@gmail.com>

--- Sources/DbPackages-sqlite.php
+++ Sources/DbPackages-sqlite.php
@@ -44,7 +44,7 @@ function db_packages_init()
 
 	// We setup an array of SMF tables we can't do auto-remove on - in case a mod writer cocks it up!
 	$reservedTables = array('admin_info_files', 'approval_queue', 'attachments', 'ban_groups', 'ban_items',
-		'board_permissions', 'boards', 'calendar', 'calendar_holidays', 'categories', 'collapsed_categories',
+		'board_permissions', 'boards', 'calendar', 'calendar_holidays', 'categories',
 		'custom_fields', 'group_moderators', 'log_actions', 'log_activity', 'log_banned', 'log_boards',
 		'log_digest', 'log_errors', 'log_floodcontrol', 'log_group_requests', 'log_karma', 'log_mark_read',
 		'log_notify', 'log_online', 'log_packages', 'log_polls', 'log_reported', 'log_reported_comments',
