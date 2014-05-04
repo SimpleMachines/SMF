5ba5d3c99a9fac89cc63d92ed40e75fb80c64d93
John Rayes <live627@gmail.com>
2014-05-03 21:45:58 -0700

    Whitespace fix
    Signed-off-by: John Rayes <live627@gmail.com>

--- other/install.php
+++ other/install.php
@@ -795,7 +795,7 @@ function DatabaseSettings()
 		$incontext['db']['name'] = ($_POST['db_type'] == 'sqlite' || $_POST['db_type'] == 'sqlite3') && isset($_POST['db_filename']) ? $_POST['db_filename'] : $_POST['db_name'];
 		$incontext['db']['server'] = $_POST['db_server'];
 		$incontext['db']['prefix'] = $_POST['db_prefix'];
-		
+
 		if (!empty($_POST['db_port']))
 			$incontext['db']['port'] = $_POST['db_port'];
 	}
