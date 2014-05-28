d3916bee83497c32eb475a3fd24c915827b7fe08
Suki <suki@missallsunday.com>
2014-05-28 12:17:03 -0500

    Remove karma from Display.template and DbPackages-mysql
    Signed-off-by: Suki <suki@missallsunday.com>

--- Themes/default/Display.template.php
+++ Themes/default/Display.template.php
@@ -585,22 +585,6 @@ function template_single_post($message, $force_alternate = null)
 			echo '
 								<li class="postcount">', $txt['member_postcount'], ': ', $message['member']['posts'], '</li>';
 
-		// Is karma display enabled?  Total or +/-?
-		if ($modSettings['karmaMode'] == '1')
-			echo '
-								<li class="karma">', $modSettings['karmaLabel'], ' ', $message['member']['karma']['good'] - $message['member']['karma']['bad'], '</li>';
-		elseif ($modSettings['karmaMode'] == '2')
-			echo '
-								<li class="karma">', $modSettings['karmaLabel'], ' +', $message['member']['karma']['good'], '/-', $message['member']['karma']['bad'], '</li>';
-
-		// Is this user allowed to modify this member's karma?
-		if ($message['member']['karma']['allow'])
-			echo '
-								<li class="karma_allow">
-									<a href="', $scripturl, '?action=modifykarma;sa=applaud;uid=', $message['member']['id'], ';topic=', $context['current_topic'], '.' . $context['start'], ';m=', $message['id'], ';', $context['session_var'], '=', $context['session_id'], '">', $modSettings['karmaApplaudLabel'], '</a>
-									<a href="', $scripturl, '?action=modifykarma;sa=smite;uid=', $message['member']['id'], ';topic=', $context['current_topic'], '.', $context['start'], ';m=', $message['id'], ';', $context['session_var'], '=', $context['session_id'], '">', $modSettings['karmaSmiteLabel'], '</a>
-								</li>';
-
 		// Show their personal text?
 		if (!empty($modSettings['show_blurb']) && $message['member']['blurb'] != '')
 			echo '
