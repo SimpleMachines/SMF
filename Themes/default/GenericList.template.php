5b157ccb54db00952fd7e3648c7e524c947add70
Antes <antes@simplemachines.org>
2014-06-16 22:51:09 +0300

    Some more cleaning and UX improvements... Signed-off-by: Antes <antes@simplemachines.org>

--- Themes/default/GenericList.template.php
+++ Themes/default/GenericList.template.php
@@ -26,7 +26,7 @@ function template_show_list($list_id = null)
 
 	if (isset($cur_list['form']))
 		echo '
-	<form class="generic_list_wrapper" action="', $cur_list['form']['href'], '" method="post"', empty($cur_list['form']['name']) ? '' : ' name="' . $cur_list['form']['name'] . '" id="' . $cur_list['form']['name'] . '"', ' accept-charset="', $context['character_set'], '">';
+	<form action="', $cur_list['form']['href'], '" method="post"', empty($cur_list['form']['name']) ? '' : ' name="' . $cur_list['form']['name'] . '" id="' . $cur_list['form']['name'] . '"', ' accept-charset="', $context['character_set'], '">';
 
 	// Show the title of the table (if any).
 	if (!empty($cur_list['title']))
--- Themes/default/ManageNews.template.php
+++ Themes/default/ManageNews.template.php
@@ -21,35 +21,33 @@ function template_email_members()
 
 	echo '
 	<div id="admincenter">
-		<form action="', $scripturl, '?action=admin;area=news;sa=mailingcompose" method="post" id="admin_newsletters" class="flow_hidden" accept-charset="', $context['character_set'], '">
+		<form action="', $scripturl, '?action=admin;area=news;sa=mailingcompose" method="post" id="admin_newsletters" class="roundframe flow_hidden" accept-charset="', $context['character_set'], '">
 			<div class="cat_bar">
 				<h3 class="catbg">', $txt['admin_newsletters'], '</h3>
 			</div>
-			<div class="information">
+			<div class="description">
 				', $txt['admin_news_select_recipients'], '
 			</div>
-			<div class="windowbg">
-				<div class="content">
-					<dl class="settings">
-						<dt>
-							<strong>', $txt['admin_news_select_group'], ':</strong><br>
-							<span class="smalltext">', $txt['admin_news_select_group_desc'], '</span>
-						</dt>
-						<dd>';
+			<div class="padding">
+				<dl class="settings">
+					<dt>
+						<strong>', $txt['admin_news_select_group'], ':</strong><br>
+						<span class="smalltext">', $txt['admin_news_select_group_desc'], '</span>
+					</dt>
+					<dd>';
 
 	foreach ($context['groups'] as $group)
 				echo '
-							<label for="groups_', $group['id'], '"><input type="checkbox" name="groups[', $group['id'], ']" id="groups_', $group['id'], '" value="', $group['id'], '" checked class="input_check"> ', $group['name'], '</label> <em>(', $group['member_count'], ')</em><br>';
+						<label for="groups_', $group['id'], '"><input type="checkbox" name="groups[', $group['id'], ']" id="groups_', $group['id'], '" value="', $group['id'], '" checked class="input_check"> ', $group['name'], '</label> <em>(', $group['member_count'], ')</em><br>';
 
 	echo '
-							<br>
-							<label for="checkAllGroups"><input type="checkbox" id="checkAllGroups" checked onclick="invertAll(this, this.form, \'groups\');" class="input_check"> <em>', $txt['check_all'], '</em></label>';
+						<br>
+						<label for="checkAllGroups"><input type="checkbox" id="checkAllGroups" checked onclick="invertAll(this, this.form, \'groups\');" class="input_check"> <em>', $txt['check_all'], '</em></label>';
 
 	echo '
-						</dd>
-					</dl>
-					<br class="clear">
-				</div>
+					</dd>
+				</dl>
+				<br class="clear">
 			</div>
 			<br>
 
@@ -60,61 +58,59 @@ function template_email_members()
 				</h3>
 			</div>
 
-			<div id="advanced_panel_div" class="windowbg2">
-				<div class="content">
-					<dl class="settings">
-						<dt>
-							<strong>', $txt['admin_news_select_email'], ':</strong><br>
-							<span class="smalltext">', $txt['admin_news_select_email_desc'], '</span>
-						</dt>
-						<dd>
-							<textarea name="emails" rows="5" cols="30" style="' . (isBrowser('is_ie8') ? 'width: 635px; max-width: 98%; min-width: 98%' : 'width: 98%') . ';"></textarea>
-						</dd>
-						<dt>
-							<strong>', $txt['admin_news_select_members'], ':</strong><br>
-							<span class="smalltext">', $txt['admin_news_select_members_desc'], '</span>
-						</dt>
-						<dd>
-							<input type="text" name="members" id="members" value="" size="30" class="input_text">
-							<span id="members_container"></span>
-						</dd>
-					</dl>
-					<hr class="bordercolor">
-					<dl class="settings">
-						<dt>
-							<strong>', $txt['admin_news_select_excluded_groups'], ':</strong><br>
-							<span class="smalltext">', $txt['admin_news_select_excluded_groups_desc'], '</span>
-						</dt>
-						<dd>';
+			<div id="advanced_panel_div" class="padding">
+				<dl class="settings">
+					<dt>
+						<strong>', $txt['admin_news_select_email'], ':</strong><br>
+						<span class="smalltext">', $txt['admin_news_select_email_desc'], '</span>
+					</dt>
+					<dd>
+						<textarea name="emails" rows="5" cols="30" style="' . (isBrowser('is_ie8') ? 'width: 635px; max-width: 98%; min-width: 98%' : 'width: 98%') . ';"></textarea>
+					</dd>
+					<dt>
+						<strong>', $txt['admin_news_select_members'], ':</strong><br>
+						<span class="smalltext">', $txt['admin_news_select_members_desc'], '</span>
+					</dt>
+					<dd>
+						<input type="text" name="members" id="members" value="" size="30" class="input_text">
+						<span id="members_container"></span>
+					</dd>
+				</dl>
+				<hr class="bordercolor">
+				<dl class="settings">
+					<dt>
+						<strong>', $txt['admin_news_select_excluded_groups'], ':</strong><br>
+						<span class="smalltext">', $txt['admin_news_select_excluded_groups_desc'], '</span>
+					</dt>
+					<dd>';
 
 	foreach ($context['groups'] as $group)
 				echo '
-							<label for="exclude_groups_', $group['id'], '"><input type="checkbox" name="exclude_groups[', $group['id'], ']" id="exclude_groups_', $group['id'], '" value="', $group['id'], '" class="input_check"> ', $group['name'], '</label> <em>(', $group['member_count'], ')</em><br>';
+						<label for="exclude_groups_', $group['id'], '"><input type="checkbox" name="exclude_groups[', $group['id'], ']" id="exclude_groups_', $group['id'], '" value="', $group['id'], '" class="input_check"> ', $group['name'], '</label> <em>(', $group['member_count'], ')</em><br>';
 
 	echo '
-							<br>
-							<label for="checkAllGroupsExclude"><input type="checkbox" id="checkAllGroupsExclude" onclick="invertAll(this, this.form, \'exclude_groups\');" class="input_check"> <em>', $txt['check_all'], '</em></label><br>
-						</dd>
-						<dt>
-							<strong>', $txt['admin_news_select_excluded_members'], ':</strong><br>
-							<span class="smalltext">', $txt['admin_news_select_excluded_members_desc'], '</span>
-						</dt>
-						<dd>
-							<input type="text" name="exclude_members" id="exclude_members" value="" size="30" class="input_text">
-							<span id="exclude_members_container"></span>
-						</dd>
-					</dl>
-					<hr class="bordercolor">
-					<dl class="settings">
-						<dt>
-							<label for="email_force"><strong>', $txt['admin_news_select_override_notify'], ':</strong></label><br>
-							<span class="smalltext">', $txt['email_force'], '</span>
-						</dt>
-						<dd>
-							<input type="checkbox" name="email_force" id="email_force" value="1" class="input_check">
-						</dd>
-					</dl>
-				</div>
+						<br>
+						<label for="checkAllGroupsExclude"><input type="checkbox" id="checkAllGroupsExclude" onclick="invertAll(this, this.form, \'exclude_groups\');" class="input_check"> <em>', $txt['check_all'], '</em></label><br>
+					</dd>
+					<dt>
+						<strong>', $txt['admin_news_select_excluded_members'], ':</strong><br>
+						<span class="smalltext">', $txt['admin_news_select_excluded_members_desc'], '</span>
+					</dt>
+					<dd>
+						<input type="text" name="exclude_members" id="exclude_members" value="" size="30" class="input_text">
+						<span id="exclude_members_container"></span>
+					</dd>
+				</dl>
+				<hr class="bordercolor">
+				<dl class="settings">
+					<dt>
+						<label for="email_force"><strong>', $txt['admin_news_select_override_notify'], ':</strong></label><br>
+						<span class="smalltext">', $txt['email_force'], '</span>
+					</dt>
+					<dd>
+						<input type="checkbox" name="email_force" id="email_force" value="1" class="input_check">
+					</dd>
+				</dl>
 			</div>
 			<div class="righttext">
 				<input type="submit" value="', $txt['admin_next'], '" class="button_submit">
