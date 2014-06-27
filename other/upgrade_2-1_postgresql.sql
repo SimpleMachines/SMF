7dc419a0328919f3a51ed6b515ab330a3b97d075
Suki <suki@missallsunday.com>
2014-06-27 11:44:14 -0500

    If we already are using a custom dir, delete the predefined one.
    Signed-off-by: Suki <suki@missallsunday.com>

--- other/upgrade_2-1_postgresql.sql
+++ other/upgrade_2-1_postgresql.sql
@@ -146,6 +146,10 @@ $custom_av_dir = !empty($modSettings['custom_avatar_dir']) ? $modSettings['custo
 if (!is_writable($custom_av_dir))
 	@chmod($custom_av_dir, 0777);
 
+// If we already are using a custom dir, delete the predefined one.
+if ($custom_av_dir != $GLOBALS['boarddir'] .'/custom_avatar')
+	@rmdir($GLOBALS['boarddir'] .'/custom_avatar');
+
 $request = upgrade_query("
 	SELECT MAX(id_attach)
 	FROM {$db_prefix}attachments");
