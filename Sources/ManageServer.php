fa96a1851d449a948e3b9061f8393d340b2b86c3
John Rayes <live627@gmail.com>
2014-06-28 15:01:21 -0700

    ! saveSettings() didn't check POST variables
    Signed-off-by: John Rayes <live627@gmail.com>

--- Sources/ManageServer.php
+++ Sources/ManageServer.php
@@ -951,7 +951,7 @@ function saveSettings(&$config_vars)
 	// Figure out which config vars we're saving here...
 	foreach ($config_vars as $var)
 	{
-		if (!is_array($var) || $var[2] != 'file')
+		if (!is_array($var) || $var[2] != 'file' || !isset($_POST[$var[0]]))
 			continue;
 
 		$config_var = $var[0];
