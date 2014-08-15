423bd7cb47ed69c27da1da6a8c6befe48b9c9a18
Michael Eshom <oldiesmann@oldiesmann.us>
2014-08-15 14:43:00 -0400

    ! Forgot the language strings
    Signed-off-by: Michael Eshom <oldiesmann@oldiesmann.us>

--- Themes/default/languages/ManageSmileys.english.php
+++ Themes/default/languages/ManageSmileys.english.php
@@ -21,9 +21,6 @@ $txt['smiley_sets_name'] = 'Name';
 $txt['smiley_sets_url'] = 'URL';
 $txt['smiley_sets_default'] = 'Default';
 
-$txt['smiley_sets_latest'] = 'Smileys of the moment';
-$txt['smiley_sets_latest_fetch'] = 'Loading smiley information from simplemachines.org...';
-
 $txt['smileys_add_method'] = 'Image Source';
 $txt['smileys_add_existing'] = 'Use existing file';
 $txt['smileys_add_upload'] = 'Upload new smiley';
