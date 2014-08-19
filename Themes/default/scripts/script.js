SimpleMachines/SMF

commit 75b612c1e2538e9b62467ef7f6197d959b663c1c

* Reworking the page index to be formatted by the template - Document thee function more - Fix page indexes in RTL mode (Antes! Bad kitty! >:D - Jaavascript won’t work right. The event doesn’t seem to update. I can expand the first 50 pages, but the next click adds the first 50 again. Help? :D
Signed-off-by: John Rayes <live627@gmail.com>

Themes/default/scripts/script.js
@@ -702,7 +702,7 @@ function smf_setThemeOption(option, value, theme, cur_session_id, cur_session_va
 }
 
 // Shows the page numbers by clicking the dots (in compact view).
-function expandPages(spanNode, baseURL, firstPage, lastPage, perPage)
+function expandPages(spanNode, baseLink, firstPage, lastPage, perPage)
 {
 	var replacement = '', i, oldLastPage = 0;
 	var perPageLimit = 50;
@@ -716,13 +716,17 @@ function expandPages(spanNode, baseURL, firstPage, lastPage, perPage)
 
 	// Calculate the new pages.
 	for (i = firstPage; i < lastPage; i += perPage)
-		replacement += '<a class="navPages" href="' + baseURL.replace(/%1\$d/, i).replace(/%%/g, '%') + '">' + (1 + i / perPage) + '</a> ';
+		replacement += baseLink.replace(/%1\$d/, i).replace(/%2\$s/, 1 + i / perPage).replace(/%%/g, '%');
 
-	if (oldLastPage > 0)
-		replacement += '<span class="expand_pages" onclick="expandPages(this, \'' + baseURL + '\', ' + lastPage + ', ' + oldLastPage + ', ' + perPage + ');"> ... </span>';
+	// Add the new page links.
+	$(spanNode).before(replacement);
 
-	// Replace the dots by the new page links.
-	setOuterHTML(spanNode, replacement);
+	if (oldLastPage)
+		$(spanNode).off().one('click', function () {
+			expandPages(this, baseLink, lastPage, oldLastPage, perPage);
+		});
+	else
+		$(spanNode).remove()
 }
 
 function smc_preCacheImage(sSrc)

Sources/Subs.php
@@ -535,16 +535,18 @@ function updateSettings($changeArray, $update = false)
  * an example is available near the function definition.
  * $pageindex = constructPageIndex($scripturl . '?board=' . $board, $_REQUEST['start'], $num_messages, $maxindex, true);
  *
- * @param string $base_url
- * @param int $start
- * @param int $max_value
- * @param int $num_per_page
- * @param bool $flexible_start = false
- * @param bool $show_prevnext = true
+ * @param string $base_url The basic URL to be used for each link.
+ * @param int &$start The start position, by reference. If this is not a multiple of the number of items per page, it is sanitized to be so and the value will persist upon the function's return.
+ * @param int $max_value The total number of items you are paginating for.
+ * @param int $num_per_page The number of items to be displayed on a given page. $start will be forced to be a multiple of this value.
+ * @param bool $flexible_start Whether a ;start=x component should be introduced into the URL automatically (see above)
+ * @param bool $show_prevnext Whether the Previous and Next links should be shown (should be on only when navigating the list)
+ *
+ * @return string The complete HTML of the page index that was requested, formated by the template.
  */
 function constructPageIndex($base_url, &$start, $max_value, $num_per_page, $flexible_start = false, $show_prevnext = true)
 {
-	global $modSettings, $context, $txt, $smcFunc;
+	global $modSettings, $context, $smcFunc, $settings;
 
 	// Save whether $start was less than 0 or not.
 	$start = (int) $start;
@@ -566,24 +568,24 @@ function constructPageIndex($base_url, &$start, $max_value, $num_per_page, $flex
 	if (WIRELESS)
 		$base_url .= ';' . WIRELESS_PROTOCOL;
 
-	$base_link = '<a class="navPages" href="' . ($flexible_start ? $base_url : strtr($base_url, array('%' => '%%')) . ';start=%1$d') . '">%2$s</a> ';
-	$pageindex = $txt['pages'] . ': ';
+	$base_link = strtr($settings['page_index']['page'], array('{URL}' => $flexible_start ? $base_url : strtr($base_url, array('%' => '%%')) . ';start=%1$d'));
+	$pageindex = $settings['page_index']['extra_before'];
 
 	// Compact pages is off or on?
 	if (empty($modSettings['compactTopicPagesEnable']))
 	{
 		// Show the left arrow.
-		$pageindex .= $start == 0 ? ' ' : sprintf($base_link, $start - $num_per_page, '<span class="previous_page"></span>');
+		$pageindex .= $start == 0 ? ' ' : sprintf($base_link, $start - $num_per_page, $settings['page_index']['previous_page']);
 
 		// Show all the pages.
 		$display_page = 1;
 		for ($counter = 0; $counter < $max_value; $counter += $num_per_page)
-			$pageindex .= $start == $counter && !$start_invalid ? '<span class="current_page"><strong>' . $display_page++ . '</strong></span> ' : sprintf($base_link, $counter, $display_page++);
+			$pageindex .= $start == $counter && !$start_invalid ? sprintf($settings['page_index']['current_page'], $display_page++) : sprintf($base_link, $counter, $display_page++);
 
 		// Show the right arrow.
 		$display_page = ($start + $num_per_page) > $max_value ? $max_value : ($start + $num_per_page);
 		if ($start != $counter - $max_value && !$start_invalid)
-			$pageindex .= $display_page > $counter - $num_per_page ? ' ' : sprintf($base_link, $display_page, '<span class="next_page"></span>');
+			$pageindex .= $display_page > $counter - $num_per_page ? ' ' : sprintf($base_link, $display_page, $settings['page_index']['next_page']);
 	}
 	else
 	{
@@ -592,7 +594,7 @@ function constructPageIndex($base_url, &$start, $max_value, $num_per_page, $flex
 
 		// Show the "prev page" link. (>prev page< 1 ... 6 7 [8] 9 10 ... 15 next page)
 		if (!empty($start) && $show_prevnext)
-			$pageindex .= sprintf($base_link, $start - $num_per_page, '<span class="previous_page"></span>');
+			$pageindex .= sprintf($base_link, $start - $num_per_page, $settings['page_index']['previous_page']);
 		else
 			$pageindex .= '';
 
@@ -602,7 +604,12 @@ function constructPageIndex($base_url, &$start, $max_value, $num_per_page, $flex
 
 		// Show the ... after the first page.  (prev page 1 >...< 6 7 [8] 9 10 ... 15 next page)
 		if ($start > $num_per_page * ($PageContiguous + 1))
-			$pageindex .= '<span class="expand_pages" onclick="' . $smcFunc['htmlspecialchars']('expandPages(this, ' . JavaScriptEscape(($flexible_start ? $base_url : strtr($base_url, array('%' => '%%')) . ';start=%1$d')) . ', ' . $num_per_page . ', ' . ($start - $num_per_page * $PageContiguous) . ', ' . $num_per_page . ');') . '"><strong> ... </strong></span>';
+			$pageindex .= strtr($settings['page_index']['expand_pages'], array(
+				'{LINK}' => JavaScriptEscape($smcFunc['htmlspecialchars']($base_link)),
+				'{FIRST_PAGE}' => $num_per_page,
+				'{LAST_PAGE}' => $start - $num_per_page * $PageContiguous,
+				'{PER_PAGE}' => $num_per_page,
+			));
 
 		// Show the pages before the current one. (prev page 1 ... >6 7< [8] 9 10 ... 15 next page)
 		for ($nCont = $PageContiguous; $nCont >= 1; $nCont--)
@@ -614,7 +621,7 @@ function constructPageIndex($base_url, &$start, $max_value, $num_per_page, $flex
 
 		// Show the current page. (prev page 1 ... 6 7 >[8]< 9 10 ... 15 next page)
 		if (!$start_invalid)
-			$pageindex .= '<span class="current_page"><strong>[' . ($start / $num_per_page + 1) . ']</strong></span>';
+			$pageindex .= sprintf($settings['page_index']['current_page'], $start / $num_per_page + 1);
 		else
 			$pageindex .= sprintf($base_link, $start, $start / $num_per_page + 1);
 
@@ -629,7 +636,12 @@ function constructPageIndex($base_url, &$start, $max_value, $num_per_page, $flex
 
 		// Show the '...' part near the end. (prev page 1 ... 6 7 [8] 9 10 >...< 15 next page)
 		if ($start + $num_per_page * ($PageContiguous + 1) < $tmpMaxPages)
-			$pageindex .= '<span class="expand_pages" onclick="' . $smcFunc['htmlspecialchars']('expandPages(this, ' . JavaScriptEscape(($flexible_start ? $base_url : strtr($base_url, array('%' => '%%')) . ';start=%1$d')) . ', ' . ($start + $num_per_page * ($PageContiguous + 1)) . ', ' . $tmpMaxPages . ', ' . $num_per_page . ');') . '" onmouseover="this.style.cursor=\'pointer\';"> ... </span>';
+			$pageindex .= strtr($settings['page_index']['expand_pages'], array(
+				'{LINK}' => JavaScriptEscape($smcFunc['htmlspecialchars']($base_link)),
+				'{FIRST_PAGE}' => $start + $num_per_page * ($PageContiguous + 1),
+				'{LAST_PAGE}' => $tmpMaxPages,
+				'{PER_PAGE}' => $num_per_page,
+			));
 
 		// Show the last number in the list. (prev page 1 ... 6 7 [8] 9 10 ... >15<  next page)
 		if ($start + $num_per_page * $PageContiguous < $tmpMaxPages)
@@ -637,8 +649,9 @@ function constructPageIndex($base_url, &$start, $max_value, $num_per_page, $flex
 
 		// Show the "next page" link. (prev page 1 ... 6 7 [8] 9 10 ... 15 >next page<)
 		if ($start != $tmpMaxPages && $show_prevnext)
-			$pageindex .= sprintf($base_link, $start + $num_per_page, '<span class="next_page"></span>');
+			$pageindex .= sprintf($base_link, $start + $num_per_page, $settings['page_index']['next_page']);
 	}
+	$pageindex .= $settings['page_index']['extra_after'];
 
 	return $pageindex;
 }

Themes/default/css/index.css
@@ -562,6 +562,9 @@ div.pagesection div.floatright input, div.pagesection div.floatright select {
 .pagelinks {
 	margin: 8px 0 0 0;
 }
+.pagelinks p {
+	display: inline;
+}
 .navPages {
 	padding: 0 1px;
 }

Themes/default/css/rtl.css
@@ -76,6 +76,12 @@ td.lastpost {
 	border-left: 0px !important;
 	border-right: 1px solid #ddd;
 }
+.previous_page {
+	background-position: -113px -33px;
+}
+.next_page {
+	background-position: -113px -49px;
+}
 
 /* Lists with settings use these a lot.
 ------------------------------------------------------- */

Themes/default/index.template.php
@@ -40,7 +40,7 @@
  */
 function template_init()
 {
-	global $settings;
+	global $settings, $txt;
 
 	/* $context, $options and $txt may be available for use, but may not be fully populated yet. */
 
@@ -62,8 +62,19 @@ function template_init()
 	// Set the following variable to true if this theme requires the optional theme strings file to be loaded.
 	$settings['require_theme_strings'] = false;
 
-	// Set the following variable to true is this theme wants to display the avatar of the user that posted the last post on the board index and message index
+	// Set the following variable to true is this theme wants to display the avatar of the user that posted the last post on the board index and message index.
 	$settings['avatars_on_indexes'] = false;
+
+	// This defines the formatting for the page indexes used throughout the forumm.
+	$settings['page_index'] = array(
+		'extra_before' => '<p>' . $txt['pages'] . ':</p> ',
+		'previous_page' => '<span class="previous_page"></span>',
+		'current_page' => '<span class="current_page"><strong>[%1$d]</strong></span> ',
+		'page' => '<a class="navPages" href="{URL}">%2$s</a> ',
+		'expand_pages' => '<span class="expand_pages" onclick="expandPages(this, {LINK}, {FIRST_PAGE}, {LAST_PAGE}, {PER_PAGE});"> ... </span>',
+		'next_page' => '<span class="next_page"></span>',
+		'extra_after' => '',
+	);
 }
 
 /**

commit 53c1577fc52ecdad20d954736d5b859a09f99696

! Fix bug introduced in 75b612c1e2538e9b62467ef7f6197d959b663c1c
Signed-off-by: John Rayes <live627@gmail.com>

Themes/default/scripts/script.js
@@ -729,11 +729,13 @@ function expandPages(spanNode, baseLink, firstPage, lastPage, perPage)
 	$(spanNode).before(replacement);
 
 	if (oldLastPage)
-		$(spanNode).off().one('click', function () {
-			expandPages(this, baseLink, lastPage, oldLastPage, perPage);
-		});
+		// Access the raw DOM element so the native onclick event can be overridden.
+		spanNode.onclick = function ()
+		{
+			expandPages(spanNode, baseLink, lastPage, oldLastPage, perPage);
+		};
 	else
-		$(spanNode).remove()
+		$(spanNode).remove();
 }
 
 function smc_preCacheImage(sSrc)