6889972e0a0e5a4b9773d5baadc20549988b769f
John Rayes <live627@gmail.com>
2014-08-18 23:29:02 -0700

    ! Stop the undefined index error
    Signed-off-by: John Rayes <live627@gmail.com>

--- Sources/Subs-Menu.php
+++ Sources/Subs-Menu.php
@@ -130,7 +130,7 @@ function createMenu($menuData, $menuOptions = array())
 						elseif (isset($area['icon']))
 							$menu_context['sections'][$section_id]['areas'][$area_id]['icon'] = '<span class="generic_icons '.$area['icon'] .'"></span>';
 						else
-							$menu_context['sections'][$section_id]['areas'][$area_id]['icon'] = '<span class="generic_icons '.$area['icon'] .'"></span>';
+							$menu_context['sections'][$section_id]['areas'][$area_id]['icon'] = '<span class="generic_icons '.$area_id .'"></span>';
 
 						if (isset($area['icon_class']) && empty($menu_context['sections'][$section_id]['areas'][$area_id]['icon']))
 							$menu_context['sections'][$section_id]['areas'][$area_id]['icon_class'] = $menu_context['current_action'] . '-menu-icon ' . $area['icon_class'];
