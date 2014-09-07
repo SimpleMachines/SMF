SimpleMachines/SMF

commit 5e9ad31775de5fbfc2709dac49601b46d8a10b1d

Fix undefined index errors in Display.php for guests Notice: Undefined index: pref in /mnt/data/Web/smf21/Sources/Display.php on line 224 Guests don't have watch preferences.
Signed-off-by: Yoshi2889 <rick.2889@gmail.com>

Sources/Display.php
@@ -221,7 +221,7 @@ function Display()
 	$context['topic_unwatched'] = isset($topicinfo['unwatched']) ? $topicinfo['unwatched'] : 0;
 	$context['topic_notification'] = !empty($user_info['id']) ? $topicinfo['notify_prefs'] : array();
 	// 0 => unwatched, 1 => normal, 2 => receive alerts, 3 => receive emails
-	$context['topic_notification_mode'] = $context['topic_unwatched'] ? 0 : ($topicinfo['notify_prefs']['pref'] & 0x02 ? 3 : ($topicinfo['notify_prefs']['pref'] & 0x01 ? 2 : 1));
+	$context['topic_notification_mode'] = !$user_info['is_guest'] ? ($context['topic_unwatched'] ? 0 : ($topicinfo['notify_prefs']['pref'] & 0x02 ? 3 : ($topicinfo['notify_prefs']['pref'] & 0x01 ? 2 : 1))) : 0;
 
 	// Add up unapproved replies to get real number of replies...
 	if ($modSettings['postmod_active'] && allowedTo('approve_posts'))
@@ -527,6 +527,11 @@ function Display()
 	$context['is_locked'] = $topicinfo['locked'];
 	$context['is_sticky'] = $topicinfo['is_sticky'];
 	$context['is_approved'] = $topicinfo['approved'];
+
+	// @todo Tricks? We don't want to show the poll icon in the topic class here, so pretend it's not one.
+	$context['is_poll'] = false;
+	determineTopicClass($context);
+
 	$context['is_poll'] = $topicinfo['id_poll'] > 0 && $modSettings['pollMode'] == '1' && allowedTo('poll_view');
 
 	// Did this user start the topic or not?

commit 9ad73b67e0f1f580b8f1cfacbb467f8906caecad

! Fix bug introduced in 5e9ad31775de5fbfc2709dac49601b46d8a10b1d
Signed-off-by: John Rayes <live627@gmail.com>

Sources/Display.php
@@ -527,11 +527,6 @@ function Display()
 	$context['is_locked'] = $topicinfo['locked'];
 	$context['is_sticky'] = $topicinfo['is_sticky'];
 	$context['is_approved'] = $topicinfo['approved'];
-
-	// @todo Tricks? We don't want to show the poll icon in the topic class here, so pretend it's not one.
-	$context['is_poll'] = false;
-	determineTopicClass($context);
-
 	$context['is_poll'] = $topicinfo['id_poll'] > 0 && $modSettings['pollMode'] == '1' && allowedTo('poll_view');
 
 	// Did this user start the topic or not?