985ed55e5877ad138c74f1664111ce5779468bae
Suki <suki@missallsunday.com>
2014-09-12 13:43:56 -0500

    meh... would be cool if this was a valid way to fix this :P #2298
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/ManageServer.php
+++ Sources/ManageServer.php
@@ -1072,7 +1072,7 @@ function saveDBSettings(&$config_vars)
 	global $sourcedir, $smcFunc;
 	static $board_list = null;
 
-	// validateToken('admin-dbsc');
+	validateToken('admin-dbsc');
 
 	$inlinePermissions = array();
 	foreach ($config_vars as $var)
