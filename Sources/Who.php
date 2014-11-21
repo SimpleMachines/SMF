619533e0ab98a277cc00cd6c7d333a1edbf4807d
Suki <suki@missallsunday.com>
2014-11-21 08:49:04 -0600

    Remove &quot;CoreISP&quot;
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/Who.php
+++ Sources/Who.php
@@ -687,7 +687,7 @@ function Credits($in_admin = false)
 					'members' => array(
 						'Derek Schwab',
 						'Michael Johnson',
-						'Liroy &quot;CoreISP&quot; van Hoewijk',
+						'Liroy van Hoewijk',
 					),
 				),
 			),
