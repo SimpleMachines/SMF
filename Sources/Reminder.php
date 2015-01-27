ca5a10c1f9c1cfa9419313aed549ac4d11e3d6d1
John Rayes <live627@gmail.com>
2015-01-26 21:06:24 -0700

    Fix #2691
    Restore lines removed in 2a8a2be3fdfaa33b67a00a3f599949dfa6bfc03c
    
    Signed-off-by: John Rayes <live627@gmail.com>

--- Sources/Reminder.php
+++ Sources/Reminder.php
@@ -141,6 +141,8 @@ function RemindPick()
 		$emaildata = loadEmailTemplate('forgot_password', $replacements, empty($row['lngfile']) || empty($modSettings['userLanguage']) ? $language : $row['lngfile']);
 		$context['description'] = $txt['reminder_sent'];
 
+		sendmail($row['email_address'], $emaildata['subject'], $emaildata['body'], null, 'reminder', false, 1);
+
 		// Set the password in the database.
 		updateMemberData($row['id_member'], array('validation_code' => substr(md5($password), 0, 10)));
 
