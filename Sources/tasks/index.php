131d7d9c314e076ccfdab5b66b1b40491f756d0a
Suki <suki@missallsunday.com>
2015-02-05 14:24:33 -0600

    Try to handle it with the upper level index.php
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/tasks/index.php
+++ Sources/tasks/index.php
@@ -1,17 +1,8 @@
 <?php
 
-/**
- * This file is here solely to protect your tasks directory.
- */
-
-// Look for Settings.php....
-if (file_exists(dirname(dirname(__FILE__)) . '/Settings.php'))
-{
-	// Found it!
-	require(dirname(dirname(__FILE__)) . '/Settings.php');
-	header('Location: ' . $boardurl);
-}
-// Can't find it... just forget it.
+// Try to handle it with the upper level index.php. (it should know what to do.)
+if (file_exists(dirname(dirname(__FILE__)) . '/index.php'))
+	include (dirname(dirname(__FILE__)) . '/index.php');
 else
 	exit;
 
