SimpleMachines/SMF

commit 27e668f1f29c15fdbb247d39851a6282021af23b

! Split out paid subs errors in the error log, in case it's useful to someone.
Signed-off-by: Peter Spicer <sleeping@myperch.org>

Themes/default/languages/ManageMaintenance.english.php
@@ -126,6 +126,8 @@ $txt['errortype_user'] = 'User';
 $txt['errortype_user_desc'] = 'Errors resulting from user errors.  Includes failed passwords, trying to login when banned, and trying to do an action for which they do not have permission.';
 $txt['errortype_cron'] = 'Cron';
 $txt['errortype_cron_desc'] = 'Errors resulting from background tasks.';
+$txt['errortype_paidsubs'] = 'Paid Subs';
+$txt['errortype_paidsubs'] = 'Errors resulting from paid subscriptions, which can include notification of payment failures.';
 
 $txt['maintain_recount'] = 'Recount all forum totals and statistics';
 $txt['maintain_recount_info'] = 'Should the total replies of a topic or the number of PMs in your inbox be incorrect: this function will recount all saved counts and statistics for you.';

Sources/Errors.php
@@ -83,6 +83,7 @@ function log_error($error_message, $error_type = 'general', $file = null, $line 
 		'template',
 		'debug',
 		'cron',
+		'paidsubs',
 	);
 
 	// This prevents us from infinite looping if the hook or call produces an error.

subscriptions.php
@@ -312,7 +312,7 @@ function generateSubscriptionError($text)
 	}
 
 	// Then just log and die.
-	log_error($text);
+	log_error($text, 'paidsubs');
 
 	exit;
 }

commit 3001351daa402ed448e84dab9c6f1d86e0047fd7

Fix bug introduced in 27e668f1f29c15fdbb247d39851a6282021af23b
Signed-off-by: John Rayes <live627@gmail.com>

Themes/default/languages/ManageMaintenance.english.php
@@ -127,7 +127,7 @@ $txt['errortype_user_desc'] = 'Errors resulting from user errors. Includes faile
 $txt['errortype_cron'] = 'Cron';
 $txt['errortype_cron_desc'] = 'Errors resulting from background tasks.';
 $txt['errortype_paidsubs'] = 'Paid Subs';
-$txt['errortype_paidsubs'] = 'Errors resulting from paid subscriptions, which can include notification of payment failures.';
+$txt['errortype_paidsubs_desc'] = 'Errors resulting from paid subscriptions, which can include notification of payment failures.';
 
 $txt['maintain_recount'] = 'Recount all forum totals and statistics';
 $txt['maintain_recount_info'] = 'Should the total replies of a topic or the number of PMs in your inbox be incorrect: this function will recount all saved counts and statistics for you.';
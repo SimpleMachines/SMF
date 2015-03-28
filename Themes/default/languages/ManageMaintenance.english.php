3001351daa402ed448e84dab9c6f1d86e0047fd7
John Rayes <live627@gmail.com>
2015-03-27 17:27:51 -0700

    Fix bug introduced in 27e668f1f29c15fdbb247d39851a6282021af23b
    Signed-off-by: John Rayes <live627@gmail.com>

--- Themes/default/languages/ManageMaintenance.english.php
+++ Themes/default/languages/ManageMaintenance.english.php
@@ -127,7 +127,7 @@ $txt['errortype_user_desc'] = 'Errors resulting from user errors. Includes faile
 $txt['errortype_cron'] = 'Cron';
 $txt['errortype_cron_desc'] = 'Errors resulting from background tasks.';
 $txt['errortype_paidsubs'] = 'Paid Subs';
-$txt['errortype_paidsubs'] = 'Errors resulting from paid subscriptions, which can include notification of payment failures.';
+$txt['errortype_paidsubs_desc'] = 'Errors resulting from paid subscriptions, which can include notification of payment failures.';
 
 $txt['maintain_recount'] = 'Recount all forum totals and statistics';
 $txt['maintain_recount_info'] = 'Should the total replies of a topic or the number of PMs in your inbox be incorrect: this function will recount all saved counts and statistics for you.';
