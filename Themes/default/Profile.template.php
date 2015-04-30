7c53cb9dbd068f479a5716145cc7006f130ee404
Antes <antes@simplemachines.org>
2015-04-30 12:27:49 +0300

    Well, as much as i like how it looks localization is also part of SMF, so we need to change our UI to improve for all (fixes #2818).
    Signed-off-by: Antes <antes@simplemachines.org>

--- Themes/default/Profile.template.php
+++ Themes/default/Profile.template.php
@@ -82,12 +82,12 @@ function template_alerts_popup()
 	// Unlike almost every other template, this is designed to be included into the HTML directly via $().load()
 	echo '
 		<div class="alert_bar">
-			<div class="alerts_opts floatright">
+			<div class="alerts_opts block">
 				<a href="' . $scripturl . '?action=profile;area=notification;sa=markread;', $context['session_var'], '=', $context['session_id'], '" onclick="return markAlertsRead(this)">', $txt['mark_alerts_read'], '</a>
-				| <a href="', $scripturl, '?action=profile;area=notification;sa=alerts">', $txt['alert_settings'], '</a>
+				<a href="', $scripturl, '?action=profile;area=notification;sa=alerts" class="floatright">', $txt['alert_settings'], '</a>
 			</div>
-			<div class="alerts_box floatleft">
-				<a href="', $scripturl, '?action=profile;area=showalerts">', $txt['all_alerts'], '</a>
+			<div class="alerts_box centertext">
+				<a href="', $scripturl, '?action=profile;area=showalerts" class="button">', $txt['all_alerts'], '</a>
 			</div>
 		</div>
 		<div class="alerts_unread">';
