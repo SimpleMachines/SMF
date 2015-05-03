SimpleMachines/SMF

commit 726757a5a3e420684aa6c78f2e4a7cf70bf90ebf

- Remove OpenID support
Signed-off-by: Michael Eshom <oldiesmann@oldiesmann.us>

Themes/default/languages/Profile.english.php
@@ -79,13 +79,8 @@ $txt['secret_why_blank'] = 'why is this blank?';
 
 $txt['authentication_reminder'] = 'Authentication Reminder';
 $txt['password_reminder_desc'] = 'If you\'ve forgotten your login details, don\'t worry, they can be retrieved. To start this process please enter your username or email address below.';
-$txt['authentication_options'] = 'Please select one of the two options below';
-$txt['authentication_openid_email'] = 'Email me a reminder of my OpenID identity';
-$txt['authentication_openid_secret'] = 'Answer my &quot;secret question&quot; to display my OpenID identity';
 $txt['authentication_password_email'] = 'Email me a new password';
 $txt['authentication_password_secret'] = 'Let me set a new password by answering my &quot;secret question&quot;';
-$txt['openid_secret_reminder'] = 'Please enter your answer to the question below. If you get it correct your OpenID identity will be shown.';
-$txt['reminder_openid_is'] = 'The OpenID identity associated with your account is:<br>&nbsp;&nbsp;&nbsp;&nbsp;<strong>%1$s</strong><br><br>Please make a note of this for future reference.';
 $txt['reminder_continue'] = 'Continue';
 
 $txt['current_theme'] = 'Current Theme';
@@ -282,7 +277,6 @@ $txt['user_email'] = 'Username/Email';
 $txt['reminder_subject'] = 'New password for ' . $context['forum_name'];
 $txt['reminder_mail'] = 'This mail was sent because the \'forgot password\' function has been applied to your account. To set a new password, click the following link';
 $txt['reminder_sent'] = 'A mail has been sent to your email address. Click the link in that mail to set a new password.';
-$txt['reminder_openid_sent'] = 'Your current OpenID identity has been sent to your email address.';
 $txt['reminder_set_password'] = 'Set Password';
 $txt['reminder_password_set'] = 'Password successfully set';
 $txt['reminder_error'] = '%1$s failed to answer their secret question correctly when attempting to change a forgotten password.';

SSI.php
@@ -1056,18 +1056,8 @@ function ssi_login($redirect_to = '', $output_method = 'echo')
 				</tr><tr>
 					<td style="text-align: right; border-spacing: 1"><label for="passwrd">', $txt['password'], ':</label>&nbsp;</td>
 					<td><input type="password" name="passwrd" id="passwrd" size="9" class="input_password"></td>
-				</tr>';
-
-	// Open ID?
-	if (!empty($modSettings['enableOpenID']))
-		echo '<tr>
-					<td colspan="2" style="text-align :center"><strong>&mdash;', $txt['or'], '&mdash;</strong></td>
-				</tr><tr>
-					<td style="text-align: right"><label for="openid_url">', $txt['openid'], ':</label>&nbsp;</td>
-					<td><input type="text" name="openid_identifier" id="openid_url" class="input_text openid_login" size="17"></td>
-				</tr>';
-
-	echo '<tr>
+				</tr>
+				<tr>
 					<td><input type="hidden" name="cookielength" value="-1"></td>
 					<td><input type="submit" value="', $txt['login'], '" class="button_submit"></td>
 				</tr>

Sources/Load.php
@@ -1040,7 +1040,7 @@ function loadMemberData($users, $is_name = false, $set = 'normal')
 			$select_columns .= ', mem.buddy_list,  mem.additional_groups';
 			break;
 		case 'profile':
-			$select_columns .= ', mem.additional_groups, mem.openid_uri, mem.id_theme, mem.pm_ignore_list, mem.pm_email_notify, mem.pm_receive_from,
+			$select_columns .= ', mem.additional_groups, mem.id_theme, mem.pm_ignore_list, mem.pm_email_notify, mem.pm_receive_from,
 			mem.time_format, mem.timezone, mem.secret_question, mem.smiley_set,
 			mem.total_time_logged_in, mem.notify_announcements, mem.notify_regularity, mem.notify_send_body,
 			mem.notify_types, lo.url, mem.ignore_boards, mem.password_salt, mem.pm_prefs, mem.buddy_list, mem.alerts';

Sources/LogInOut.php
@@ -121,11 +121,6 @@ function Login2()
 			fatal_lang_error('login_cookie_error', false);
 
 		$user_info['can_mod'] = allowedTo('access_mod_center') || (!$user_info['is_guest'] && ($user_info['mod_cache']['gq'] != '0=1' || $user_info['mod_cache']['bq'] != '0=1' || ($modSettings['postmod_active'] && !empty($user_info['mod_cache']['ap']))));
-		if ($user_info['can_mod'] && isset($user_settings['openid_uri']) && empty($user_settings['openid_uri']))
-		{
-			$_SESSION['moderate_time'] = time();
-			unset($_SESSION['just_registered']);
-		}
 
 		// Some whitelisting for login_url...
 		if (empty($_SESSION['login_url']))
@@ -191,13 +186,6 @@ function Login2()
 		'name' => $txt['login'],
 	);
 
-	if (!empty($_POST['openid_identifier']) && !empty($modSettings['enableOpenID']))
-	{
-		require_once($sourcedir . '/Subs-OpenID.php');
-		if (($open_id = smf_openID_validate($_POST['openid_identifier'])) !== 'no_data')
-			return $open_id;
-	}
-
 	// You forgot to type your username, dummy!
 	if (!isset($_POST['user']) || $_POST['user'] == '')
 	{
@@ -229,7 +217,7 @@ function Login2()
 	// Load the data up!
 	$request = $smcFunc['db_query']('', '
 		SELECT passwd, id_member, id_group, lngfile, is_activated, email_address, additional_groups, member_name, password_salt,
-			openid_uri, passwd_flood
+			passwd_flood
 		FROM {db_prefix}members
 		WHERE ' . ($smcFunc['db_case_sensitive'] ? 'LOWER(member_name) = LOWER({string:user_name})' : 'member_name = {string:user_name}') . '
 		LIMIT 1',
@@ -243,7 +231,7 @@ function Login2()
 		$smcFunc['db_free_result']($request);
 
 		$request = $smcFunc['db_query']('', '
-			SELECT passwd, id_member, id_group, lngfile, is_activated, email_address, additional_groups, member_name, password_salt, openid_uri,
+			SELECT passwd, id_member, id_group, lngfile, is_activated, email_address, additional_groups, member_name, password_salt,
 			passwd_flood
 			FROM {db_prefix}members
 			WHERE email_address = {string:user_name}
@@ -478,13 +466,6 @@ function DoLogin()
 	// Are you banned?
 	is_not_banned(true);
 
-	// An administrator, set up the login so they don't have to type it again.
-	if ($user_info['is_admin'] && isset($user_settings['openid_uri']) && empty($user_settings['openid_uri']))
-	{
-		$_SESSION['admin_time'] = time();
-		unset($_SESSION['just_registered']);
-	}
-
 	// Don't stick the language or theme after this point.
 	unset($_SESSION['language'], $_SESSION['id_theme']);
 
@@ -560,10 +541,6 @@ function Logout($internal = false, $redirect = true)
 
 	if (isset($_SESSION['pack_ftp']))
 		$_SESSION['pack_ftp'] = null;
-
-	// They cannot be open ID verified any longer.
-	if (isset($_SESSION['openid']))
-		unset($_SESSION['openid']);
 
 	// It won't be first login anymore.
 	unset($_SESSION['first_login']);

Sources/ManageRegistration.php
@@ -298,7 +298,6 @@ function ModifyRegistrationSettings($return_config = false)
 
 	$config_vars = array(
 			array('select', 'registration_method', array($txt['setting_registration_standard'], $txt['setting_registration_activate'], $txt['setting_registration_approval'], $txt['setting_registration_disabled'])),
-			array('select', 'enableOpenID', array(0 => $txt['enableOpenID_disabled'], 1 => $txt['enableOpenID_enabled'], 2 => $txt['enableOpenID_enabled_login'])),
 			array('check', 'send_welcomeEmail'),
 		'',
 			array('int', 'coppaAge', 'subtext' => $txt['setting_coppaAge_desc'], 'onchange' => 'checkCoppa();', 'onkeyup' => 'checkCoppa();'),

Sources/Profile-Modify.php
@@ -303,7 +303,6 @@ function loadProfileFields($force_reload = false)
 			'subtext' => $txt['password_strength'],
 			'size' => 20,
 			'value' => '',
-			'enabled' => empty($cur_profile['openid_uri']),
 			'permission' => 'profile_password',
 			'save_key' => 'passwd',
 			// Note this will only work if passwrd2 also exists!
@@ -334,7 +333,6 @@ function loadProfileFields($force_reload = false)
 		'passwrd2' => array(
 			'type' => 'password',
 			'label' => ucwords($txt['verify_pass']),
-			'enabled' => empty($cur_profile['openid_uri']),
 			'size' => 20,
 			'value' => '',
 			'permission' => 'profile_password',
@@ -1772,94 +1770,6 @@ function theme($memID)
 			'theme_settings',
 		)
 	);
-}
-
-/**
- * Changing authentication method? Only appropriate for people using OpenID.
- *
- * @param int $memID id_member
- * @param bool $saving = false
- */
-function authentication($memID, $saving = false)
-{
-	global $context, $cur_profile, $sourcedir, $post_errors, $modSettings;
-
-	loadLanguage('Login');
-
-	// We are saving?
-	if ($saving)
-	{
-		// Moving to password passed authentication?
-		if ($_POST['authenticate'] == 'passwd')
-		{
-			// Didn't enter anything?
-			if ($_POST['passwrd1'] == '')
-				$post_errors[] = 'no_password';
-			// Do the two entries for the password even match?
-			elseif (!isset($_POST['passwrd2']) || $_POST['passwrd1'] != $_POST['passwrd2'])
-				$post_errors[] = 'bad_new_password';
-			// Is it valid?
-			else
-			{
-				require_once($sourcedir . '/Subs-Auth.php');
-				$passwordErrors = validatePassword($_POST['passwrd1'], $cur_profile['member_name'], array($cur_profile['real_name'], $cur_profile['email_address']));
-
-				// Were there errors?
-				if ($passwordErrors != null)
-					$post_errors[] = 'password_' . $passwordErrors;
-			}
-
-			if (empty($post_errors))
-			{
-				// Integration?
-				call_integration_hook('integrate_reset_pass', array($cur_profile['member_name'], $cur_profile['member_name'], $_POST['passwrd1']));
-
-				// Go then.
-				$passwd = hash_password($cur_profile['member_name'], un_htmlspecialchars($_POST['passwrd1']));
-
-				// Do the important bits.
-				updateMemberData($memID, array('openid_uri' => '', 'passwd' => $passwd));
-				$cur_profile['passwd'] = $passwd;
-
-				if ($context['user']['is_owner'])
-				{
-					setLoginCookie(60 * $modSettings['cookieTime'], $memID, hash_salt($passwd, $cur_profile['password_salt']));
-					redirectexit('action=profile;area=authentication;updated');
-				}
-				else
-					redirectexit('action=profile;u=' . $memID);
-			}
-
-			return true;
-		}
-		// Not right yet!
-		elseif ($_POST['authenticate'] == 'openid' && !empty($_POST['openid_identifier']))
-		{
-			require_once($sourcedir . '/Subs-OpenID.php');
-			$_POST['openid_identifier'] = smf_openID_canonize($_POST['openid_identifier']);
-
-			if (smf_openid_member_exists($_POST['openid_identifier']))
-				$post_errors[] = 'openid_in_use';
-			elseif (empty($post_errors))
-			{
-				// Authenticate using the new OpenID URI first to make sure they didn't make a mistake.
-				if ($context['user']['is_owner'])
-				{
-					$_SESSION['new_openid_uri'] = $_POST['openid_identifier'];
-
-					smf_openID_validate($_POST['openid_identifier'], false, null, 'change_uri');
-				}
-				else
-					updateMemberData($memID, array('openid_uri' => $_POST['openid_identifier']));
-			}
-		}
-	}
-
-	// Some stuff.
-	$context['member']['openid_uri'] = $cur_profile['openid_uri'];
-	$context['auth_method'] = empty($cur_profile['openid_uri']) ? 'password' : 'openid';
-	$context['sub_template'] = 'authentication_method';
-	loadJavascriptFile('register.js', array('default_theme' => true, 'defer' => false), 'smf_register');
 }
 
 /**

Sources/Profile.php
@@ -249,21 +249,6 @@ function ModifyProfile($post_errors = array())
 						'any' => array('profile_extra_any'),
 					),
 				),
-				'authentication' => array(
-					'label' => $txt['authentication'],
-					'file' => 'Profile-Modify.php',
-					'function' => 'authentication',
-					'icon' => 'openid',
-					'enabled' => !empty($modSettings['enableOpenID']) && ($modSettings['enableOpenID'] == 1 || !empty($cur_profile['openid_uri'])),
-					'sc' => 'post',
-					'token' => 'profile-au%u',
-					'hidden' => empty($modSettings['enableOpenID']) && empty($cur_profile['openid_uri']),
-					'password' => true,
-					'permission' => array(
-						'own' => array('profile_password_any', 'profile_password_own'),
-						'any' => array('profile_password_any'),
-					),
-				),
 				'notification' => array(
 					'label' => $txt['notification'],
 					'file' => 'Profile-Modify.php',
@@ -428,7 +413,7 @@ function ModifyProfile($post_errors = array())
 			else
 				$profile_areas[$section_id]['areas'][$area_id]['permission'] = $area['permission'][$context['user']['is_owner'] ? 'own' : 'any'];
 
-			// Password required - only if not on OpenID.
+			// Password required in most cases
 			if (!empty($area['password']))
 				$context['password_areas'][] = $area_id;
 		}
@@ -571,7 +556,7 @@ function ModifyProfile($post_errors = array())
 
 	// All the subactions that require a user password in order to validate.
 	$check_password = $context['user']['is_owner'] && in_array($profile_include_data['current_area'], $context['password_areas']);
-	$context['require_password'] = $check_password && empty($user_settings['openid_uri']);
+	$context['require_password'] = $check_password;
 
 	// If we're in wireless then we have a cut down template...
 	if (WIRELESS && $context['sub_template'] == 'summary' && WIRELESS_PROTOCOL != 'wap')
@@ -593,32 +578,23 @@ function ModifyProfile($post_errors = array())
 
 		if ($check_password)
 		{
-			// If we're using OpenID try to revalidate.
-			if (!empty($user_settings['openid_uri']))
-			{
-				require_once($sourcedir . '/Subs-OpenID.php');
-				smf_openID_revalidate();
-			}
-			else
-			{
-				// You didn't even enter a password!
-				if (trim($_POST['oldpasswrd']) == '')
-					$post_errors[] = 'no_password';
+			// You didn't even enter a password!
+			if (trim($_POST['oldpasswrd']) == '')
+				$post_errors[] = 'no_password';
 
-				// Since the password got modified due to all the $_POST cleaning, lets undo it so we can get the correct password
-				$_POST['oldpasswrd'] = un_htmlspecialchars($_POST['oldpasswrd']);
+			// Since the password got modified due to all the $_POST cleaning, lets undo it so we can get the correct password
+			$_POST['oldpasswrd'] = un_htmlspecialchars($_POST['oldpasswrd']);
 
-				// Does the integration want to check passwords?
-				$good_password = in_array(true, call_integration_hook('integrate_verify_password', array($cur_profile['member_name'], $_POST['oldpasswrd'], false)), true);
+			// Does the integration want to check passwords?
+			$good_password = in_array(true, call_integration_hook('integrate_verify_password', array($cur_profile['member_name'], $_POST['oldpasswrd'], false)), true);
 
-				// Bad password!!!
-				if (!$good_password && !hash_verify_password($user_profile[$memID]['member_name'], un_htmlspecialchars(stripslashes($_POST['oldpasswrd'])), $user_info['passwd']))
-					$post_errors[] = 'bad_password';
+			// Bad password!!!
+			if (!$good_password && !hash_verify_password($user_profile[$memID]['member_name'], un_htmlspecialchars(stripslashes($_POST['oldpasswrd'])), $user_info['passwd']))
+				$post_errors[] = 'bad_password';
 
-				// Warn other elements not to jump the gun and do custom changes!
-				if (in_array('bad_password', $post_errors))
-					$context['password_auth_failed'] = true;
-			}
+			// Warn other elements not to jump the gun and do custom changes!
+			if (in_array('bad_password', $post_errors))
+				$context['password_auth_failed'] = true;
 		}
 
 		// Change the IP address in the database.

Sources/Register.php
@@ -189,22 +189,11 @@ function Register($reg_errors = array())
 	else
 		$context['visual_verification'] = false;
 
-	// Are they coming from an OpenID login attempt?
-	if (!empty($_SESSION['openid']['verified']) && !empty($_SESSION['openid']['openid_uri']))
-	{
-		$context['openid'] = $_SESSION['openid']['openid_uri'];
-		$context['username'] = $smcFunc['htmlspecialchars'](!empty($_POST['user']) ? $_POST['user'] : $_SESSION['openid']['nickname']);
-		$context['email'] = $smcFunc['htmlspecialchars'](!empty($_POST['email']) ? $_POST['email'] : $_SESSION['openid']['email']);
-	}
-	// See whether we have some pre-filled values.
-	else
-	{
-		$context += array(
-			'openid' => isset($_POST['openid_identifier']) ? $_POST['openid_identifier'] : '',
-			'username' => isset($_POST['user']) ? $smcFunc['htmlspecialchars']($_POST['user']) : '',
-			'email' => isset($_POST['email']) ? $smcFunc['htmlspecialchars']($_POST['email']) : '',
-		);
-	}
+
+	$context += array(
+		'username' => isset($_POST['user']) ? $smcFunc['htmlspecialchars']($_POST['user']) : '',
+		'email' => isset($_POST['email']) ? $smcFunc['htmlspecialchars']($_POST['email']) : '',
+	);
 
 	// Were there any errors?
 	$context['registration_errors'] = array();
@@ -216,10 +205,8 @@ function Register($reg_errors = array())
 
 /**
  * Actually register the member.
- *
- * @param bool $verifiedOpenID = false
  */
-function Register2($verifiedOpenID = false)
+function Register2()
 {
 	global $txt, $modSettings, $context, $sourcedir;
 	global $smcFunc, $maintenance;
@@ -234,63 +221,52 @@ function Register2($verifiedOpenID = false)
 	// Start collecting together any errors.
 	$reg_errors = array();
 
-	// Did we save some open ID fields?
-	if ($verifiedOpenID && !empty($context['openid_save_fields']))
-	{
-		foreach ($context['openid_save_fields'] as $id => $value)
-			$_POST[$id] = $value;
-	}
-
 	// You can't register if it's disabled.
 	if (!empty($modSettings['registration_method']) && $modSettings['registration_method'] == 3)
 		fatal_lang_error('registration_disabled', false);
 
-	// Things we don't do for people who have already confirmed their OpenID allegances via register.
-	if (!$verifiedOpenID)
+	// Well, if you don't agree, you can't register.
+	if (!empty($modSettings['requireAgreement']) && empty($_SESSION['registration_agreed']))
+		redirectexit();
+
+	// Make sure they came from *somewhere*, have a session.
+	if (!isset($_SESSION['old_url']))
+		redirectexit('action=signup');
+
+	// If we don't require an agreement, we need a extra check for coppa.
+	if (empty($modSettings['requireAgreement']) && !empty($modSettings['coppaAge']))
+		$_SESSION['skip_coppa'] = !empty($_POST['accept_agreement']);
+	// Are they under age, and under age users are banned?
+	if (!empty($modSettings['coppaAge']) && empty($modSettings['coppaType']) && empty($_SESSION['skip_coppa']))
 	{
-		// Well, if you don't agree, you can't register.
-		if (!empty($modSettings['requireAgreement']) && empty($_SESSION['registration_agreed']))
-			redirectexit();
+		loadLanguage('Errors');
+		fatal_lang_error('under_age_registration_prohibited', false, array($modSettings['coppaAge']));
+	}
 
-		// Make sure they came from *somewhere*, have a session.
-		if (!isset($_SESSION['old_url']))
-			redirectexit('action=signup');
+	// Check the time gate for miscreants. First make sure they came from somewhere that actually set it up.
+	if (empty($_SESSION['register']['timenow']) || empty($_SESSION['register']['limit']))
+		redirectexit('action=signup');
+	// Failing that, check the time on it.
+	if (time() - $_SESSION['register']['timenow'] < $_SESSION['register']['limit'])
+	{
+		loadLanguage('Errors');
+		$reg_errors[] = $txt['error_too_quickly'];
+	}
 
-		// If we don't require an agreement, we need a extra check for coppa.
-		if (empty($modSettings['requireAgreement']) && !empty($modSettings['coppaAge']))
-			$_SESSION['skip_coppa'] = !empty($_POST['accept_agreement']);
-		// Are they under age, and under age users are banned?
-		if (!empty($modSettings['coppaAge']) && empty($modSettings['coppaType']) && empty($_SESSION['skip_coppa']))
+	// Check whether the visual verification code was entered correctly.
+	if (!empty($modSettings['reg_verification']))
+	{
+		require_once($sourcedir . '/Subs-Editor.php');
+		$verificationOptions = array(
+			'id' => 'register',
+		);
+		$context['visual_verification'] = create_control_verification($verificationOptions, true);
+
+		if (is_array($context['visual_verification']))
 		{
 			loadLanguage('Errors');
-			fatal_lang_error('under_age_registration_prohibited', false, array($modSettings['coppaAge']));
-		}
-
-		// Check the time gate for miscreants. First make sure they came from somewhere that actually set it up.
-		if (empty($_SESSION['register']['timenow']) || empty($_SESSION['register']['limit']))
-			redirectexit('action=signup');
-		// Failing that, check the time on it.
-		if (time() - $_SESSION['register']['timenow'] < $_SESSION['register']['limit'])
-		{
-			loadLanguage('Errors');
-			$reg_errors[] = $txt['error_too_quickly'];
-		}
-
-		// Check whether the visual verification code was entered correctly.
-		if (!empty($modSettings['reg_verification']))
-		{
-			require_once($sourcedir . '/Subs-Editor.php');
-			$verificationOptions = array(
-				'id' => 'register',
-			);
-			$context['visual_verification'] = create_control_verification($verificationOptions, true);
-
-			if (is_array($context['visual_verification']))
-			{
-				loadLanguage('Errors');
-				foreach ($context['visual_verification'] as $error)
-					$reg_errors[] = $txt['error_' . $error];
-			}
+			foreach ($context['visual_verification'] as $error)
+				$reg_errors[] = $txt['error_' . $error];
 		}
 	}
 
@@ -378,8 +354,6 @@ function Register2($verifiedOpenID = false)
 		'email' => !empty($_POST['email']) ? $_POST['email'] : '',
 		'password' => !empty($_POST['passwrd1']) ? $_POST['passwrd1'] : '',
 		'password_check' => !empty($_POST['passwrd2']) ? $_POST['passwrd2'] : '',
-		'openid' => !empty($_POST['openid_identifier']) ? $_POST['openid_identifier'] : '',
-		'auth_method' => !empty($_POST['authenticate']) ? $_POST['authenticate'] : '',
 		'check_reserved_name' => true,
 		'check_password_strength' => true,
 		'check_email_ban' => true,
@@ -474,26 +448,6 @@ function Register2($verifiedOpenID = false)
 		$_REQUEST['step'] = 2;
 		$_SESSION['register']['limit'] = 5; // If they've filled in some details, they won't need the full 10 seconds of the limit.
 		return Register($reg_errors);
-	}
-	// If they're wanting to use OpenID we need to validate them first.
-	if (empty($_SESSION['openid']['verified']) && !empty($_POST['authenticate']) && $_POST['authenticate'] == 'openid')
-	{
-		// What do we need to save?
-		$save_variables = array();
-		foreach ($_POST as $k => $v)
-			if (!in_array($k, array('sc', 'sesc', $context['session_var'], 'passwrd1', 'passwrd2', 'regSubmit')))
-				$save_variables[$k] = $v;
-
-		require_once($sourcedir . '/Subs-OpenID.php');
-		smf_openID_validate($_POST['openid_identifier'], false, $save_variables);
-	}
-	// If we've come from OpenID set up some default stuff.
-	elseif ($verifiedOpenID || (!empty($_POST['openid_identifier']) && $_POST['authenticate'] == 'openid'))
-	{
-		$regOptions['username'] = !empty($_POST['user']) && trim($_POST['user']) != '' ? $_POST['user'] : $_SESSION['openid']['nickname'];
-		$regOptions['email'] = !empty($_POST['email']) && trim($_POST['email']) != '' ? $_POST['email'] : $_SESSION['openid']['email'];
-		$regOptions['auth_method'] = 'openid';
-		$regOptions['openid'] = !empty($_POST['openid_identifier']) ? $_POST['openid_identifier'] : $_SESSION['openid']['openid_uri'];
 	}
 
 	$memberID = registerMember($regOptions, true);

commit 8ff53cb3cc4d2b2e752377409e41a2330e9a2385

Fixes bug  introduced in 726757a5a3e420684aa6c78f2e4a7cf70bf90ebf
Fix $2848

Signed-off-by: John Rayes <live627@gmail.com>

Themes/default/languages/Profile.english.php
@@ -79,6 +79,7 @@ $txt['secret_why_blank'] = 'why is this blank?';
 
 $txt['authentication_reminder'] = 'Authentication Reminder';
 $txt['password_reminder_desc'] = 'If you\'ve forgotten your login details, don\'t worry, they can be retrieved. To start this process please enter your username or email address below.';
+$txt['authentication_options'] = 'Please select one of the two options below';
 $txt['authentication_password_email'] = 'Email me a new password';
 $txt['authentication_password_secret'] = 'Let me set a new password by answering my &quot;secret question&quot;';
 $txt['reminder_continue'] = 'Continue';