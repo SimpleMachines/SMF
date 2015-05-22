5232f8740564a47365b36bb90569baa2c91be890
Suki <suki@missallsunday.com>
2015-05-21 20:21:11 -0500

    Don't rely on mod authors to do anything... since we are already selecting columns, we need to add an , before imploding any new values
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/Display.php
+++ Sources/Display.php
@@ -161,7 +161,7 @@ function Display()
 			IFNULL(mem.real_name, ms.poster_name) AS topic_started_name, ms.poster_time AS topic_started_time,
 			' . ($user_info['is_guest'] ? 't.id_last_msg + 1' : 'IFNULL(lt.id_msg, IFNULL(lmr.id_msg, -1)) + 1') . ' AS new_from
 			' . (!empty($board_info['recycle']) ? ', id_previous_board, id_previous_topic' : '') . '
-			' . (!empty($topic_selects) ? implode(',', $topic_selects) : '') . '
+			' . (!empty($topic_selects) ? (', '. implode(', ', $topic_selects)) : '') . '
 			' . (!$user_info['is_guest'] ? ', IFNULL(lt.unwatched, 0) as unwatched' : '') . '
 		FROM {db_prefix}topics AS t
 			INNER JOIN {db_prefix}messages AS ms ON (ms.id_msg = t.id_first_msg)
