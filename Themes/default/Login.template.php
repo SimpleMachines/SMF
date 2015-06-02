438f10086b27b66bffc75807fd7ad4a91109a259
Shitiz Garg <mail@dragooon.net>
2015-06-03 02:39:09 +0530

    Slight HTML cleanup for 2FA
    Signed-off-by: Shitiz Garg <mail@dragooon.net>

--- Themes/default/Login.template.php
+++ Themes/default/Login.template.php
@@ -129,7 +129,7 @@ function template_login_tfa()
 							<input type="text" class="input_text" name="tfa_code" style="width: 150px;" value="', !empty($context['tfa_value']) ? $context['tfa_value'] : '', '">
 							<input type="submit" class="button_submit" name="submit" value="', $txt['login'], '" style="float: none; margin: 0;"><br />
 						</div><br />
-						<div><input type="checkbox" value="1" name="tfa_preserve" />&nbsp;', $txt['tfa_preserve'], '</div>
+						<div><input type="checkbox" value="1" name="tfa_preserve" id="tfa_preserve"/><label for="tfa_preserve">&nbsp;', $txt['tfa_preserve'], '</label></div>
 						<hr />
 						<input type="button" class="button_submit" name="backup" value="', $txt['tfa_backup'], '" style="float: none; margin: 0;">
 					</div>
