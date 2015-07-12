34c931188cf692a39b76faa47f824ecca9dcd1a6
John Rayes <live627@gmail.com>
2015-07-11 17:54:34 -0700

    Fix #2806
    Signed-off-by: John Rayes <live627@gmail.com>

--- Themes/default/languages/EmailTemplates.english.php
+++ Themes/default/languages/EmailTemplates.english.php
@@ -326,6 +326,21 @@ You can approve or reject this post by clicking the link below:
 {REGARDS}';
 
 /**
+	@additional_params: alert_unapproved_topic
+		SUBJECT: The subject of the topic causing the notification
+		LINK: A link to the topic.
+	@description:
+*/
+$txt['alert_unapproved_topic_subject'] = 'New Unapproved Topic: {SUBJECT}';
+$txt['alert_unapproved_topic_body'] = 'A new topic, \'{SUBJECT}\', has been made which needs approved.
+
+You can approve or reject this topic by clicking the link below:
+
+{LINK}
+
+{REGARDS}';
+
+/**
 	@additional_params: request_membership
 		RECPNAME: The name of the person recieving the email
 		APPYNAME: The name of the person applying for group membership
