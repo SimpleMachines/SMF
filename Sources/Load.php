e0df5aff8f4babdb8586d77a76fcb8a26d4da856
John Rayes <live627@gmail.com>
2015-08-19 01:22:49 -0700

    remove these tags  from description_allowed_tags
    Signed-off-by: John Rayes <live627@gmail.com>

--- Sources/Load.php
+++ Sources/Load.php
@@ -307,7 +307,7 @@ function reloadSettings()
 	);
 
 	// Define a list of allowed tags for descriptions.
-	$context['description_allowed_tags'] = array('abbr', 'acronym', 'anchor', 'bdo', 'br', 'center', 'color', 'font', 'hr', 'i', 'iurl', 'left', 'li', 'list', 'ltr', 'move', 'pre', 'right', 's', 'sub', 'sup', 'table', 'td', 'tr', 'u', 'url',);
+	$context['description_allowed_tags'] = array('abbr', 'anchor', 'center', 'color', 'font', 'hr', 'i', 'iurl', 'left', 'li', 'list', 'ltr', 'pre', 'right', 's', 'sub', 'sup', 'table', 'td', 'tr', 'u', 'url',);
 
 	// Get an error count, if necessary
 	if (!isset($context['num_errors']))
