c2cb52711b458101146bb9d11f92091beb26778a
albertlast <mathiaspapealbert@hotmail.com>
2016-01-04 12:10:13 +0100

    add floodcontrol table
    Signed-off-by: albertlast  <albertlast@hotmail.de>

--- other/install_2-1_postgresql.sql
+++ other/install_2-1_postgresql.sql
@@ -638,7 +638,7 @@ CREATE INDEX {$db_prefix}log_errors_ip ON {$db_prefix}log_errors (ip);
 # Table structure for table `log_floodcontrol`
 #
 
-CREATE TABLE {$db_prefix}log_floodcontrol (
+CREATE {$unlogged} TABLE {$db_prefix}log_floodcontrol (
   ip char(16) NOT NULL default '                ',
   log_time int NOT NULL default '0',
   log_type varchar(8) NOT NULL default 'post',
