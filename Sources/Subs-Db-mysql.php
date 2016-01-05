1d0a9220d471383fd8545ad4a2a9a98f678614b7
albertlast <mathiaspapealbert@hotmail.com>
2016-01-05 11:41:07 +0100

    deactivate check for exists values
    Signed-off-by: albertlast  <albertlast@hotmail.de>

--- Sources/Subs-Db-mysql.php
+++ Sources/Subs-Db-mysql.php
@@ -153,8 +153,9 @@ function smf_db_replacement__callback($matches)
 	if ($matches[1] === 'literal')
 		return '\'' . mysql_real_escape_string($matches[2], $connection) . '\'';
 
-	if (!isset($values[$matches[2]]))
-		smf_db_error_backtrace('The database value you\'re trying to insert does not exist: ' . (isset($smcFunc['htmlspecialchars']) ? $smcFunc['htmlspecialchars']($matches[2]) : htmlspecialchars($matches[2])), '', E_USER_ERROR, __FILE__, __LINE__);
+	//deactivate to allow null values
+//	if (!isset($values[$matches[2]]))
+//		smf_db_error_backtrace('The database value you\'re trying to insert does not exist: ' . (isset($smcFunc['htmlspecialchars']) ? $smcFunc['htmlspecialchars']($matches[2]) : htmlspecialchars($matches[2])), '', E_USER_ERROR, __FILE__, __LINE__);
 
 	$replacement = $values[$matches[2]];
 
--- Sources/Subs-Db-postgresql.php
+++ Sources/Subs-Db-postgresql.php
@@ -141,8 +141,9 @@ function smf_db_replacement__callback($matches)
 	if ($matches[1] === 'literal')
 		return '\'' . pg_escape_string($matches[2]) . '\'';
 
-	if (!isset($values[$matches[2]]))
-		smf_db_error_backtrace('The database value you\'re trying to insert does not exist: ' . (isset($smcFunc['htmlspecialchars']) ? $smcFunc['htmlspecialchars']($matches[2]) : htmlspecialchars($matches[2])), '', E_USER_ERROR, __FILE__, __LINE__);
+	//deactivate to allow null values
+//	if (!isset($values[$matches[2]]))
+//		smf_db_error_backtrace('The database value you\'re trying to insert does not exist: ' . (isset($smcFunc['htmlspecialchars']) ? $smcFunc['htmlspecialchars']($matches[2]) : htmlspecialchars($matches[2])), '', E_USER_ERROR, __FILE__, __LINE__);
 
 	$replacement = $values[$matches[2]];
 
