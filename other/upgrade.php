26064ffa28d4550b7820dddb3e0cdfa48e70eb83
albertlast <mathiaspapealbert@hotmail.com>
2016-04-10 06:48:18 +0200

    Fix Function string error, sql syntax in update, array count function
    Signed-off-by: albertlast  <albertlast@hotmail.de>

--- other/upgrade.php
+++ other/upgrade.php
@@ -5768,6 +5768,8 @@ function template_upgrade_complete()
  */
 function MySQLConvertOldIp($targetTable,$oldCol,$newCol)
 {
+	global $smcFunc;
+	
 	//mysql default max length is 1mb http://dev.mysql.com/doc/refman/5.1/en/packet-too-large.html
 	$max = 10000;
 	$arIp = array();
@@ -5780,16 +5782,16 @@ function MySQLConvertOldIp($targetTable,$oldCol,$newCol)
     ENGINE = MEMORY');
 	$smcFunc['db_free_result']($request);
 	
-	$request = $smcFunc['db_query']('', 'select distinct '.$oldCol.' from {db_prefix}'.$targetTable);
+	$request = $smcFunc['db_query']('', 'SELECT DISTINCT '.$oldCol.' FROM {db_prefix}'.$targetTable);
 	while($row = $smcFunc['db_fetch_assoc']($request))
 		$arIp[] = $row[$oldCol]; 
 	$smcFunc['db_free_result']($request);
 	
-	$insertStart = 'insert into {db_prefix}ip_table(oldip, newip) values';
+	$insertStart = 'INSERT INTO {db_prefix}ip_table(oldip, newip) VALUES';
 	$query = $insertStart;
 	$impArray = array();
 	$x = 0;
-	for($i = 0; $i < length($arIp); $i++)
+	for($i = 0; $i < count($arIp); $i++)
 	{
 		$query .= $x > 0 ? ',':'';
 		$query .= '({string:ip'.$x.'},{inet:ip'.$x.'})';
@@ -5808,9 +5810,9 @@ function MySQLConvertOldIp($targetTable,$oldCol,$newCol)
 	$request = $smcFunc['db_query']('', $query, $impArray);
 	$smcFunc['db_free_result']($request);
 	
-	$request = $smcFunc['db_query']('', 'update table {db_prefix}'.$targetTable.' a
-	join {db_prefix}ip_table b on a.'.$oldCol.' = b.oldip
-	set a.'.$newCol.' = b.newip');
+	$request = $smcFunc['db_query']('', 'UPDATE {db_prefix}'.$targetTable.' a
+	JOIN {db_prefix}ip_table b ON a.'.$oldCol.' = b.oldip
+	SET a.'.$newCol.' = b.newip');
 	$smcFunc['db_free_result']($request);
 	
 	$request = $smcFunc['db_query']('', 'DROP TABLE IF EXISTS {db_prefix}ip_table');
