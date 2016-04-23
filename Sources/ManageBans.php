8277d0b9b62e9d7a17cb6ddb0d7eee1ce9e4ce48
FearThe1337 <git@fearthe1337.com>
2016-04-23 17:00:56 +0200

    Fix IP display bug that affected ~3% of the ips + 1 minor typo.
    Due to the data being binary a dot or colon can occur in the binary data. Respectively the bug triggers in an IP address containing 46 and 58.
    
    Signed-off-by: FearThe1337 <git@fearthe1337.com>

--- Sources/ManageBans.php
+++ Sources/ManageBans.php
@@ -2155,7 +2155,7 @@ function range2ip($low, $high)
 	$low = inet_dtop($low);
 	$high = inet_dtop($high);
 
-	if ($low == '255.255.255.255') return 'unkown';
+	if ($low == '255.255.255.255') return 'unknown';
 	if ($low == $high)
 	    return $low;
 	else
