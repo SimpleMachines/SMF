ac1aa245df450ad641b6fb6f63a879dd42bd3fb6
Jon Stovell <jonstovell@gmail.com>
2016-07-30 20:43:59 -0600

    ... And change the code to refer to pikaday.min.js
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

--- Sources/Post.php
+++ Sources/Post.php
@@ -320,7 +320,7 @@ function Post($post_errors = array())
 
 		loadCSSFile('pikaday.css', array('defer' => false), 'smf_datepicker');
 		loadCSSFile('jquery.timepicker.css', array('defer' => false), 'smf_timepicker');
-		loadJavascriptFile('pikaday.js', array('defer' => true), 'smf_datepicker');
+		loadJavascriptFile('pikaday.min.js', array('defer' => true), 'smf_datepicker');
 		loadJavascriptFile('jquery.timepicker.min.js', array('defer' => true), 'smf_timepicker');
 		loadJavascriptFile('datepair.min.js', array('defer' => true), 'smf_datepair');
 		addInlineJavascript('
