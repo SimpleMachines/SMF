a0f8f30f560d8dda9060d7d635c90fd6aced31e6
Jon Stovell <jonstovell@gmail.com>
2016-09-23 00:57:10 -0600

    Even better validation for these bits
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

--- Sources/Subs-Calendar.php
+++ Sources/Subs-Calendar.php
@@ -1151,8 +1151,8 @@ function getNewEventDatetimes()
 {
 	$today = getdate();
 
-	$allday = isset($_REQUEST['allday']) ? (int) $_REQUEST['allday'] : (isset($_REQUEST['start_time']) ? 0 : 1);
-	$span = isset($_REQUEST['span']) ? (int) $_REQUEST['span'] : 1;
+	$allday = isset($_REQUEST['allday']) ? 1 : (isset($_REQUEST['start_time']) ? 0 : 1);
+	$span = filter_var($_REQUEST['span'], FILTER_VALIDATE_INT, array('options' => array('min_range' => 0))) ? $_REQUEST['span'] : 1;
 
 	if (!empty($_REQUEST['tz']) && in_array($_REQUEST['tz'], timezone_identifiers_list(DateTimeZone::ALL_WITH_BC)))
 		$tz = $_REQUEST['tz'];
