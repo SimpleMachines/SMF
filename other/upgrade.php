86d5a5283a187c3fd5749220c555bc227c9c6de0
John Rayes <live627@gmail.com>
2016-12-09 18:24:46 -0700

    Fix #3647
    Signed-off-by: John Rayes <live627@gmail.com>

--- other/upgrade.php
+++ other/upgrade.php
@@ -1301,7 +1301,7 @@ function BackupDatabase()
 				return upgradeExit();
 		}
 
-		if ($is_debug && $command_line)
+		if ($command_line)
 		{
 			echo "\n" . ' Successful.\'' . "\n";
 			flush();
@@ -3931,7 +3931,7 @@ function serialize_to_json()
 								)
 							);
 
-							if ($is_debug || $command_line)
+							if ($command_line)
 								echo ' done.';
 						}
 					}
