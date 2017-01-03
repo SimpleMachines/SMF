9ffcae4fee7b4d7d8c83e4edd0ad76efa7f0bffc
Fran Domínguez <d3vcho@gmail.com>
2017-01-03 11:40:14 +0100

    install.php copyright update
    Signed-off by: Frankie "d3vcho" <d3vcho@gmail.com>

--- other/install.php
+++ other/install.php
@@ -1918,7 +1918,7 @@ function template_install_below()
 		</div></div>
 		<div id="footer">
 			<ul>
-				<li class="copyright"><a href="http://www.simplemachines.org/" title="Simple Machines Forum" target="_blank" class="new_win">SMF &copy; 2016, Simple Machines</a></li>
+				<li class="copyright"><a href="http://www.simplemachines.org/" title="Simple Machines Forum" target="_blank" class="new_win">SMF &copy; 2017, Simple Machines</a></li>
 			</ul>
 		</div>
 	</body>
