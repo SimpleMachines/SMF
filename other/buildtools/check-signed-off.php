96e50ef5da9cad6fcf26380d706a9a411f65fff6
jdarwood007 <unmonitored+github@sleepycode.com>
2017-01-24 19:45:37 -0800

    Try to see if we can't handle Travis merges.
    Signed-off-by: jdarwood007 <unmonitored+github@sleepycode.com>

--- other/buildtools/check-signed-off.php
+++ other/buildtools/check-signed-off.php
@@ -16,6 +16,15 @@ $lines = explode("\n", trim(str_replace("\r", "\n", $message)));
 $lastLine = $lines[count($lines) - 1];
 var_dump($lines);
 
+// Did we find a merge?
+if (preg_match('~Merge ([A-Za-z0-9]{40}) into ([A-Za-z0-9]{40})~i', $lastLine, $merges))
+{
+	echo 'Message contains a merge, trying to find parent [' . $lastLine . ']' . '[' . $message . ']' . "\n";
+	$message = trim(shell_exec('git show -s --format=%B ' . $merges[1]));	
+	$lines = explode("\n", trim(str_replace("\r", "\n", $message)));
+	$lastLine = $lines[count($lines) - 1];
+}
+
 $result = stripos($lastLine, 'Signed-off-by:');
 if ($result === false)
 {
