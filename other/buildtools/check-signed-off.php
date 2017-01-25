ecdba22a9fac2645f9e61f46036520d07609cfd0
jdarwood007 <unmonitored+github@sleepycode.com>
2017-01-24 19:20:50 -0800

    This was still failing.  Try again.
    Signed-off-by: jdarwood007 <unmonitored+github@sleepycode.com>

--- other/buildtools/check-signed-off.php
+++ other/buildtools/check-signed-off.php
@@ -11,7 +11,16 @@
  * @version 2.1 Beta 3
  */
 
-$message = shell_exec('git show -s --format=%B HEAD | tail -n 1');
-$result = stripos($message, 'Signed-off by:');
+$message = trim(shell_exec('git show -s --format=%B HEAD'));
+$lines = explode("\n", trim(str_replace("\r", "\n", $message)));
+$lastLine = $lines[count($lines) - 1];
+var_dump($lines);
+
+$result = stripos($lastLine, 'Signed-off-by:');
 if ($result === false)
-	die('Error: Signed-off by not found in commit message');
+{
+	// Try 2.
+	$result2 = stripos($lastLine, 'Signed by');
+	if ($result2 === false)
+		die('Error: Signed-off-by not found in commit message [' . $lastLine . ']' . "\n");
+}
