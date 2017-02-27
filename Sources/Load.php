c266758f674da879cec5dcd0b5c372602ebd7794
sbulen <bulens@pacbell.net>
2017-02-26 22:50:10 -0800

    Issue setLocale using language settings Signed by Shawn Bulen, bulens@pacbell.net

--- Sources/Load.php
+++ Sources/Load.php
@@ -74,9 +74,6 @@ function reloadSettings()
 	// UTF-8 ?
 	$utf8 = (empty($modSettings['global_character_set']) ? $txt['lang_character_set'] : $modSettings['global_character_set']) === 'UTF-8';
 
-	// setlocale is required for basename() & pathinfo() to work properly on files with utf8 characters in the names
-	setlocale(LC_CTYPE,'en_US.UTF-8');
-
 	// Set a list of common functions.
 	$ent_list = empty($modSettings['disableEntityCheck']) ? '&(#\d{1,7}|quot|amp|lt|gt|nbsp);' : '&(#021|quot|amp|lt|gt|nbsp);';
 	$ent_check = empty($modSettings['disableEntityCheck']) ? function($string)
@@ -2653,6 +2650,9 @@ function loadLanguage($template_name, $lang = '', $fatal = true, $force_reload =
 				// Note that we found it.
 				$found = true;
 
+				// setlocale is required for basename() & pathinfo() to work properly on the selected language
+				setlocale(LC_CTYPE, $txt['lang_locale'] . '.' . $txt['lang_character_set']);
+				
 				break;
 			}
 		}
