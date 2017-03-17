1eb5e49616237f23720aea62488e897013b97dfa
albertlast <mathiaspapealbert@hotmail.com>
2017-03-17 22:39:55 +0100

    false is right
    Signed-off-by: albertlast albertlast@hotmail.de

--- Sources/Errors.php
+++ Sources/Errors.php
@@ -154,7 +154,7 @@ function fatal_error($error, $log = 'general', $status = 500)
  *  - the information is logged if log is specified.
  *
  * @param string $error The error message
- * @param string|boolean $log The type of error, or false to not log it
+ * @param string|false $log The type of error, or false to not log it
  * @param array $sprintf An array of data to be sprintf()'d into the specified message
  * @param int $status = false The HTTP status code associated with this error
  */
