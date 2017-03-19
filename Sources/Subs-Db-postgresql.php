692aa9b4a1d54aaca70d4f9a67f58d435fc69e6e
albertlast <mathiaspapealbert@hotmail.com>
2017-03-19 06:44:00 +0100

    Do the mb4 fix only when needed
    Signed-off-by: albertlast albertlast@hotmail.de

--- Sources/Subs-Db-postgresql.php
+++ Sources/Subs-Db-postgresql.php
@@ -58,6 +58,7 @@ function smf_db_initiate($db_server, $db_name, $db_user, $db_passwd, &$db_prefix
 			'db_case_sensitive' => true,
 			'db_escape_wildcard_string' => 'smf_db_escape_wildcard_string',
 			'db_is_resource' => 'is_resource',
+			'db_mb4' => true,
 		);
 
 	if (!empty($db_options['persist']))
