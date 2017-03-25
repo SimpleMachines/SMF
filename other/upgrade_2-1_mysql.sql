d2a93abed0f2849548235f7750937f82ff901bdf
sbulen <bulens@pacbell.net>
2017-03-24 21:18:49 -0700

    Clean up themes not in file system Signed by Shawn Bulen, bulens@pacbell.net

--- other/upgrade_2-1_mysql.sql
+++ other/upgrade_2-1_mysql.sql
@@ -924,29 +924,45 @@ foreach ($toMove as $move)
 /******************************************************************************/
 --- Cleaning up after old themes...
 /******************************************************************************/
----# Disabling old themes
+---# Clean up settings for unused themes
 ---{
-$smcFunc['db_query']('', '
-	DELETE FROM {db_prefix}themes
-	WHERE id_theme != {int:default_theme}
-	  AND variable IN ({array_string:remove_settings});',
+// Fetch list of theme directories
+$request = $smcFunc['db_query']('', '
+	SELECT id_theme, variable, value
+	  FROM {db_prefix}themes
+	WHERE variable = {string:theme_dir}
+	  AND id_theme != {int:default_theme};',
 	array(
 		'default_theme' => 1,
-		'remove_settings' => array('theme_dir','theme_url'),
+		'theme_dir' => 'theme_dir',
 	)
 );
----}
----#
-
----# Setting knownThemes to current default
----{
+// Check which themes exist in the filesystem & save off their IDs 
+// Dont delete default theme(start with 1 in the array), & make sure to delete old core theme
+$known_themes = array('1');
+$core_dir = $GLOBALS['boarddir'] . '/Themes/core';
+while ($row = $smcFunc['db_fetch_assoc']($request))	{
+	if ($row['value'] != $core_dir && is_dir($row['value'])) {
+		$known_themes[] = $row['id_theme'];
+	}
+}
+// Cleanup unused theme settings
+$smcFunc['db_query']('', '
+	DELETE FROM {db_prefix}themes
+	WHERE id_theme NOT IN ({array_int:known_themes});',
+	array(
+		'known_themes' => $known_themes,
+	)
+);
+// Set knownThemes
+$known_themes = implode(',', $known_themes);
 $smcFunc['db_query']('', '
 	UPDATE {db_prefix}settings
-	SET value = {string:default_theme}
-	WHERE variable = {string:known_themes};',
+	SET value = {string:known_themes}
+	WHERE variable = {string:known_theme_str};',
 	array(
-		'default_theme' => '1',
-		'known_themes' => 'knownThemes',
+		'known_theme_str' => 'knownThemes',
+		'known_themes' => $known_themes,
 	)
 );
 ---}
