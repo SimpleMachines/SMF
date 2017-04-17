e474467ac89690a388dd5db3efef50c940040ccd
albertlast <mathiaspapealbert@hotmail.com>
2017-04-17 07:24:19 +0200

    FixDoubleQuotes in PG fix #4005 (#4007)
    * FixDoubleQuotes in PG
    
    Signed-off-by: albertlast albertlast@hotmail.de
    
    * add space and if
    
    Signed-off-by: albertlast albertlast@hotmail.de

--- Sources/DbSearch-postgresql.php
+++ Sources/DbSearch-postgresql.php
@@ -118,6 +118,10 @@ function smf_db_search_query($identifier, $db_string, $db_values = array(), $con
 			$db_values['db_error_skip'] = true;
 		}
 	}
+	
+	//fix double quotes
+	if ($identifier == 'insert_into_log_messages_fulltext')
+		$db_values = str_replace('"', "'", $db_values);
 
 	$return = $smcFunc['db_query']('', $db_string,
 		$db_values, $connection
