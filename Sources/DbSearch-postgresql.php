0d8bc8e89e47158257527f005727ae97a5f9f948
albertlast <mathiaspapealbert@hotmail.com>
2017-04-17 07:23:28 +0200

    add space and if
    Signed-off-by: albertlast albertlast@hotmail.de

--- Sources/DbSearch-postgresql.php
+++ Sources/DbSearch-postgresql.php
@@ -120,7 +120,8 @@ function smf_db_search_query($identifier, $db_string, $db_values = array(), $con
 	}
 	
 	//fix double quotes
-	$db_values = str_replace('"',"'",$db_values);
+	if ($identifier == 'insert_into_log_messages_fulltext')
+		$db_values = str_replace('"', "'", $db_values);
 
 	$return = $smcFunc['db_query']('', $db_string,
 		$db_values, $connection
