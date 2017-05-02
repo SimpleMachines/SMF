b53452e4d5dc35c42dac482ddeb9ff4dde391096
albertlast <mathiaspapealbert@hotmail.com>
2017-05-02 21:58:55 +0200

    improve Check
    Signed-off-by: albertlast albertlast@hotmail.de

--- Sources/Subs.php
+++ Sources/Subs.php
@@ -3704,7 +3704,7 @@ function getAttachmentFilename($filename, $attachment_id, $dir = null, $new = fa
 		$file_hash = sha1(md5($filename . time()) . mt_rand());
 
 	// Are we using multiple directories?
-	if (!empty($modSettings['currentAttachmentUploadDir']) && $dir != null)
+	if (is_array($modSettings['attachmentUploadDir']))
 		$path = $modSettings['attachmentUploadDir'][$dir];
 
 	else
