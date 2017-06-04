5245f5249f4e5d5cd702d699117276b197f2928c
Yoshi2889 <rick.2889@gmail.com>
2017-06-04 16:01:07 +0200

    Optimize smf_db_table_structure
    Signed-off-by: Yoshi2889 <rick.2889@gmail.com>

--- Sources/DbPackages-postgresql.php
+++ Sources/DbPackages-postgresql.php
@@ -698,10 +698,9 @@ function smf_db_calculate_type($type_name, $type_size = null, $reverse = false)
  * Get table structure.
  *
  * @param string $table_name The name of the table
- * @param array $parameters Not used?
- * @return An array of table structure - the name, the column info from {@link smf_db_list_columns()} and the index info from {@link smf_db_list_indexes()}
+ * @return array An array of table structure - the name, the column info from {@link smf_db_list_columns()} and the index info from {@link smf_db_list_indexes()}
  */
-function smf_db_table_structure($table_name, $parameters = array())
+function smf_db_table_structure($table_name)
 {
 	global $smcFunc, $db_prefix;
 
