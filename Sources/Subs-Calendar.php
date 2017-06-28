a79db98facd1452b9ab5453a7621a25fbe33072d
albertlast <mathiaspapealbert@hotmail.com>
2017-06-28 19:57:54 +0200

    Fix Guests (#4162)
    Signed-off-by: albertlast albertlast@hotmail.de

--- Sources/Subs-Calendar.php
+++ Sources/Subs-Calendar.php
@@ -1617,7 +1617,7 @@ function getUserTimezone($id_member = null)
 	}
 
 	//maybe the current user is the one
-	if (isset($user_settings) && $user_settings['id_member'] == $id_member)
+	if (isset($user_settings['id_member']) && $user_settings['id_member'] == $id_member)
 	{
 		$member_cache[$id_member] = $user_settings['timezone'];
 		return $user_settings['timezone'];
