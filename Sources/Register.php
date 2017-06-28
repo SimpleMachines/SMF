SimpleMachines/SMF

commit 0f46853a857847aea3cf413274998e5a94195bdc

Bye bye allow_editDisplayName
Signed-off-by: Suki <suki@missallsunday.com>

Sources/Register.php
@@ -347,7 +347,7 @@ function Register2($verifiedOpenID = false)
 	require_once($sourcedir . '/Subs-Members.php');
 
 	// Validation... even if we're not a mall.
-	if (isset($_POST['real_name']) && (!empty($modSettings['allow_editDisplayName']) || allowedTo('moderate_forum')))
+	if (isset($_POST['real_name']) && (allowedTo('profile_displayed_name') || allowedTo('moderate_forum')))
 	{
 		$_POST['real_name'] = trim(preg_replace('~[\t\n\r \x0B\0' . ($context['utf8'] ? ($context['server']['complex_preg_chars'] ? '\x{A0}\x{AD}\x{2000}-\x{200F}\x{201F}\x{202F}\x{3000}\x{FEFF}' : "\xC2\xA0\xC2\xAD\xE2\x80\x80-\xE2\x80\x8F\xE2\x80\x9F\xE2\x80\xAF\xE2\x80\x9F\xE3\x80\x80\xEF\xBB\xBF") : '\x00-\x08\x0B\x0C\x0E-\x19\xA0') . ']+~' . ($context['utf8'] ? 'u' : ''), ' ', $_POST['real_name']));
 		if (trim($_POST['real_name']) != '' && !isReservedName($_POST['real_name']) && $smcFunc['strlen']($_POST['real_name']) < 60)

Sources/ManageSettings.php
@@ -148,7 +148,6 @@ function ModifyBasicSettings($return_config = false)
 			array('check', 'allow_guestAccess'),
 			array('check', 'enable_buddylist'),
 			array('check', 'enable_unwatch'),
-			array('check', 'allow_editDisplayName'),
 			array('check', 'allow_hideOnline'),
 			array('check', 'titlesEnable'),
 			array('text', 'default_personal_text', 'subtext' => $txt['default_personal_text_note'], 'disabled' => !$can_personal_text),

Sources/Profile-Modify.php
@@ -476,13 +476,13 @@ function loadProfileFields($force_reload = false)
 			'),
 		),
 		'real_name' => array(
-			'type' => !empty($modSettings['allow_editDisplayName']) || allowedTo('moderate_forum') ? 'text' : 'label',
+			'type' => allowedTo('moderate_forum') ? 'text' : 'label',
 			'label' => $txt['name'],
 			'subtext' => $txt['display_name_desc'],
 			'log_change' => true,
 			'input_attr' => array('maxlength="60"'),
-			'permission' => 'profile_identity',
-			'enabled' => !empty($modSettings['allow_editDisplayName']) || allowedTo('moderate_forum'),
+			'permission' => 'profile_displayed_name',
+			'enabled' => allowedTo('moderate_forum'),
 			'input_validate' => create_function('&$value', '
 				global $context, $smcFunc, $sourcedir, $cur_profile;
 

commit 0f3b7d9ccbd62bc929e0325d1dc93926d4b49215

Correctly handles setting the displayed name during registration
0f46853 introduced a bug with allowing guests to set their displayed name during registration. Specifically, replacing `!empty($modSettings['allow_editDisplayName'])` with `allowedTo('profile_displayed_name')` meant that guests in the process of registering could never meet the condition, since `allowedTo('profile_displayed_name')` will always return false for guests.

This code fixes the problem by checking whether regular members have the 'profile_displayed_name_own' permission. If so, then the guest will be allowed to change their displayed name as soon as they complete registration anyway, so we allow it to be set during registration.

Of course, all of this only comes up for themes that offer a field to set the displayed name on the registration form (or if the user manually appends `real_name=foo` to the URL parameters). So to test this bug and its solution, you'll want to add an input field to the registration template for `$_POST['real_name']`.

Signed-off-by: Jon Stovell <jonstovell@gmail.com>

Sources/Register.php
@@ -314,12 +314,38 @@ function Register2()
 	// Needed for isReservedName() and registerMember().
 	require_once($sourcedir . '/Subs-Members.php');
 
-	// Validation... even if we're not a mall.
-	if (isset($_POST['real_name']) && (allowedTo('profile_displayed_name') || allowedTo('moderate_forum')))
+	// Maybe you want set the displayed name during registration
+	if (isset($_POST['real_name']))
 	{
-		$_POST['real_name'] = trim(preg_replace('~[\t\n\r \x0B\0' . ($context['utf8'] ? '\x{A0}\x{AD}\x{2000}-\x{200F}\x{201F}\x{202F}\x{3000}\x{FEFF}' : '\x00-\x08\x0B\x0C\x0E-\x19\xA0') . ']+~' . ($context['utf8'] ? 'u' : ''), ' ', $_POST['real_name']));
-		if (trim($_POST['real_name']) != '' && !isReservedName($_POST['real_name']) && $smcFunc['strlen']($_POST['real_name']) < 60)
-			$possible_strings[] = 'real_name';
+		// Are you already allowed to edit the displayed name?
+		if (allowedTo('profile_displayed_name') || allowedTo('moderate_forum'))
+			$canEditDisplayName = true;
+
+		// If you are a guest, will you be allowed to once you register?
+		else
+		{
+			$request = $smcFunc['db_query']('', '
+				SELECT add_deny
+				FROM {db_prefix}permissions
+				WHERE id_group = {int:id_group} AND permission = {string:permission}',
+				array(
+					'id_group' => 0,
+					'permission' => 'profile_displayed_name_own',
+				)
+			);
+			list($canEditDisplayName) = $smcFunc['db_fetch_row']($request);
+			$smcFunc['db_free_result']($request);
+		}
+
+		if ($canEditDisplayName)
+		{
+			// Sanitize it
+			$_POST['real_name'] = trim(preg_replace('~[\t\n\r \x0B\0' . ($context['utf8'] ? '\x{A0}\x{AD}\x{2000}-\x{200F}\x{201F}\x{202F}\x{3000}\x{FEFF}' : '\x00-\x08\x0B\x0C\x0E-\x19\xA0') . ']+~' . ($context['utf8'] ? 'u' : ''), ' ', $_POST['real_name']));
+
+			// Only set it if we are sure it is good
+			if (trim($_POST['real_name']) != '' && !isReservedName($_POST['real_name']) && $smcFunc['strlen']($_POST['real_name']) < 60)
+				$possible_strings[] = 'real_name';
+		}
 	}
 
 	// Handle a string as a birthdate...