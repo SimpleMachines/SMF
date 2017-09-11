31ca31df358a905af0d7a05d008d45e6b55242ac
albertlast <mathiaspapealbert@hotmail.com>
2017-09-11 07:18:18 +0200

    check var in error
    Signed-off-by: albertlast albertlast@hotmail.de

--- Sources/Errors.php
+++ Sources/Errors.php
@@ -490,7 +490,7 @@ function log_error_online($error, $sprintf = array())
 	if (SMF == 'SSI' || SMF == 'BACKGROUND')
 		return;
 
-	$session_id = $user_info['is_guest'] ? 'ip' . $user_info['ip'] : session_id();
+	$session_id = isset($user_info['is_guest']) && $user_info['is_guest'] ? 'ip' . $user_info['ip'] : session_id();
 
 	// First, we have to get the online log, because we need to break apart the serialized string.
 	$request = $smcFunc['db_query']('', '
