d57e668c18e6feb905887831132e0c447e8b3361
albertlast <mathiaspapealbert@hotmail.com>
2017-10-26 09:26:38 +0200

    check vs array not vs cookie
    Signed-off-by: albertlast albertlast@hotmail.de

--- Sources/Subs-Auth.php
+++ Sources/Subs-Auth.php
@@ -45,22 +45,22 @@ function setLoginCookie($cookie_length, $id, $password = '')
 		// Valid Format
 		$validFormat = preg_match('~^{"0":\d{1,7},"1":"[0-9a-f]{0,128}","2":\d{1,14}(,"3":\d{1}(,"path":"\\\\/.*")?)?}$~' , $_COOKIE[$cookiename]) === 1;
 
-		// Get the path and check it
-		$cookie_url = url_parts(!empty($modSettings['localCookies']), !empty($modSettings['globalCookies']));
-		$pathWrong = empty($_COOKIE[$cookiename]['path']) || ($_COOKIE[$cookiename]['path'] != $cookie_url[1]);
-
 		$array = $smcFunc['json_decode']($_COOKIE[$cookiename], true);
 
 		// Legacy format
 		if (is_null($array))
 			$array = safe_unserialize($_COOKIE[$cookiename]);
 
+		// Get the path and check it
+		$cookie_url = url_parts(!empty($modSettings['localCookies']), !empty($modSettings['globalCookies']));
+		$pathWrong = empty($array['path']) || ($array['path'] != $cookie_url[1]);
+
 		// Out with the old, in with the new!
 		if ((isset($array[3]) && $array[3] != $cookie_state) || $pathWrong || !$validFormat)
 		{
 			$cookie_url = url_parts($array[3] & 1 > 0, $array[3] & 2 > 0);
-			if (isset($_COOKIE[$cookiename]['path']))
-				$cookie_url[1] = $_COOKIE[$cookiename]['path'];
+			if (isset($array['path']))
+				$cookie_url[1] = $array['path'];
 			smf_setcookie($cookiename, $smcFunc['json_encode'](array(0, '', 0, 'path' => $cookie_url[1]), JSON_FORCE_OBJECT), 1, $cookie_url[1], $cookie_url[0]);
 		}
 	}
