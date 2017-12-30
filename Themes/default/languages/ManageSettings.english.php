59e6912dd9a806ceb02e7b16a3069829181c83ab
Martyn Brown <tinoest@gmail.com>
2017-12-30 17:13:03 +0000

    Update ManageSettings.english.php

--- Themes/default/languages/ManageSettings.english.php
+++ Themes/default/languages/ManageSettings.english.php
@@ -415,4 +415,3 @@ $txt['tfa_mode_enabled'] = 'Enabled';
 $txt['tfa_mode_disabled'] = 'Disabled';
 $txt['tfa_mode_subtext'] = 'Allows users to have a second layer of security while logging in, users would need an app like Google Authenticator paired with their account';
 
-?>
