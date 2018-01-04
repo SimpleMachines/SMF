eb2022351244bc889f4adc533d8f0f3dc078fdf9
albertlast <mathiaspapealbert@hotmail.com>
2018-01-04 19:03:54 +0100

    Less queries (#4412)
    * correlated subquery for Participation
    
    Signed-off-by: albertlast albertlast@hotmail.de
    
    * run once when range_limit is empty
    
    Signed-off-by: albertlast albertlast@hotmail.de
    
    * added as
    
    Signed-off-by: albertlast albertlast@hotmail.de

--- Sources/MessageIndex.php
+++ Sources/MessageIndex.php
@@ -339,11 +339,18 @@ function MessageIndex()
 		$message_index_tables = array();
 		$message_index_wheres = array();
 		call_integration_hook('integrate_message_index', array(&$message_index_selects, &$message_index_tables, &$message_index_parameters, &$message_index_wheres, &$topic_ids));
+		
+		if (!empty($modSettings['enableParticipation']) && !$user_info['is_guest'])
+			$enableParticipation = true;
+		else
+			$enableParticipation = false;
 
 		$result = $smcFunc['db_query']('substring', '
 			SELECT
 				t.id_topic, t.num_replies, t.locked, t.num_views, t.is_sticky, t.id_poll, t.id_previous_board,
 				' . ($user_info['is_guest'] ? '0' : 'COALESCE(lt.id_msg, COALESCE(lmr.id_msg, -1)) + 1') . ' AS new_from,
+				' . ( $enableParticipation ? ' COALESCE(( SELECT 1 FROM {db_prefix}messages AS parti WHERE t.id_topic = parti.id_topic and parti.id_member = {int:current_member} LIMIT 1) , 0) as is_posted_in,
+				'	: '') . '
 				t.id_last_msg, t.approved, t.unapproved_posts, ml.poster_time AS last_poster_time, t.id_redirect_topic,
 				ml.id_msg_modified, ml.subject AS last_subject, ml.icon AS last_icon,
 				ml.poster_name AS last_member_name, ml.id_member AS last_id_member,' . (!empty($settings['avatars_on_indexes']) ? ' meml.avatar, meml.email_address, memf.avatar AS first_member_avatar, memf.email_address AS first_member_mail, COALESCE(af.id_attach, 0) AS first_member_id_attach, af.filename AS first_member_filename, af.attachment_type AS first_member_attach_type, COALESCE(al.id_attach, 0) AS last_member_id_attach, al.filename AS last_member_filename, al.attachment_type AS last_member_attach_type,' : '') . '
@@ -512,7 +519,7 @@ function MessageIndex()
 				'is_locked' => !empty($row['locked']),
 				'is_redirect' => !empty($row['id_redirect_topic']),
 				'is_poll' => $modSettings['pollMode'] == '1' && $row['id_poll'] > 0,
-				'is_posted_in' => false,
+				'is_posted_in' => ($enableParticipation ? $row['is_posted_in'] : false),
 				'is_watched' => false,
 				'icon' => $row['first_icon'],
 				'icon_url' => $settings[$context['icon_sources'][$row['first_icon']]] . '/post/' . $row['first_icon'] . '.png',
@@ -550,26 +557,6 @@ function MessageIndex()
 		// Fix the sequence of topics if they were retrieved in the wrong order. (for speed reasons...)
 		if ($fake_ascending)
 			$context['topics'] = array_reverse($context['topics'], true);
-
-		if (!empty($modSettings['enableParticipation']) && !$user_info['is_guest'] && !empty($topic_ids))
-		{
-			$result = $smcFunc['db_query']('', '
-				SELECT id_topic
-				FROM {db_prefix}messages
-				WHERE id_topic IN ({array_int:topic_list})
-					AND id_member = {int:current_member}
-				GROUP BY id_topic
-				LIMIT {int:limit}',
-				array(
-					'current_member' => $user_info['id'],
-					'topic_list' => $topic_ids,
-					'limit' => count($topic_ids),
-				)
-			);
-			while ($row = $smcFunc['db_fetch_assoc']($result))
-				$context['topics'][$row['id_topic']]['is_posted_in'] = true;
-			$smcFunc['db_free_result']($result);
-		}
 	}
 
 	$context['jump_to'] = array(
