2d2b1a1cd98ce0c8a113b04179a3c6510e5101fe
albertlast <mathiaspapealbert@hotmail.com>
2018-01-14 20:56:52 +0100

    wraped
    Signed-off-by: albertlast albertlast@hotmail.de

--- Themes/default/GenericControls.template.php
+++ Themes/default/GenericControls.template.php
@@ -295,11 +295,13 @@ function template_control_verification($verify_id, $display_type = 'all', $reset
 			}
 
 			if ($verify_context['can_recaptcha'])
+			{
 				$lang = (isset($txt['lang_recaptcha']) ? $txt['lang_recaptcha'] : $txt['lang_dictionary']); 
 				echo '
 				<div class="g-recaptcha centertext" data-sitekey="' . $verify_context['recaptcha_site_key'] . '" data-theme="' . $verify_context['recaptcha_theme'] . '"></div>
 				<br>
 				<script type="text/javascript" src="https://www.google.com/recaptcha/api.js?hl='.$lang.'"></script>';
+			}
 		}
 		else
 		{
