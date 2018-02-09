7b517d8dbc619cbc4f85259e7a292d8162640b82
albertlast <mathiaspapealbert@hotmail.com>
2018-02-09 18:28:11 +0100

    add arrow
    Signed-off-by: albertlast albertlast@hotmail.de

--- other/upgrade.php
+++ other/upgrade.php
@@ -2368,7 +2368,7 @@ Usage: /path/to/php -f ' . basename(__FILE__) . ' -- [OPTION]...
 	$create = $smcFunc['db_create_table']('{db_prefix}priv_check', array(array('name' => 'id_test', 'type' => 'int', 'size' => 10, 'unsigned' => true, 'auto' => true)), array(array('columns' => array('id_test'), 'primary' => true)), array(), 'overwrite');
 
 	// ALTER
-	$alter = $smcFunc['db_add_column']('{db_prefix}priv_check', array('name' => 'txt', 'type' => 'varchar', 'size' = 255, 'null' => false, 'default' => ''));
+	$alter = $smcFunc['db_add_column']('{db_prefix}priv_check', array('name' => 'txt', 'type' => 'varchar', 'size' => 255, 'null' => false, 'default' => ''));
 
 	// DROP
 	$drop = $smcFunc['db_drop_table']('{db_prefix}priv_check');
