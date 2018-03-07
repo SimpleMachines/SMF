5702a53ef3867e8797903035a568d1c46ee0c409
Jon Stovell <jonstovell@gmail.com>
2018-03-07 13:03:47 -0700

    Uses Punycode library in set_tld_regex() instead of bespoke algorithm
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

--- Sources/Subs.php
+++ Sources/Subs.php
@@ -5921,7 +5921,11 @@ function set_tld_regex($update = false)
 	{
 		$tlds = fetch_web_data('https://data.iana.org/TLD/tlds-alpha-by-domain.txt');
 
-		// If the Internet Assigned Numbers Authority can't be reached, the Internet is gone. We're probably running on a server hidden in a bunker deep underground to protect it from marauding bandits roaming on the surface. We don't want to waste precious electricity on pointlessly repeating background tasks, so we'll wait until the next regularly scheduled update to see if civilization has been restored.
+		// If the Internet Assigned Numbers Authority can't be reached, the Internet is gone.
+		// We're probably running on a server hidden in a bunker deep underground to protect it from
+		// marauding bandits roaming on the surface. We don't want to waste precious electricity on
+		// pointlessly repeating background tasks, so we'll wait until the next regularly scheduled
+		// update to see if civilization has been restored.
 		if ($tlds === false)
 			$postapocalypticNightmare = true;
 	}
@@ -5945,110 +5949,9 @@ function set_tld_regex($update = false)
 		});
 
 		// Convert Punycode to Unicode
-		$tlds = array_map(function ($input) {
-			$prefix = 'xn--';
-			$safe_char = 0xFFFC;
-			$base = 36;
-			$tmin = 1;
-			$tmax = 26;
-			$skew = 38;
-			$damp = 700;
-			$output_parts = array();
-
-			$input = str_replace(strtoupper($prefix), $prefix, $input);
-
-			$enco_parts = (array) explode('.', $input);
-
-			foreach ($enco_parts as $encoded)
-			{
-				if (strpos($encoded,$prefix) !== 0 || strlen(trim(str_replace($prefix,'',$encoded))) == 0)
-				{
-					$output_parts[] = $encoded;
-					continue;
-				}
-
-				$is_first = true;
-				$bias = 72;
-				$idx = 0;
-				$char = 0x80;
-				$decoded = array();
-				$output='';
-				$delim_pos = strrpos($encoded, '-');
-
-				if ($delim_pos > strlen($prefix))
-				{
-					for ($k = strlen($prefix); $k < $delim_pos; ++$k)
-					{
-						$decoded[] = ord($encoded{$k});
-					}
-				}
-
-				$deco_len = count($decoded);
-				$enco_len = strlen($encoded);
-
-				for ($enco_idx = $delim_pos ? ($delim_pos + 1) : 0; $enco_idx < $enco_len; ++$deco_len)
-				{
-					for ($old_idx = $idx, $w = 1, $k = $base; 1 ; $k += $base)
-					{
-						$cp = ord($encoded{$enco_idx++});
-						$digit = ($cp - 48 < 10) ? $cp - 22 : (($cp - 65 < 26) ? $cp - 65 : (($cp - 97 < 26) ? $cp - 97 : $base));
-						$idx += $digit * $w;
-						$t = ($k <= $bias) ? $tmin : (($k >= $bias + $tmax) ? $tmax : ($k - $bias));
-
-						if ($digit < $t)
-							break;
-
-						$w = (int) ($w * ($base - $t));
-					}
-
-					$delta = $idx - $old_idx;
-					$delta = intval($is_first ? ($delta / $damp) : ($delta / 2));
-					$delta += intval($delta / ($deco_len + 1));
-
-					for ($k = 0; $delta > (($base - $tmin) * $tmax) / 2; $k += $base)
-						$delta = intval($delta / ($base - $tmin));
-
-					$bias = intval($k + ($base - $tmin + 1) * $delta / ($delta + $skew));
-					$is_first = false;
-					$char += (int) ($idx / ($deco_len + 1));
-					$idx %= ($deco_len + 1);
-
-					if ($deco_len > 0)
-					{
-						for ($i = $deco_len; $i > $idx; $i--)
-							$decoded[$i] = $decoded[($i - 1)];
-					}
-					$decoded[$idx++] = $char;
-				}
-
-				foreach ($decoded as $k => $v)
-				{
-					// 7bit are transferred literally
-					if ($v < 128)
-						$output .= chr($v);
-
-					// 2 bytes
-					elseif ($v < (1 << 11))
-						$output .= chr(192+($v >> 6)) . chr(128+($v & 63));
-
-					// 3 bytes
-					elseif ($v < (1 << 16))
-						$output .= chr(224+($v >> 12)) . chr(128+(($v >> 6) & 63)) . chr(128+($v & 63));
-
-					// 4 bytes
-					elseif ($v < (1 << 21))
-						$output .= chr(240+($v >> 18)) . chr(128+(($v >> 12) & 63)) . chr(128+(($v >> 6) & 63)) . chr(128+($v & 63));
-
-					//  'Conversion from UCS-4 to UTF-8 failed: malformed input at byte '.$k
-					else
-						$output .= $safe_char;
-				}
-
-				$output_parts[] = $output;
-			}
-
-			return implode('.', $output_parts);
-		}, $tlds);
+		require_once($sourcedir . '/punycode/Punycode.php');
+		$Punycode = new TrueBV\Punycode();
+		$tlds = array_map(function ($input) use ($Punycode) { return $Punycode->decode($input); }, $tlds);
 	}
 	// Otherwise, use the 2012 list of gTLDs and ccTLDs for now and schedule a background update
 	else
