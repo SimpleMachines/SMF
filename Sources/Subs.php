eef0e8e934d598f22d7c96ea6a92a0f46f5e8299
Jon Stovell <jonstovell@gmail.com>
2018-03-12 01:14:16 -0600

    Adds missing type declaration for youtube BBC in parse_bbc()
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

--- Sources/Subs.php
+++ Sources/Subs.php
@@ -1752,6 +1752,7 @@ function parse_bbc($message, $smileys = true, $cache_id = '', $parse_tags = arra
 			),
 			array(
 				'tag' => 'youtube',
+				'type' => 'unparsed_content',
 				'content' => '<iframe width="560" height="315" frameborder="0" src="https://www.youtube.com/embed/$1?wmode=opaque" data-youtube-id="$1" allowfullscreen=""></iframe>',
 				'disabled_content' => '<a href="https://www.youtube.com/watch?v=$1" target="_blank" rel="noopener">https://www.youtube.com/watch?v=$1</a>',
 			),
