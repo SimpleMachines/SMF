7ed1c82e37ed1e9d0e8d1818beb685c79f852360
Jon Stovell <jonstovell@gmail.com>
2018-04-07 12:59:10 -0600

    Allows custom descriptions for entire zones or just single locations
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

--- Themes/default/languages/Timezones.english.php
+++ Themes/default/languages/Timezones.english.php
@@ -14,30 +14,43 @@
  * version uses. If you need to add, replace, or remove locations in order to
  * meet the needs of your language, SMF will handle your changes gracefully.
  *
- * However, you should only name ONE representative location for each time zone.
- * For example, if you use 'Europe/Berlin' to represent Central European Time,
- * you should not include entries for 'Europe/Paris', 'Europe/Amsterdam',
- * 'Europe/Rome', etc. Those places all use the same time zone, so only only
- * location is needed to represent them all. If two locations in the same time
- * zone are included in this list, the first one defined will be used and the
- * second will be ignored.
+ * HOW THIS FILE WORKS:
  *
- * If you want to use a certain location to represent a time zone, but do not
- * want to give it a custom description, you can leave the description empty. An
- * empty description will be populated with city names like usual. This can be
- * useful if you want to make sure that a certain city is used as the
- * representative of a certain time zone, but still want the description to be
- * filled dynamically for whatever reason.
+ * There are three sections to this file. The first two sections use the $tztxt
+ * variable, and the third uses the $txt variable.
+ *
+ * The first section uses this format:
+ *
+ *     $tztxt['America/Anchorage'] = 'Alaska';
+ *
+ * This tells SMF (1) to use 'America/Anchorage' as the specific location to
+ * represent all locations that use the same time zone rules that Anchorage
+ * does, and (2) to use 'Alaska' as the description string to show the user in
+ * the select menu.
+ *
+ * The second section uses this format:
+ *
+ *     $tztxt['Europe/London'] = '';
+ *
+ * This tells SMF (1) to use 'Europe/London' as the specific location to
+ * represent all locations that use the same time zone rules that London does,
+ * and (2) to place London first in the list of locations used as the
+ * description string shown to the user.
+ *
+ * The third secion uses this format:
+ *
+ *     $txt['America/Adak'] = 'Aleutian Islands';
+ *
+ * This simply tells SMF to replace 'Adak' with 'Aleutian Islands' in the
+ * description string shown to the user. It does not tell SMF anything in
+ * particular about where or how it should use the 'America/Adak' location.
  */
 
 global $tztxt;
 
+// Descriptions for entire time zones to use instead of a list of locations
 $tztxt['UTC'] = 'Coordinated Universal Time';
-$tztxt['America/Adak'] = 'Aleutian Islands';
-$tztxt['Pacific/Marquesas'] = 'Marquesas Islands';
-$tztxt['Pacific/Gambier'] = 'Gambier Islands';
 $tztxt['America/Anchorage'] = 'Alaska';
-$tztxt['Pacific/Pitcairn'] = 'Pitcairn Islands';
 $tztxt['America/Los_Angeles'] = 'Pacific Time (USA, Canada)';
 $tztxt['America/Denver'] = 'Mountain Time (USA, Canada)';
 $tztxt['America/Phoenix'] = 'Mountain Time (no DST)';
@@ -47,35 +60,40 @@ $tztxt['America/New_York'] = 'Eastern Time (USA, Canada)';
 $tztxt['America/Jamaica'] = 'Eastern Time (no DST)';
 $tztxt['America/Halifax'] = 'Atlantic Time (Canada)';
 $tztxt['America/Anguilla'] = 'Atlantic Time (no DST)';
-$tztxt['America/St_Johns'] = 'Newfoundland';
-$tztxt['America/Chihuahua'] = 'Chihuahua, Mazatlan';
-$tztxt['Pacific/Easter'] = 'Easter Island';
-$tztxt['Atlantic/Stanley'] = 'Falkland Islands';
-$tztxt['America/Miquelon'] = 'Saint Pierre and Miquelon';
-$tztxt['America/Argentina/Buenos_Aires'] = 'Buenos Aires';
 $tztxt['America/Sao_Paulo'] = 'Brasilia Time';
 $tztxt['America/Araguaina'] = 'Brasilia Time (no DST)';
-$tztxt['America/Godthab'] = 'Greenland';
-$tztxt['America/Noronha'] = 'Fernando de Noronha';
 $tztxt['Atlantic/Reykjavik'] = 'Greenwich Mean Time (no DST)';
-$tztxt['Europe/London'] = '';
 $tztxt['Europe/Berlin'] = 'Central European Time';
-$tztxt['Europe/Helsinki'] = 'Eastern European Time';
-$tztxt['Africa/Brazzaville'] = 'Brazzaville, Lagos, Porto-Novo';
-$tztxt['Asia/Jerusalem'] = 'Jerusalem';
-$tztxt['Europe/Moscow'] = '';
 $tztxt['Africa/Khartoum'] = 'Eastern Africa Time';
+$tztxt['Europe/Helsinki'] = 'Eastern European Time';
 $tztxt['Asia/Riyadh'] = 'Arabia Time';
-$tztxt['Asia/Kolkata'] = 'India, Sri Lanka';
-$tztxt['Asia/Dhaka'] = 'Astana, Dhaka';
-$tztxt['Antarctica/DumontDUrville'] = 'Dumont D\'Urville Station';
-$tztxt['Antarctica/Vostok'] = 'Vostok Station';
-$tztxt['Australia/Lord_Howe'] = 'Lord Howe Island';
-$tztxt['Pacific/Guadalcanal'] = 'Solomon Islands';
-$tztxt['Pacific/Norfolk'] = 'Norfolk Island';
-$tztxt['Pacific/Noumea'] = 'New Caledonia';
-$tztxt['Pacific/Auckland'] = 'Auckland, McMurdo Station';
-$tztxt['Pacific/Kwajalein'] = 'Marshall Islands';
-$tztxt['Pacific/Chatham'] = 'Chatham Islands';
+
+// Use a list of locations as the description, but force the named one to always be first
+$tztxt['Europe/London'] = '';
+$tztxt['Europe/Moscow'] = '';
+
+// Custom descriptions for individual locations
+$txt['America/Adak'] = 'Aleutian Islands';
+$txt['Pacific/Marquesas'] = 'Marquesas Islands';
+$txt['Pacific/Gambier'] = 'Gambier Islands';
+$txt['Pacific/Pitcairn'] = 'Pitcairn Islands';
+$txt['America/St_Johns'] = 'Newfoundland';
+$txt['Pacific/Easter'] = 'Easter Island';
+$txt['Atlantic/Stanley'] = 'Falkland Islands';
+$txt['America/Miquelon'] = 'St. Pierre and Miquelon';
+$txt['America/Godthab'] = 'Greenland';
+$txt['America/Noronha'] = 'Fernando de Noronha';
+$txt['Asia/Kolkata'] = 'India';
+$txt['Asia/Colombo'] = 'Sri Lanka';
+$txt['Asia/Dhaka'] = 'Bangladesh';
+$txt['Antarctica/DumontDUrville'] = 'Dumont D\'Urville Station';
+$txt['Antarctica/Vostok'] = 'Vostok Station';
+$txt['Australia/Lord_Howe'] = 'Lord Howe Island';
+$txt['Pacific/Guadalcanal'] = 'Solomon Islands';
+$txt['Pacific/Norfolk'] = 'Norfolk Island';
+$txt['Pacific/Noumea'] = 'New Caledonia';
+$txt['Antarctica/McMurdo'] = 'McMurdo Station';
+$txt['Pacific/Kwajalein'] = 'Marshall Islands';
+$txt['Pacific/Chatham'] = 'Chatham Islands';
 
 ?>
