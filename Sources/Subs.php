3521c0a504858d52c149b9e5e011653ba986ec10
albertlast <mathiaspapealbert@hotmail.com>
2018-04-25 17:49:32 +0200

    add deny support
    Signed-off-by: albertlast albertlast@hotmail.de

--- Sources/Subs.php
+++ Sources/Subs.php
@@ -6303,7 +6303,9 @@ function build_query_board($userid)
 	else
 	{
 		$query_part['query_see_board'] = '((FIND_IN_SET(' . implode(', b.member_groups) != 0 OR FIND_IN_SET(', $groups) . ', b.member_groups) != 0)' . (!empty($deny_boards_access) ? ' AND (FIND_IN_SET(' . implode(', b.deny_member_groups) = 0 AND FIND_IN_SET(', $groups) . ', b.deny_member_groups) = 0)' : '') . (isset($mod_cache) ? ' OR ' . $mod_cache['mq'] : '') . ')';
-		$query_part['query_see_board_join'] = 'JOIN (SELECT DISTINCT id_board FROM ' . $db_prefix . 'board_permissions_view WHERE grp IN ( '. implode(',', $groups) .') ) bpv ON (bpv.id_board = b.id_board)';
+		$query_part['query_see_board_join'] = 'JOIN (SELECT DISTINCT id_board FROM ' . $db_prefix . 'board_permissions_view WHERE (grp IN ( '. implode(',', $groups) .') and deny = 0) '
+				.  ( !empty($deny_boards_access) ? ' AND (grp NOT IN ( '. implode(',', $groups) .') and deny = 1)' : '')
+				. ') bpv ON (bpv.id_board = b.id_board)';
 	}
 		
 	// Build the list of boards they WANT to see.
