7f58b7e5b5e4341301de2e86cdf7709a790b7935
John Rayes <live627@gmail.com>
2018-10-30 17:12:25 -0700

    Assume a denial if permissions have not been loaded yet
    Fix #4716

--- Sources/Security.php
+++ Sources/Security.php
@@ -903,7 +903,7 @@ function allowedTo($permission, $boards = null, $any = false)
 		return true;
 
 	// You're never allowed to do something if your data hasn't been loaded yet!
-	if (empty($user_info))
+	if (empty($user_info) || !isset($user_info['permissions']))
 		return false;
 
 	// Administrators are supermen :P.
