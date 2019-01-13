2ef0f2c2ebc7b4e83e4792baa9e77bb57abfa19d
John Rayes <live627@gmail.com>
2019-01-13 15:49:40 -0700

    params for index removing were backwards
    Signed-off-by: John Rayes <live627@gmail.com>

--- Themes/default/languages/Packages.english.php
+++ Themes/default/languages/Packages.english.php
@@ -190,7 +190,7 @@ $txt['package_db_uninstall_details'] = 'Details';
 $txt['package_db_uninstall_actions'] = 'Checking this option will result in the following actions';
 $txt['package_db_remove_table'] = 'Drop table &quot;%1$s&quot;';
 $txt['package_db_remove_column'] = 'Remove column &quot;%2$s&quot; from &quot;%1$s&quot;';
-$txt['package_db_remove_index'] = 'Remove index &quot;%1$s&quot; from &quot;%2$s&quot;';
+$txt['package_db_remove_index'] = 'Remove index &quot;%2$s&quot; from &quot;%1$s&quot;';
 
 $txt['package_advanced_button'] = 'Emulation Support';
 $txt['package_advanced_options'] = 'Advanced Options';
