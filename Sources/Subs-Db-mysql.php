ba42f7429db5e8e340c19b43f36bb86c2c58824c
albertlast <mathiaspapealbert@hotmail.com>
2019-02-22 06:17:11 +0100

    better sql mode handling
    Signed-off-by: albertlast albertlast@hotmail.de

--- Sources/Subs-Db-mysql.php
+++ Sources/Subs-Db-mysql.php
@@ -100,10 +100,11 @@ function smf_db_initiate($db_server, $db_name, $db_user, $db_passwd, $db_prefix,
 	if (empty($db_options['dont_select_db']) && !@mysqli_select_db($connection, $db_name) && empty($db_options['non_fatal']))
 		display_db_error();
 
+	$sql_mode = '';
 	// Set the mysql default mode
-	$sql_mode = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
+	$sql_mode .= (empyt($sql_mode) ? '' : ',') . 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
 	// Disable backlash escape, only '' is than a valid one
-	$sql_mode .= 'NO_BACKSLASH_ESCAPES';
+	$sql_mode .= (empyt($sql_mode) ? '' : ',') . 'NO_BACKSLASH_ESCAPES';
 
 	mysqli_query($connection, 'SET SESSION sql_mode = ' . $sql_mode);
 
