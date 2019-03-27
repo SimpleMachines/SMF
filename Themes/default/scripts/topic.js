b474c929a68b557f3e47054e7660670fa2675e30
SychO <sychocouldy@gmail.com>
2019-03-27 18:53:36 +0100

    Fix expanding attached images
    Signed-off-by: SychO <sychocouldy@gmail.com>

--- Themes/default/scripts/topic.js
+++ Themes/default/scripts/topic.js
@@ -678,29 +678,6 @@ InTopicModeration.prototype.handleSubmit = function (sSubmitType)
 
 
 // *** Other functions...
-function expandThumb(thumbID)
-{
-	var img = document.getElementById('thumb_' + thumbID);
-	var link = document.getElementById('link_' + thumbID);
-
-	// save the currently displayed image attributes
-	var tmp_src = img.src;
-	var tmp_height = img.style.height;
-	var tmp_width = img.style.width;
-
-	// set the displayed image attributes to the link attributes, this will expand in place
-	img.src = link.href;
-	img.style.width = link.style.width;
-	img.style.height = link.style.height;
-
-	// place the image attributes back
-	link.href = tmp_src;
-	link.style.width = tmp_width;
-	link.style.height = tmp_height;
-
-	return false;
-}
-
 function ignore_toggles(msgids, text)
 {
 	for (i = 0; i < msgids.length; i++)
