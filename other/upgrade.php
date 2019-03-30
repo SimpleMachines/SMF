491962c75042ef1f02de3417114707e4f910f7f2
SychO <sychocouldy@gmail.com>
2019-03-30 18:15:35 +0100

    Update lang version in upgrader
    Signed-off-by: SychO <sychocouldy@gmail.com>

--- other/upgrade.php
+++ other/upgrade.php
@@ -15,7 +15,7 @@
 define('SMF_VERSION', '2.1 RC2');
 define('SMF_FULL_VERSION', 'SMF ' . SMF_VERSION);
 define('SMF_SOFTWARE_YEAR', '2019');
-define('SMF_LANG_VERSION', '2.1 RC1');
+define('SMF_LANG_VERSION', '2.1 RC2');
 define('SMF_INSTALLING', 1);
 
 /**
