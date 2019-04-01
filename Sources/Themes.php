b2cccb48f48c316ea32f887849a8c75b84ac068e
John Rayes <live627@gmail.com>
2019-03-31 17:23:37 -0700

    simplify a query

--- Sources/Themes.php
+++ Sources/Themes.php
@@ -1050,12 +1050,10 @@ function PickTheme()
 			SELECT id_theme, variable, value
 			FROM {db_prefix}themes
 			WHERE variable IN ({string:name}, {string:theme_url}, {string:theme_dir}, {string:images_url}, {string:disable_user_variant})' . (!allowedTo('admin_forum') ? '
-				AND id_theme IN ({array_string:known_themes})' : '') . '
-				AND id_theme != {int:default_theme}
-				AND id_member = {int:no_member}
-				AND id_theme IN ({array_string:enable_themes})',
+				AND id_theme IN ({array_string:known_themes})' : '
+				AND id_theme IN ({array_string:enable_themes})') . '
+				AND id_member = {int:no_member}',
 			array(
-				'default_theme' => 0,
 				'name' => 'name',
 				'no_member' => 0,
 				'theme_url' => 'theme_url',
