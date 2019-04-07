9daa7bd1e6dfac5f1e7e092545286459adb65e62
albertlast <mathiaspapealbert@hotmail.com>
2019-04-07 21:46:21 +0200

    better text
    Signed-off-by: albertlast albertlast@hotmail.de

--- Sources/Subs-Db-mysql.php
+++ Sources/Subs-Db-mysql.php
@@ -777,9 +777,16 @@ function smf_db_insert($method = 'replace', $table, $columns, $data, $keys, $ret
 	$queryTitle = $method == 'replace' ? 'REPLACE' : ($method == 'ignore' ? 'INSERT IGNORE' : 'INSERT');
 
 	// Sanity check for replace is key part of the columns array
-	if ($method == 'replace' && (empty($keys) || count(array_intersect_key($columns, array_flip($keys))) !== count($keys)))
-		smf_db_error_backtrace('Primary Key field missing in insert call',
-				'Change the method of db insert to insert or add the pk field to the columns array', E_USER_ERROR, __FILE__, __LINE__);
+	if ($method == 'replace')
+		{
+			if (empty($keys))
+			smf_db_error_backtrace('When using the replace mode, the key column is a required entry.',
+					'Change the method of db insert to insert or add the pk field to the key array', E_USER_ERROR, __FILE__, __LINE__);
+			if (count(array_intersect_key($columns, array_flip($keys))) !== count($keys))
+				smf_db_error_backtrace('Primary Key field missing in insert call',
+					'Change the method of db insert to insert or add the pk field to the columns array', E_USER_ERROR, __FILE__, __LINE__);
+
+		}
 
 	if (!$with_returning || $method != 'ingore')
 	{
--- Sources/Subs-Db-postgresql.php
+++ Sources/Subs-Db-postgresql.php
@@ -707,9 +707,15 @@ function smf_db_insert($method = 'replace', $table, $columns, $data, $keys, $ret
 	$table = str_replace('{db_prefix}', $db_prefix, $table);
 
 	// Sanity check for replace is key part of the columns array
-	if ($method == 'replace' && (empty($keys) || count(array_intersect_key($columns, array_flip($keys))) !== count($keys)))
-		smf_db_error_backtrace('Primary Key field missing in insert call',
+	if ($method == 'replace')
+	{
+		if (empty($keys))
+		smf_db_error_backtrace('When using the replace mode, the key column is a required entry.',
+				'Change the method of db insert to insert or add the pk field to the key array', E_USER_ERROR, __FILE__, __LINE__);
+		if (count(array_intersect_key($columns, array_flip($keys))) !== count($keys))
+			smf_db_error_backtrace('Primary Key field missing in insert call',
 				'Change the method of db insert to insert or add the pk field to the columns array', E_USER_ERROR, __FILE__, __LINE__);
+	}			
 
 	// PostgreSQL doesn't support replace: we implement a MySQL-compatible behavior instead
 	if ($method == 'replace' || $method == 'ignore')
