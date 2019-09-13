9a4c77aa38f87bcfc384338e552f2f05051ef79f
Jon Stovell <jonstovell@gmail.com>
2019-09-12 18:39:54 -0600

    Does a better job checking for SVGs in the image proxy Since SVGs are not always detected as 'image/svg+xml', we sometimes have to do extra work. Previously, we handled this by assuming any plain text file with an '.svg' extension was an SVG. Now we check the content to see if it looks like one.
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

--- proxy.php
+++ proxy.php
@@ -231,7 +231,6 @@ class ProxyServer
 	protected function cacheImage($request)
 	{
 		$dest = $this->getCachedPath($request);
-		$ext = strtolower(pathinfo(parse_url($request, PHP_URL_PATH), PATHINFO_EXTENSION));
 
 		$image = fetch_web_data($request);
 
@@ -244,7 +243,7 @@ class ProxyServer
 		$mime_type = finfo_buffer($finfo, $image);
 
 		// SVG needs a little extra care
-		if ($ext == 'svg' && $mime_type == 'text/plain')
+		if (in_array($mime_type, array('text/plain', 'text/xml')) && strpos($image, '<svg') !== false && strpos($image, '</svg>') !== false)
 			$mime_type = 'image/svg+xml';
 
 		// Make sure the url is returning an image
