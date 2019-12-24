89e44582e86b1fcf2907fcf6111785fd1373b153
Jessica González <suki@missallsunday.com>
2019-12-24 13:29:43 -0600

    redis_cache class
    Signed-off-by: Jessica González <suki@missallsunday.com>

---
+++ Sources/CacheAPI-redis.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Simple Machines Forum (SMF)
+ *
+ * @package SMF
+ * @author Simple Machines http://www.simplemachines.org
+ * @copyright 2019 Simple Machines and individual contributors
+ * @license http://www.simplemachines.org/about/smf/license.php BSD
+ *
+ * @version 2.1 RC2
+ */
+
+if (!defined('SMF'))
+	die('No direct access...');
+
+/**
+ * Our Cache API class
+ *
+ * @package cacheAPI
+ */
+class redis_cache extends cache_api
+{
+
+}
+
+?>
