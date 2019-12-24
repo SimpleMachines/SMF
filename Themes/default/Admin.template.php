54044ea09b0ea1ba81f2af85afa891e9ef00db29
Jessica González <suki@missallsunday.com>
2019-12-24 10:54:57 -0600

    No new line for this
    Signed-off-by: Jessica González <suki@missallsunday.com>

--- Themes/default/Admin.template.php
+++ Themes/default/Admin.template.php
@@ -866,8 +866,7 @@ function template_show_settings()
 				// Show a selection box.
 				elseif ($config_var['type'] == 'select')
 				{
-					$select_size = !empty($config_var['size']) ? $config_var['size'] : (!empty($config_var['data']) && (count($config_var['data']) <= 4) ? count($config_var['data']) : 4)
-					;
+					$select_size = !empty($config_var['size']) ? $config_var['size'] : (!empty($config_var['data']) && (count($config_var['data']) <= 4) ? count($config_var['data']) : 4);
 
 					echo '
 										<select name="', $config_var['name'], '" id="', $config_var['name'], '" ', $javascript, $disabled, (!empty($config_var['multiple']) ? ' multiple="multiple"' : ''), (!empty($select_size) ? ' size="' . $select_size . '"' : ''), '>';
