37c214184304f0506847e70cda143bd877d24f7c
albertlast <mathiaspapealbert@hotmail.com>
2020-01-06 10:56:04 +0100

    Fix syntax
    Signed-off-by: albertlast albertlast@hotmail.de

--- proxy.php
+++ proxy.php
@@ -34,7 +34,7 @@ if (!defined('POSTGRE_TITLE'))
 if (!defined('MYSQL_TITLE'))
 	define('MYSQL_TITLE', 'MySQL');
 
-/if (!defined('SMF_USER_AGENT'))
+if (!defined('SMF_USER_AGENT'))
 	define('SMF_USER_AGENT', 'Mozilla/5.0 (' . php_uname('s') . ' ' . php_uname('m') . ') AppleWebKit/605.1.15 (KHTML, like Gecko)  SMF/' . strtr(SMF_VERSION, ' ', '.'));
 
 **
