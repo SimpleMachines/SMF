6eb3562ae152b983b43ceb1dd4a07b533f3bf58c
albertlast <mathiaspapealbert@hotmail.com>
2020-01-06 11:01:59 +0100

    fix
    Signed-off-by: albertlast albertlast@hotmail.de

--- proxy.php
+++ proxy.php
@@ -37,7 +37,7 @@ if (!defined('MYSQL_TITLE'))
 if (!defined('SMF_USER_AGENT'))
 	define('SMF_USER_AGENT', 'Mozilla/5.0 (' . php_uname('s') . ' ' . php_uname('m') . ') AppleWebKit/605.1.15 (KHTML, like Gecko)  SMF/' . strtr(SMF_VERSION, ' ', '.'));
 
-**
+/**
  * Class ProxyServer
  */
 class ProxyServer
