cf2f0dba681daded8d7c86ffe547c0457affd873
Jessica González <suki@missallsunday.com>
2020-01-16 17:01:06 -0600

    fix: add check for fileinfo ext on install
    Signed-off-by: Jessica González <suki@missallsunday.com>

--- other/install.php
+++ other/install.php
@@ -502,6 +502,10 @@ function Welcome()
 	if (!extension_loaded('mbstring'))
 		$incontext['error'] = $txt['install_no_mbstring'];
 
+	// Also Jim!
+    if (!extension_loaded('fileinfo'))
+        $incontext['error'] = $txt['install_no_fileinfo'];
+
 	// Check for https stream support.
 	$supported_streams = stream_get_wrappers();
 	if (!in_array('https', $supported_streams))
