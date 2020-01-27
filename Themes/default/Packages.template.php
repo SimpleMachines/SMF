5bc5a634cab3e60556d14676c4ae1ad28e3a2c99
Bugo <bugo@dragomano.ru>
2020-01-27 19:55:33 +0500

    Change a class for the second title

--- Themes/default/Packages.template.php
+++ Themes/default/Packages.template.php
@@ -467,8 +467,8 @@ function template_list()
 		<div class="cat_bar">
 			<h3 class="catbg">', $txt['list_file'], '</h3>
 		</div>
-		<div class="cat_bar">
-			<h3 class="catbg">', $txt['files_archive'], ' ', $context['filename'], ':</h3>
+		<div class="title_bar">
+			<h4 class="titlebg">', $txt['files_archive'], ' ', $context['filename'], ':</h4>
 		</div>
 		<div class="windowbg">
 			<ol>';
