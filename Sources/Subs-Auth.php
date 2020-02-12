aa921cb26e575f1cd372eb1b8a839911dbcfb2be
Jon Stovell <jonstovell@gmail.com>
2020-02-12 09:48:29 -0700

    Supercharges cookie security
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

--- Sources/Subs-Auth.php
+++ Sources/Subs-Auth.php
@@ -892,7 +892,14 @@ function hash_password($username, $password, $cost = null)
  */
 function hash_salt($password, $salt)
 {
-	return hash_hmac('sha1', sha1($password . $salt), get_auth_secret());
+	// Can we use SHA-3?
+	$algo = in_array('sha3-512', hash_algos()) ? 'sha3-512' : 'sha512';
+
+	// Use the salt to derive a unique key from the authentication secret.
+	$secret_key = hash_pbkdf2($algo, get_auth_secret(), $salt, 4000);
+
+	// Now use the derived key to generate an HMAC of the password.
+	return hash_hmac($algo, $password, $secret_key);
 }
 
 /**
@@ -941,6 +948,39 @@ function hash_benchmark($hashTime = 0.2)
 	while ($timeTaken < $hashTime);
 
 	return $cost;
+}
+
+if (!function_exists('hash_pbkdf2'))
+{
+	function hash_pbkdf2($algo, $password, $salt, $iterations, $length = 0, $raw_output = false)
+	{
+		if (!in_array(strtolower($algo), hash_algos()))
+			trigger_error(__FUNCTION__ . '(): Unknown hashing algorithm: ' . $algo, E_USER_WARNING);
+		if (!is_numeric($iterations))
+			trigger_error(__FUNCTION__ . '(): expects parameter 4 to be long, ' . gettype($iterations) . ' given', E_USER_WARNING);
+		if (!is_numeric($length))
+			trigger_error(__FUNCTION__ . '(): expects parameter 5 to be long, ' . gettype($length) . ' given', E_USER_WARNING);
+		if ($iterations <= 0)
+			trigger_error(__FUNCTION__ . '(): Iterations must be a positive integer: ' . $iterations, E_USER_WARNING);
+		if ($length < 0)
+			trigger_error(__FUNCTION__ . '(): Length must be greater than or equal to 0: ' . $length, E_USER_WARNING);
+
+		$output = '';
+		$block_count = $length ? ceil($length / strlen(hash($algo, '', $raw_output))) : 1;
+		for ($i = 1; $i <= $block_count; $i++)
+		{
+			$last = $xorsum = hash_hmac($algo, $salt . pack('N', $i), $password, true);
+			for ($j = 1; $j < $iterations; $j++)
+			{
+			    $xorsum ^= ($last = hash_hmac($algo, $last, $password, true));
+			}
+			$output .= $xorsum;
+		}
+
+		if (!$raw_output)
+			$output = bin2hex($output);
+		return $length ? substr($output, 0, $length) : $output;
+	}
 }
 
 ?>
