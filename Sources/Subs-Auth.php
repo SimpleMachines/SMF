ad35146b4a5a54520c2e3db7002ab95bb7b7f68b
Jon Stovell <jonstovell@gmail.com>
2020-02-15 20:08:51 -0700

    Dials back the overkill level in hash_salt()
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

--- Sources/Subs-Auth.php
+++ Sources/Subs-Auth.php
@@ -892,14 +892,11 @@ function hash_password($username, $password, $cost = null)
  */
 function hash_salt($password, $salt)
 {
-	// Can we use SHA-3?
-	$algo = in_array('sha3-512', hash_algos()) ? 'sha3-512' : 'sha512';
+	// Append the salt to get a user-specific authentication secret.
+	$secret_key = get_auth_secret() . $salt;
 
-	// Use the salt to derive a unique key from the authentication secret.
-	$secret_key = hash_pbkdf2($algo, get_auth_secret(), $salt, 4000);
-
-	// Now use the derived key to generate an HMAC of the password.
-	return hash_hmac($algo, $password, $secret_key);
+	// Now use that to generate an HMAC of the password.
+	return hash_hmac('sha512', $password, $secret_key);
 }
 
 /**
@@ -948,41 +945,6 @@ function hash_benchmark($hashTime = 0.2)
 	while ($timeTaken < $hashTime);
 
 	return $cost;
-}
-
-// Based on code by Trevor Herselman.
-// https://www.php.net/manual/en/function.hash-pbkdf2.php#118301
-if (!function_exists('hash_pbkdf2'))
-{
-	function hash_pbkdf2($algo, $password, $salt, $iterations, $length = 0, $raw_output = false)
-	{
-		if (!in_array(strtolower($algo), hash_algos()))
-			trigger_error(__FUNCTION__ . '(): Unknown hashing algorithm: ' . $algo, E_USER_WARNING);
-		if (!is_numeric($iterations))
-			trigger_error(__FUNCTION__ . '(): expects parameter 4 to be long, ' . gettype($iterations) . ' given', E_USER_WARNING);
-		if (!is_numeric($length))
-			trigger_error(__FUNCTION__ . '(): expects parameter 5 to be long, ' . gettype($length) . ' given', E_USER_WARNING);
-		if ($iterations <= 0)
-			trigger_error(__FUNCTION__ . '(): Iterations must be a positive integer: ' . $iterations, E_USER_WARNING);
-		if ($length < 0)
-			trigger_error(__FUNCTION__ . '(): Length must be greater than or equal to 0: ' . $length, E_USER_WARNING);
-
-		$output = '';
-		$block_count = $length ? ceil($length / strlen(hash($algo, '', $raw_output))) : 1;
-		for ($i = 1; $i <= $block_count; $i++)
-		{
-			$last = $xorsum = hash_hmac($algo, $salt . pack('N', $i), $password, true);
-			for ($j = 1; $j < $iterations; $j++)
-			{
-			    $xorsum ^= ($last = hash_hmac($algo, $last, $password, true));
-			}
-			$output .= $xorsum;
-		}
-
-		if (!$raw_output)
-			$output = bin2hex($output);
-		return $length ? substr($output, 0, $length) : $output;
-	}
 }
 
 // Based on code by "s rotondo90 at gmail com".
