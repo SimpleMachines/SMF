2471c9f7e09c1d170103b716fa91ef49516f0507
John Rayes <live627@gmail.com>
2020-02-15 23:03:55 -0700

    phpdoc markdown format

--- Sources/Class-CurlFetchWeb.php
+++ Sources/Class-CurlFetchWeb.php
@@ -19,22 +19,47 @@ if (!defined('SMF'))
  * Properly redirects even with safe mode and basedir restrictions
  * Can provide simple post options to a page
  *
- * Load class
+ * ### Load class
  * Initiate as
- *   <code>$fetch_data = new cURL_fetch_web_data();</code>
- *   optionally pass an array of cURL options and redirect count
- *   <code>$fetch_data = new cURL_fetch_web_data(array(CURLOPT_SSL_VERIFYPEER => 1), 5);</code>
+ * ```
+ * $fetch_data = new cURL_fetch_web_data();
+ * ```
+ * Optionally pass an array of cURL options and redirect count
+ * ```
+ * $fetch_data = new cURL_fetch_web_data(array(CURLOPT_SSL_VERIFYPEER => 1), 5);
+ * ```
  *
- * Make the call
- *   <code>$fetch_data->get_url_data('https://www.simplemachines.org'); // fetch a page</code>
- *   <code>$fetch_data->get_url_data('https://www.simplemachines.org', array('user' => 'name', 'password' => 'password')); // post to a page</code>
- *   <code>$fetch_data->get_url_data('https://www.simplemachines.org', parameter1&parameter2&parameter3); // post to a page</code>
+ * ### Make the call
+ * Fetch a page
+ * ```
+ * $fetch_data->get_url_data('https://www.simplemachines.org');
+ * ```
+ * Post to a page providing an array
+ * ```
+ * $fetch_data->get_url_data('https://www.simplemachines.org', array('user' => 'name', 'password' => 'password'));
+ * ```
+ * Post to a page providing a string
+ * ```
+ * $fetch_data->get_url_data('https://www.simplemachines.org', parameter1&parameter2&parameter3);
+ * ```
  *
- * Get the data
- *   <code>$fetch_data->result('body'); // just the page content</code>
- *   <code>$fetch_data->result(); // an array of results, body, header, http result codes</code>
- *   <code>$fetch_data->result_raw(); // show all results of all calls (in the event of a redirect)</code>
- *   <code>$fetch_data->result_raw(0); // show all results of call x</code>
+ * ### Get the data
+ * Just the page content
+ * ```
+ * $fetch_data->result('body');
+ * ```
+ * An array of results, body, header, http result codes
+ * ```
+ * $fetch_data->result();
+ * ```
+ * Show all results of all calls (in the event of a redirect)
+ * ```
+ * $fetch_data->result_raw();
+ * ```
+ * Show the results of a specific call (in the event of a redirect)
+ * ```
+ * $fetch_data->result_raw(0);
+ * ```
  */
 class curl_fetch_web_data
 {
