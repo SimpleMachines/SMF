c4a80e6302cb0059c9601d58a4656527ced5f6f7
Jessica González <suki@missallsunday.com>
2020-03-02 16:41:45 -0600

    fix: set a new key "value"
    Signed-off-by: Jessica González <suki@missallsunday.com>

--- Sources/Subs-List.php
+++ Sources/Subs-List.php
@@ -105,16 +105,16 @@ function createList($listOptions)
 	$list_context['num_columns'] = count($listOptions['columns']);
 	$list_context['width'] = isset($listOptions['width']) ? $listOptions['width'] : '0';
 
-	// Get the file with the function for the item list.
-	if (isset($listOptions['get_items']['file']))
-		require_once($listOptions['get_items']['file']);
-
 	// Call the function and include which items we want and in what order.
-	if (!is_callable($listOptions['get_items']['function']) && is_array($listOptions['get_items']['function']))
-		$list_items = $listOptions['get_items']['function'];
+	if (!empty($listOptions['get_items']['value']) && is_array($listOptions['get_items']['value']))
+		$list_items = $listOptions['get_items']['value'];
 
 	else
 	{
+		// Get the file with the function for the item list.
+		if (isset($listOptions['get_items']['file']))
+			require_once($listOptions['get_items']['file']);
+
 		$call = call_helper($listOptions['get_items']['function'], true);
 		$list_items = call_user_func_array($call, array_merge(array($list_context['start'], $list_context['items_per_page'], $sort), empty($listOptions['get_items']['params']) ? array() : $listOptions['get_items']['params']));
 		$list_items = empty($list_items) ? array() : $list_items;
