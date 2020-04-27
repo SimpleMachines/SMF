8b323f694a4dc8c620ec8206dc45a4f5d805b151
albertlast <mathiaspapealbert@hotmail.com>
2020-04-27 16:15:35 +0200

    License File
    Signed-off-by: albertlast albertlast@hotmail.de

--- tests/installTest.php
+++ tests/installTest.php
@@ -1,5 +1,19 @@
 <?php
 
+/**
+ * The main purpose of this file is to show a list of all errors that were
+ * logged on the forum, and allow filtering and deleting them.
+ *
+ * Simple Machines Forum (SMF)
+ *
+ * @package SMF
+ * @author Simple Machines https://www.simplemachines.org
+ * @copyright 2020 Simple Machines and individual contributors
+ * @license https://www.simplemachines.org/about/smf/license.php BSD
+ *
+ * @version 2.1 RC2
+ */
+
 define('SMFPHPUNIT',TRUE);
 use PHPUnit\Framework\TestCase;
 require_once('.\install.php');
