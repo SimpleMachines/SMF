167bf60ff5727b68e7f90b44b81cfbb218eae677
MisterVector <whizkid3000@hotmail.com>
2020-05-06 08:15:06 -0700

    Added missing global $context
    Signed-off-by: MisterVector <whizkid3000@hotmail.com>

--- Sources/Modlog.php
+++ Sources/Modlog.php
@@ -371,7 +371,7 @@ function list_getModLogEntryCount($query_string = '', $query_params = array(), $
  */
 function list_getModLogEntries($start, $items_per_page, $sort, $query_string = '', $query_params = array(), $log_type = 1, $ignore_boards = false)
 {
-	global $scripturl, $txt, $smcFunc, $user_info;
+	global $scripturl, $txt, $smcFunc, $user_info, $context;
 
 	$modlog_query = allowedTo('admin_forum') || $user_info['mod_cache']['bq'] == '1=1' ? '1=1' : (($user_info['mod_cache']['bq'] == '0=1' || $ignore_boards) ? 'lm.id_board = 0 AND lm.id_topic = 0' : (strtr($user_info['mod_cache']['bq'], array('id_board' => 'b.id_board')) . ' AND ' . strtr($user_info['mod_cache']['bq'], array('id_board' => 't.id_board'))));
 
