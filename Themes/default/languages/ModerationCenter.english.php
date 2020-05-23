965dca99e06ea322c18f49c69f5369ff9c5fe4ce
Christopher 'm4z' Holm <them4z@gmail.com>
2020-05-23 22:27:56 +0200

    Single-string-ify
    Signed-off-by: Christopher 'm4z' Holm <them4z@gmail.com>

--- Themes/default/languages/ModerationCenter.english.php
+++ Themes/default/languages/ModerationCenter.english.php
@@ -153,7 +153,7 @@ $txt['report_action_close_all'] = 'You successfully closed all selected reports.
 $txt['mc_warning_template_desc'] = 'Use this page to fill in the details of the template. Note that the subject for the email is not part of the template. Note that as the notification is sent by PM you can use BBC within the template. Note if you use the {MESSAGE} variable then this template will not be available when issuing a generic warning (i.e. A warning not linked to a post).';
 $txt['mc_warning_template_title'] = 'Template Title';
 $txt['mc_warning_template_body_desc'] = 'The content of the notification message. Note that you can use the following shortcuts in this template.<ul style="margin-top: 0px;"><li>{MEMBER} - Member Name.</li><li>{MESSAGE} - Link to Offending Post. (If Applicable)</li><li>{FORUMNAME} - Forum Name.</li><li>{SCRIPTURL} - Web address of forum.</li><li>{REGARDS} - Standard email sign-off.</li></ul>';
-$txt['mc_warning_template_body_default'] = '{MEMBER},' . "\n\n" . 'You have received a warning for inappropriate activity. Please cease these activities and abide by the forum rules otherwise we will take further action.' . "\n\n" . '{REGARDS}';
+$txt['mc_warning_template_body_default'] = "{MEMBER},\n\nYou have received a warning for inappropriate activity. Please cease these activities and abide by the forum rules otherwise we will take further action.\n\n{REGARDS}";
 $txt['mc_warning_template_personal'] = 'Personal Template';
 $txt['mc_warning_template_personal_desc'] = 'If you select this option only you will be able to see, edit and use this template. If not selected all moderators will be able to use this template.';
 $txt['mc_warning_template_error_no_title'] = 'You must set a title.';
