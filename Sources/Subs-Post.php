ce22bcd9ee0e90c6539d0e964902d84cf399f182
Christopher 'm4z' Holm <them4z@gmail.com>
2020-05-24 12:59:42 +0200

    Fix unused global variable
    Signed-off-by: Christopher 'm4z' Holm <them4z@gmail.com>

--- Sources/Subs-Post.php
+++ Sources/Subs-Post.php
@@ -2796,7 +2796,7 @@ function adminNotify($type, $memberID, $member_name = null)
  */
 function loadEmailTemplate($template, $replacements = array(), $lang = '', $loadLang = true)
 {
-	global $txt, $mbname, $scripturl, $settings;
+	global $txt, $mbname, $scripturl, $settings, $context;
 
 	// First things first, load up the email templates language file, if we need to.
 	if ($loadLang)
