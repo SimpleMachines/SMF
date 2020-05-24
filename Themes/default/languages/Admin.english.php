9b00aece65bbff714a351145c0fb9e398c20be64
Christopher 'm4z' Holm <them4z@gmail.com>
2020-05-24 17:38:27 +0200

    Revert "Fix #6132"
    This reverts commit 35ca7f70adab704fe5df2189a9df9f0fb061d318.

--- Themes/default/languages/Admin.english.php
+++ Themes/default/languages/Admin.english.php
@@ -359,6 +359,7 @@ $txt['attachment_security_settings'] = 'Attachment security settings';
 
 $txt['attach_dir_does_not_exist'] = 'Does not exist';
 $txt['attach_dir_not_writable'] = 'Not writable';
+$txt['attach_dir_files_missing'] = 'Files Missing (<a href="' . $scripturl . '?action=admin;area=manageattachments;sa=repair;%2$s=%1$s">Repair</a>)';
 $txt['attach_dir_unused'] = 'Unused';
 $txt['attach_dir_empty'] = 'Empty';
 $txt['attach_dir_ok'] = 'OK';
