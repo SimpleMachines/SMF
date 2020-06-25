cac794347d82a3d7d5adbdcd3c8554a6a3020d55
Jon Stovell <jonstovell@gmail.com>
2020-06-25 12:32:31 -0600

    Allows a more conservative range for export_rate
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

--- Sources/ManageServer.php
+++ Sources/ManageServer.php
@@ -797,7 +797,7 @@ function ModifyExportSettings($return_config = false)
 		array('text', 'export_dir', 40),
 		array('int', 'export_expiry', 'subtext' => $txt['zero_to_disable'], 'postinput' => $txt['days_word']),
 		array('int', 'export_min_diskspace_pct', 'postinput' => '%', 'max' => 80, 'disabled' => $diskspace_disabled),
-		array('int', 'export_rate', 'min' => 50, 'max' => 5000, 'step' => 50, 'subtext' => $txt['export_rate_desc']),
+		array('int', 'export_rate', 'min' => 5, 'max' => 500, 'step' => 5, 'subtext' => $txt['export_rate_desc']),
 	);
 
 	call_integration_hook('integrate_export_settings', array(&$config_vars));
@@ -814,6 +814,8 @@ function ModifyExportSettings($return_config = false)
 
 		if ($diskspace_disabled)
 			$_POST['export_min_diskspace_pct'] = 0;
+
+		$_POST['export_rate'] = max(5, min($_POST['export_rate'], 500));
 
 		saveDBSettings($config_vars);
 
