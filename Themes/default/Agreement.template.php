fe9db0f8b3a8f64d3e800955421b6b68cca4820a
Oscar Rydhé <oscar.rydhe@sony.com>
2021-01-12 09:08:34 +0100

    Update to 2021
    Fixes #6445
    
    Signed-off-by: Oscar Rydhé oscar.rydhe@gmail.com

--- Themes/default/Agreement.template.php
+++ Themes/default/Agreement.template.php
@@ -5,7 +5,7 @@
  *
  * @package SMF
  * @author Simple Machines https://www.simplemachines.org
- * @copyright 2020 Simple Machines and individual contributors
+ * @copyright 2021 Simple Machines and individual contributors
  * @license https://www.simplemachines.org/about/smf/license.php BSD
  *
  * @version 2.1 RC3
