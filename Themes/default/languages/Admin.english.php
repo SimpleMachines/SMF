93f4ddb1a21038ca5a93af4b7a72b563717262d6
Jon Stovell <jonstovell@gmail.com>
2021-06-27 18:06:59 -0600

    Shows GDPR warning to admin for 'notify_announcements' default setting
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

--- Themes/default/languages/Admin.english.php
+++ Themes/default/languages/Admin.english.php
@@ -746,6 +746,7 @@ $txt['mention'] = 'Membergroups allowed to mention users';
 $txt['notifications'] = 'Notifications';
 $txt['notify_settings'] = 'Notification Settings';
 $txt['notifications_desc'] = 'This page allows you to set the default notification options for users.';
+$txt['notify_announcements_desc'] = 'Enabling this default option violates the rules of the <a href="https://ec.europa.eu/info/law/law-topic/data-protection_en" target="_blank" rel="noopener" class="bbc_link">GDPR</a> and many other countries\' privacy and anti-spam laws.';
 
 $txt['enable_sm_stats'] = 'Allow Stat Collection';
 
