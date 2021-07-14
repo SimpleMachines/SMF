5aa1e6c90d32a8503bc59610494ef9bc1f8e5e86
Jon Stovell <jonstovell@gmail.com>
2021-07-14 10:18:21 -0600

    Fixes error message when export dir could not be created
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

--- Sources/Profile-Export.php
+++ Sources/Profile-Export.php
@@ -747,7 +747,7 @@ function create_export_dir($fallback = '')
 		// Try again at the fallback location.
 		if ($modSettings['export_dir'] != $fallback)
 		{
-			log_error($txt['export_dir_forced_change'], $modSettings['export_dir'], $fallback);
+			log_error(sprintf($txt['export_dir_forced_change'], $modSettings['export_dir'], $fallback));
 			updateSettings(array('export_dir' => $fallback));
 
 			// Secondary fallback will be the default location, so no parameter this time.
