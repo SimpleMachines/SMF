f36aeb15ca4f90f6053a5dec01352e074a2cdc51
sbulen <bulens@pacbell.net>
2021-08-14 09:47:58 -0700

    Confirm jquery_source is set Signed by Shawn Bulen, bulens@pacbell.net

--- Sources/Load.php
+++ Sources/Load.php
@@ -2472,7 +2472,11 @@ function loadTheme($id_theme = 0, $initialize = true)
 
 	// Add the JQuery library to the list of files to load.
 	$jQueryUrls = array ('cdn' => 'https://ajax.googleapis.com/ajax/libs/jquery/'. JQUERY_VERSION . '/jquery.min.js', 'jquery_cdn' => 'https://code.jquery.com/jquery-'. JQUERY_VERSION . '.min.js', 'microsoft_cdn' => 'https://ajax.aspnetcdn.com/ajax/jQuery/jquery-'. JQUERY_VERSION . '.min.js');
-	
+
+	// If nothing set yet, default to the local copy
+	if (!isset($modSettings['jquery_source']))
+		$modSettings['jquery_source'] = 'local';
+
 	if (array_key_exists($modSettings['jquery_source'], $jQueryUrls))
 		loadJavaScriptFile($jQueryUrls[$modSettings['jquery_source']], array('external' => true, 'seed' => false), 'smf_jquery');
 
