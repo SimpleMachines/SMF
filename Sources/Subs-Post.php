6352c16eee3d51e0600c9febc0becb5adb4bb064
Oscar Rydhé <oscar.rydhe@gmail.com>
2021-08-18 09:11:42 +0200

    Fix Undefined index: approved
    Fixes #6948
    
    Signed-off-by: Oscar Rydhé <oscar.rydhe@gmail.com>

--- Sources/Subs-Post.php
+++ Sources/Subs-Post.php
@@ -2345,7 +2345,7 @@ function modifyPost(&$msgOptions, &$topicOptions, &$posterOptions)
 		$searchAPI->postModified($msgOptions, $topicOptions, $posterOptions);
 
 	// Send notifications about any new quotes or mentions.
-	if ($msgOptions['send_notifications'] && $msgOptions['approved'] && (!empty($msgOptions['quoted_members']) || !empty($msgOptions['mentioned_members'])))
+	if ($msgOptions['send_notifications'] && !empty($msgOptions['approved']) && (!empty($msgOptions['quoted_members']) || !empty($msgOptions['mentioned_members'])))
 	{
 		$smcFunc['db_insert']('',
 			'{db_prefix}background_tasks',
