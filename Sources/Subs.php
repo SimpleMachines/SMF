d0083e8e833bd5433642f080619a7fe78babd0d5
Jon Stovell <jonstovell@gmail.com>
2021-08-19 19:35:43 -0600

    Ensures autolinker character classes are defined for email autolinker Fixes #6985
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

--- Sources/Subs.php
+++ Sources/Subs.php
@@ -2360,21 +2360,21 @@ function parse_bbc($message, $smileys = true, $cache_id = '', $parse_tags = arra
 						$data = strtr($data, array('&nbsp;' => sprintf($placeholder_template, 'nbsp')));
 					}
 
+					// Some reusable character classes
+					$space_chars = ($context['utf8'] ? '\p{Z}' : '\s');
+					$excluded_trailing_chars = '!;:.,?';
+					$domain_label_chars = '0-9A-Za-z\-' . ($context['utf8'] ? implode('', array(
+						'\x{A0}-\x{D7FF}', '\x{F900}-\x{FDCF}', '\x{FDF0}-\x{FFEF}',
+						'\x{10000}-\x{1FFFD}', '\x{20000}-\x{2FFFD}', '\x{30000}-\x{3FFFD}',
+						'\x{40000}-\x{4FFFD}', '\x{50000}-\x{5FFFD}', '\x{60000}-\x{6FFFD}',
+						'\x{70000}-\x{7FFFD}', '\x{80000}-\x{8FFFD}', '\x{90000}-\x{9FFFD}',
+						'\x{A0000}-\x{AFFFD}', '\x{B0000}-\x{BFFFD}', '\x{C0000}-\x{CFFFD}',
+						'\x{D0000}-\x{DFFFD}', '\x{E1000}-\x{EFFFD}',
+					)) : '');
+
 					// Parse any URLs
 					if (!isset($disabled['url']) && strpos($data, '[url') === false)
 					{
-						// Some reusable character classes
-						$space_chars = ($context['utf8'] ? '\p{Z}' : '\s');
-						$excluded_trailing_chars = '!;:.,?';
-						$domain_label_chars = '0-9A-Za-z\-' . ($context['utf8'] ? implode('', array(
-							'\x{A0}-\x{D7FF}', '\x{F900}-\x{FDCF}', '\x{FDF0}-\x{FFEF}',
-							'\x{10000}-\x{1FFFD}', '\x{20000}-\x{2FFFD}', '\x{30000}-\x{3FFFD}',
-							'\x{40000}-\x{4FFFD}', '\x{50000}-\x{5FFFD}', '\x{60000}-\x{6FFFD}',
-							'\x{70000}-\x{7FFFD}', '\x{80000}-\x{8FFFD}', '\x{90000}-\x{9FFFD}',
-							'\x{A0000}-\x{AFFFD}', '\x{B0000}-\x{BFFFD}', '\x{C0000}-\x{CFFFD}',
-							'\x{D0000}-\x{DFFFD}', '\x{E1000}-\x{EFFFD}',
-						)) : '');
-
 						// URI schemes that require some sort of special handling.
 						$schemes = array(
 							// Schemes whose URI definitions require a domain name in the
