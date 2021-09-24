0adbe34483a4f7ce5dd2c27296a02495b46cd782
sbulen <bulens@pacbell.net>
2021-09-23 19:21:19 -0700

    Only utf8 going forward Signed by Shawn Bulen, bulens@pacbell.net

--- Sources/Load.php
+++ Sources/Load.php
@@ -272,14 +272,11 @@ function reloadSettings()
 
 			return random_bytes($length);
 		},
-		'normalize' => function($string, $form = 'c') use ($utf8)
+		'normalize' => function($string, $form = 'c')
 		{
 			global $sourcedir;
 
 			$string = (string) $string;
-
-			if (!$utf8)
-				return $string;
 
 			require_once($sourcedir . '/Subs-Charset.php');
 
