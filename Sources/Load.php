dd6c626d5f85764986970a4b4ba9cc7406459132
Jessica González <suki@missallsunday.com>
2021-09-27 09:40:15 -0500

    fix: Add bt to the list of allowed HTML tags for descriptions
    Signed-off-by: Jessica González <suki@missallsunday.com>

--- Sources/Load.php
+++ Sources/Load.php
@@ -439,7 +439,7 @@ function reloadSettings()
 
 	// Define a list of allowed tags for descriptions.
 	$context['description_allowed_tags'] = array(
-		'abbr', 'anchor', 'b', 'center', 'color', 'font', 'hr', 'i', 'img',
+		'abbr', 'anchor', 'b', 'br', 'center', 'color', 'font', 'hr', 'i', 'img',
 		'iurl', 'left', 'li', 'list', 'ltr', 'pre', 'right', 's', 'sub',
 		'sup', 'table', 'td', 'tr', 'u', 'url',
 	);
@@ -2489,7 +2489,7 @@ function loadTheme($id_theme = 0, $initialize = true)
 
 	// Add the JQuery library to the list of files to load.
 	$jQueryUrls = array ('cdn' => 'https://ajax.googleapis.com/ajax/libs/jquery/'. JQUERY_VERSION . '/jquery.min.js', 'jquery_cdn' => 'https://code.jquery.com/jquery-'. JQUERY_VERSION . '.min.js', 'microsoft_cdn' => 'https://ajax.aspnetcdn.com/ajax/jQuery/jquery-'. JQUERY_VERSION . '.min.js');
-	
+
 	if (isset($modSettings['jquery_source']) && array_key_exists($modSettings['jquery_source'], $jQueryUrls))
 		loadJavaScriptFile($jQueryUrls[$modSettings['jquery_source']], array('external' => true, 'seed' => false), 'smf_jquery');
 
@@ -2738,7 +2738,7 @@ function loadCSSFile($fileName, $params = array(), $id = '')
 	$params['validate'] = isset($params['validate']) ? $params['validate'] : true;
 	$params['order_pos'] = isset($params['order_pos']) ? (int) $params['order_pos'] : 3000;
 	$params['attributes'] = isset($params['attributes']) ? $params['attributes'] : array();
-	
+
 	// Account for shorthand like admin.css?alp21 filenames
 	$id = (empty($id) ? strtr(str_replace('.css', '', basename($fileName)), '?', '_') : $id) . '_css';
 
