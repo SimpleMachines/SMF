992f04b33faf2f0286c49997f2230576b78c6297
John Rayes <live627@gmail.com>
2021-10-05 23:25:40 -0700

    Update Sources/ManageMaintenance.php
    Co-authored-by: Jon Stovell <jonstovell@gmail.com>

--- Sources/ManageMaintenance.php
+++ Sources/ManageMaintenance.php
@@ -2225,7 +2225,7 @@ function get_defined_functions_in_file(string $file): array
 {
 	$source = file_get_contents($file);
 	// token_get_all() is too slow so use a nice little regex instead.
-	preg_match_all('/^namespace\s([a-z\\\]++)\s*;|class\s+(\w++)[\w\s]*{|function\s+(\w++)\s*\(.*\)[:\|\w\s]*{/im', $source, $matches, PREG_SET_ORDER);
+	preg_match_all('/\bnamespace\s++((?P>label)(?:\\\(?P>label))*+)\s*+;|\bclass\s++((?P>label))[\w\s]*+{|\bfunction\s++((?P>label))\s*+\(.*\)[:\|\w\s]*+{(?(DEFINE)(?<label>[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*+))/i', $source, $matches, PREG_SET_ORDER);
 
 	$functions = array();
 	$namespace = '';
