ce1716f869e77c6f075283d10ed2eea52b47346e
albertlast <mathiaspapealbert@hotmail.com>
2022-02-02 06:31:43 +0100

    PG Upgrade process missing on conflict checks
    Signed-off-by: albertlast albertlast@hotmail.de

--- other/upgrade_2-1_postgresql.sql
+++ other/upgrade_2-1_postgresql.sql
@@ -496,10 +496,10 @@ INSERT INTO {$db_prefix}settings (variable, value) VALUES ('defaultMaxListItems'
 ---#
 
 ---# Adding new profile data export settings
-INSERT INTO {$db_prefix}settings (variable, value) VALUES ('export_dir', '{$boarddir}/exports');
-INSERT INTO {$db_prefix}settings (variable, value) VALUES ('export_expiry', '7');
-INSERT INTO {$db_prefix}settings (variable, value) VALUES ('export_min_diskspace_pct', '5');
-INSERT INTO {$db_prefix}settings (variable, value) VALUES ('export_rate', '250');
+INSERT INTO {$db_prefix}settings (variable, value) VALUES ('export_dir', '{$boarddir}/exports') ON CONFLICT DO NOTHING;
+INSERT INTO {$db_prefix}settings (variable, value) VALUES ('export_expiry', '7') ON CONFLICT DO NOTHING;
+INSERT INTO {$db_prefix}settings (variable, value) VALUES ('export_min_diskspace_pct', '5') ON CONFLICT DO NOTHING;
+INSERT INTO {$db_prefix}settings (variable, value) VALUES ('export_rate', '250') ON CONFLICT DO NOTHING;
 ---#
 
 ---# Adding settings for marking boards as read
