f3d65982efc0f8259c45c36de0c0ca11dd0b73f3
Jon Stovell <jonstovell@gmail.com>
2022-02-15 19:32:03 -0700

    Apply tokenTxtReplace to custom profile field names in member search
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

--- Sources/Memberlist.php
+++ Sources/Memberlist.php
@@ -575,7 +575,7 @@ function MLSearch()
 		}
 
 		foreach ($context['custom_search_fields'] as $field)
-			$context['search_fields']['cust_' . $field['colname']] = sprintf($txt['mlist_search_by'], $field['name']);
+			$context['search_fields']['cust_' . $field['colname']] = sprintf($txt['mlist_search_by'], tokenTxtReplace($field['name']));
 
 		$context['sub_template'] = 'search';
 		$context['old_search'] = isset($_GET['search']) ? $_GET['search'] : (isset($_POST['search']) ? $smcFunc['htmlspecialchars']($_POST['search']) : '');
