02148addf10323ef19fdc7464b143a1665d79b26
Jon Stovell <jonstovell@gmail.com>
2022-02-16 11:57:14 -0700

    Always shows attachment file size restrictions on posting page
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

--- Themes/default/languages/Post.english.php
+++ Themes/default/languages/Post.english.php
@@ -78,10 +78,10 @@ $txt['report_profile'] = 'Report profile of \'%1$s\'';
 $txt['reported_profile'] = 'Reported user';
 $txt['report_following_user'] = 'The profile of "%1$s" at %2$s';
 
-$txt['attach_restrict_attachmentPostLimit'] = 'maximum total size %1$d KB';
-$txt['attach_restrict_attachmentPostLimit_MB'] = 'maximum total size %1$d MB';
-$txt['attach_restrict_attachmentSizeLimit'] = 'maximum individual size %1$d KB';
-$txt['attach_restrict_attachmentSizeLimit_MB'] = 'maximum individual size %1$d MB';
+$txt['attach_restrict_attachmentPostLimit'] = 'maximum total size %1$s KB';
+$txt['attach_restrict_attachmentPostLimit_MB'] = 'maximum total size %1$s MB';
+$txt['attach_restrict_attachmentSizeLimit'] = 'maximum individual size %1$s KB';
+$txt['attach_restrict_attachmentSizeLimit_MB'] = 'maximum individual size %1$s MB';
 $txt['attach_restrict_attachmentNumPerPostLimit'] = '%1$d per post';
 $txt['attach_restrictions'] = 'Restrictions:';
 
@@ -174,7 +174,7 @@ $txt['attach_folder_warning'] = 'The attachments directory can not be located. P
 $txt['attach_folder_admin_warning'] = 'The path to the attachments directory (%1$s) is incorrect. Please correct it in the attachment settings area of your admin panel.';
 $txt['attach_limit_nag'] = 'You have reached the maximum number of attachments allowed per post.';
 $txt['attach_no_upload'] = 'There was a problem and your attachments could not be uploaded';
-$txt['attach_remaining'] = '<span class="attach_remaining">%1$d</span> attachments remaining';
+$txt['attach_remaining'] = '<span class="attach_remaining">%1$d</span> remaining';
 $txt['attach_available'] = '%1$s KB available';
 $txt['attach_kb'] = ' (%1$s KB)';
 $txt['attach_0_byte_file'] = 'The file appears to be empty. Please contact your forum administrator if this continues to be a problem';
