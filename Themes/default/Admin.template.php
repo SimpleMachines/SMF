4fedd1d755b4510e1f352eaf5385d722405f5fcf
Bugo <bugo@dragomano.ru>
2022-04-04 12:49:43 +0500

    Add js color picker for color option type
    Signed-off-by: Bugo <bugo@dragomano.ru>

--- Themes/default/Admin.template.php
+++ Themes/default/Admin.template.php
@@ -709,7 +709,7 @@ function template_not_done()
  */
 function template_show_settings()
 {
-	global $context, $txt, $scripturl;
+	global $context, $txt, $scripturl, $settings;
 
 	if (!empty($context['saved_successful']))
 		echo '
@@ -893,6 +893,14 @@ function template_show_settings()
 					echo '
 										</select>';
 				}
+				// Show a color box.
+				elseif ($config_var['type'] == 'color')
+				{
+					$load_color_js = true;
+
+					echo '
+										<input name="', $config_var['name'], '" id="', $config_var['name'], '" data-jscolor="{}" value="', $config_var['value'], '">';
+				}
 
 				// List of boards? This requires getBoardList() having been run and the results in $context['board_list'].
 				elseif ($config_var['type'] == 'boards')
@@ -1054,6 +1062,10 @@ function template_show_settings()
 			$(this).closest("fieldset").hide().prev("a").show();
 		});
 	', true);
+
+	if (!empty($load_color_js))
+		echo '
+		<script src="', $settings['default_theme_url'] . '/scripts/jscolor.min.js" defer></script>';
 }
 
 /**
