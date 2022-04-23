cfec22c794f9d456f06002df09011a9ed779cd73
Jeremy D <unmonitored+github@sleepycode.com>
2022-04-23 08:14:09 -0700

    Prevent minified files from being captured into backups, they will be generated later Add proxy.php to the backup as it is missing from index files.

--- Sources/Subs-Package.php
+++ Sources/Subs-Package.php
@@ -2926,7 +2926,7 @@ function package_create_backup($id = 'backup')
 
 	$files = array();
 
-	$base_files = array('index.php', 'SSI.php', 'agreement.txt', 'cron.php', 'ssi_examples.php', 'ssi_examples.shtml', 'subscriptions.php');
+	$base_files = array('index.php', 'SSI.php', 'agreement.txt', 'cron.php', 'proxy.php', 'ssi_examples.php', 'ssi_examples.shtml', 'subscriptions.php');
 	foreach ($base_files as $file)
 	{
 		if (file_exists($boarddir . '/' . $file))
@@ -2966,7 +2966,7 @@ function package_create_backup($id = 'backup')
 				if ($dir->isDir())
 					continue;
 
-				if (preg_match('~^(\.{1,2}|CVS|backup.*|help|images|.*\~)$~', $entry) != 0)
+				if (preg_match('~^(\.{1,2}|CVS|backup.*|help|images|.*\~|.*minified_[a-z0-9]{32}\.(js|css))$~', $entry) != 0)
 					continue;
 
 				$files[empty($_REQUEST['use_full_paths']) ? str_replace(realpath($boarddir), '', $entry) : $entry] = $entry;
