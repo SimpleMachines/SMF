e5c983b89f0f19f2fed147e096e43582fcc9e53b
Jeremy D <unmonitored+github@sleepycode.com>
2022-04-27 18:43:10 -0700

    On create_table functions, the columns specified may not have their name on the key, so search for it the hard way.

--- Sources/DbPackages-mysql.php
+++ Sources/DbPackages-mysql.php
@@ -158,7 +158,6 @@ function smf_db_create_table($table_name, $columns, $indexes = array(), $paramet
 		$table_query .= "\n\t" . smf_db_create_query_column($column) . ',';
 
 	// Loop through the indexes next...
-	$version = $smcFunc['db_get_version']();
 	foreach ($indexes as $index)
 	{
 		// MySQL If its a text column, we need to add a size.
@@ -167,14 +166,11 @@ function smf_db_create_table($table_name, $columns, $indexes = array(), $paramet
 			$c = trim($c);
 
 			// If a size was already specified, we won't be able to match it anyways.
-			if (!isset($columns[$c]) || !in_array($columns[$c]['type'], array('text', 'mediumntext', 'largetext')))
+			$key = array_search($c, array_column($columns, 'name'));
+			if ($key !== false && !isset($columns[$key]) || !in_array($columns[$key]['type'], array('text', 'mediumntext', 'largetext', 'varchar', 'char')))
 				continue;
 
-			// This is a column we need a size on and we are below 5.7 we have to stick to a smaller size.
-			if (version_compare($version, '5.7', '<'))
-				$c .= '(64)';
-			else
-				$c .= '(255)';
+			$c .= '(191)';
 		}
 
 		$idx_columns = implode(',', $index['columns']);
