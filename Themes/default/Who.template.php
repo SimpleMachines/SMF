6ad7e26c5183c37b5aba25ad0d8b974c0ae584ee
Diego Andrés <diegoandres_cortes@outlook.com>
2022-06-08 16:21:30 -0500

    add semicolon
    signed-off-by: diego andrés <diegoandres_cortes@outlook.com>

--- Themes/default/Who.template.php
+++ Themes/default/Who.template.php
@@ -66,7 +66,7 @@ function template_main()
 
 		echo '
 								<span class="member', $member['is_hidden'] ? ' hidden' : '', '">
-									', $member['is_guest'] ? $member['name'] : '<a href="' . $member['href'] . '" title="' . sprintf($txt['view_profile_of_username'], $member['name']) . '"' . (empty($member['color']) ? '' : ' style="color: ' . $member['color'] . '"') . '>' . $member['name'] . '</a>', '
+									', $member['is_guest'] ? $member['name'] : '<a href="' . $member['href'] . '" title="' . sprintf($txt['view_profile_of_username'], $member['name']) . '"' . (empty($member['color']) ? '' : ' style="color: ' . $member['color'] . ';"') . '>' . $member['name'] . '</a>', '
 								</span>';
 
 		if (!empty($member['ip']))
