37704f6b86ded7f704ebadb0497903fbd84a478b
Jon Stovell <jonstovell@gmail.com>
2022-12-22 14:18:52 -0700

    Adds class aliases for backward compatibility
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

---
+++ Sources/Class-Graphics.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Backward compatibility file.
+ *
+ * Simple Machines Forum (SMF)
+ *
+ * @package SMF
+ * @author Simple Machines https://www.simplemachines.org
+ * @copyright 2023 Simple Machines and individual contributors
+ * @license https://www.simplemachines.org/about/smf/license.php BSD
+ *
+ * @version 3.0 Alpha 1
+ */
+
+if (!defined('SMF'))
+	die('No direct access...');
+
+class_alias('SMF\\Graphics\\Gif\\ColorTable', '\\gif_color_table');
+class_alias('SMF\\Graphics\\Gif\\File', '\\gif_file');
+class_alias('SMF\\Graphics\\Gif\\FileHeader', '\\gif_file_header');
+class_alias('SMF\\Graphics\\Gif\\Image', '\\gif_image');
+class_alias('SMF\\Graphics\\Gif\\ImageHeader', '\\gif_image_header');
+class_alias('SMF\\Graphics\\Gif\\LzwCompression', '\\gif_lzw_compression');
+
+?>
