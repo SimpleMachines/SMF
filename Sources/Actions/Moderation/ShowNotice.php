9aee7ff3ab99c684e934e6eea22dc56ef3fc1f9b
Jon Stovell <jonstovell@gmail.com>
2023-06-26 21:22:00 -0600

    Implements SMF\Actions\Moderation\ShowNotice
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

---
+++ Sources/Actions/Moderation/ShowNotice.php
@@ -0,0 +1,134 @@
+<?php
+
+/**
+ * Simple Machines Forum (SMF)
+ *
+ * @package SMF
+ * @author Simple Machines https://www.simplemachines.org
+ * @copyright 2023 Simple Machines and individual contributors
+ * @license https://www.simplemachines.org/about/smf/license.php BSD
+ *
+ * @version 3.0 Alpha 1
+ */
+
+namespace SMF\Actions\Moderation;
+
+use SMF\BackwardCompatibility;
+use SMF\Actions\ActionInterface;
+
+use SMF\BBCodeParser;
+use SMF\Lang;
+use SMF\Theme;
+use SMF\Utils;
+use SMF\Db\DatabaseApi as Db;
+
+/**
+ * Shows a notice sent to a user.
+ */
+class ShowNotice implements ActionInterface
+{
+	use BackwardCompatibility;
+
+	/**
+	 * @var array
+	 *
+	 * BackwardCompatibility settings for this class.
+	 */
+	private static $backcompat = array(
+		'func_names' => array(
+			'load' => false,
+			'call' => 'ShowNotice',
+		),
+	);
+
+	/****************************
+	 * Internal static properties
+	 ****************************/
+
+	/**
+	 * @var object
+	 *
+	 * An instance of this class.
+	 * This is used by the load() method to prevent mulitple instantiations.
+	 */
+	protected static object $obj;
+
+	/****************
+	 * Public methods
+	 ****************/
+
+	/**
+	 * Does the job.
+	 */
+	public function execute(): void
+	{
+		$id_notice = (int) $_GET['nid'];
+
+		$request = Db::$db->query('', '
+			SELECT body, subject
+			FROM {db_prefix}log_member_notices
+			WHERE id_notice = {int:id_notice}',
+			array(
+				'id_notice' => $id_notice,
+			)
+		);
+		if (Db::$db->num_rows($request) == 0)
+		{
+			fatal_lang_error('no_access', false);
+		}
+		list(Utils::$context['notice_body'], Utils::$context['notice_subject']) = Db::$db->fetch_row($request);
+		Db::$db->free_result($request);
+
+		Utils::$context['notice_body'] = BBCodeParser::load()->parse(Utils::$context['notice_body'], false);
+	}
+
+	/***********************
+	 * Public static methods
+	 ***********************/
+
+	/**
+	 * Static wrapper for constructor.
+	 *
+	 * @return object An instance of this class.
+	 */
+	public static function load(): object
+	{
+		if (!isset(self::$obj))
+			self::$obj = new self();
+
+		return self::$obj;
+	}
+
+	/**
+	 * Convenience method to load() and execute() an instance of this class.
+	 */
+	public static function call(): void
+	{
+		self::load()->execute();
+	}
+
+	/******************
+	 * Internal methods
+	 ******************/
+
+	/**
+	 * Constructor. Protected to force instantiation via self::load().
+	 */
+	protected function __construct()
+	{
+		// Before we get too excited, is the current user allowed to see this?
+		isAllowedTo(array('issue_warning', 'view_warning_any'));
+
+		Utils::$context['page_title'] = Lang::$txt['show_notice'];
+		Utils::$context['sub_template'] = 'show_notice';
+		Utils::$context['template_layers'] = array();
+
+		Theme::loadTemplate('ModerationCenter');
+	}
+}
+
+// Export public static functions and properties to global namespace for backward compatibility.
+if (is_callable(__NAMESPACE__ . '\ShowNotice::exportStatic'))
+	ShowNotice::exportStatic();
+
+?>
