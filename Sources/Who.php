6b9141fbb05dce4f3301c53c4fab3f45dbc10134
Bugo <cheetah@bk.ru>
2023-11-06 22:59:17 +0500

    Update changes
    Signed-off-by: Bugo <cheetah@bk.ru>

--- Sources/Who.php
+++ Sources/Who.php
@@ -853,7 +853,7 @@ function Credits($in_admin = false)
 			'<a href="https://github.com/enyo/dropzone">Dropzone.js</a> | © Matias Meno | Licensed under <a href="http://en.wikipedia.org/wiki/MIT_License">The MIT License (MIT)</a>',
 			'<a href="https://github.com/matthiasmullie/minify">Minify</a> | © Matthias Mullie | Licensed under <a href="http://en.wikipedia.org/wiki/MIT_License">The MIT License (MIT)</a>',
 			'<a href="https://github.com/true/php-punycode">PHP-Punycode</a> | © True B.V. | Licensed under <a href="http://en.wikipedia.org/wiki/MIT_License">The MIT License (MIT)</a>',
-			'<a href="https://jscolor.com/">jscolor Color Picker</a> | © Jan Odvárko – East Desire | Licensed under <a href="https://jscolor.com/download/#open-source-license">GNU GPL v3</a>',
+			'<a href="https://github.com/mdbassit/Coloris">Coloris</a> | © Mohammed Bassit | Licensed under <a href="https://github.com/mdbassit/Coloris/blob/main/LICENSE">The MIT License (MIT)</a>',
 		),
 		'fonts' => array(
 			'<a href="https://fontlibrary.org/en/font/anonymous-pro"> Anonymous Pro</a> | © 2009 | This font is licensed under the SIL Open Font License, Version 1.1',
