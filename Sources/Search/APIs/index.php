842abd84f23bd88c32ceb86bbc2be79fb09f1ef9
jdarwood007 <unmonitored+github@sleepycode.com>
2023-11-14 18:31:37 -0800

    Standardize our Index files

--- Sources/Search/APIs/index.php
+++ Sources/Search/APIs/index.php
@@ -1,8 +1,8 @@
 <?php
 
 // Try to handle it with the upper level index.php. (it should know what to do.)
-if (file_exists(dirname(dirname(__FILE__)) . '/index.php'))
-	include (dirname(dirname(__FILE__)) . '/index.php');
+if (file_exists(dirname(__DIR__) . '/index.php'))
+	include (dirname(__DIR__) . '/index.php');
 else
 	exit;
 
