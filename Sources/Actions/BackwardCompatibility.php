d7e4f5ea0d1768eddb81b2f08f76374a85ce4e10
Joey Smith <jsmith@webinertia.net>
2024-01-02 08:00:23 -0600

    License block... Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- Sources/Actions/BackwardCompatibility.php
+++ Sources/Actions/BackwardCompatibility.php
@@ -2,6 +2,17 @@
 
 declare(strict_types=1);
 
+/**
+ * Simple Machines Forum (SMF)
+ *
+ * @package SMF
+ * @author Simple Machines https://www.simplemachines.org
+ * @copyright 2023 Simple Machines and individual contributors
+ * @license https://www.simplemachines.org/about/smf/license.php BSD
+ *
+ * @version 3.0 Alpha 1
+ */
+
 namespace SMF\Actions;
 
 use SMF\BackwardCompatibility as BackCompat;
