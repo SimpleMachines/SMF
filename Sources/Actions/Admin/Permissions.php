30a75c7971b2139375946b01a246e19541afe749
Jon Stovell <jonstovell@gmail.com>
2024-01-23 22:47:47 -0700

    Supplies expected keys in 2nd arg of integrate_load_permissions hook
    Fixes SimpleMachines/SMF#8007
    
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

--- Sources/Actions/Admin/Permissions.php
+++ Sources/Actions/Admin/Permissions.php
@@ -3453,6 +3453,7 @@ class Permissions implements ActionInterface
 		}
 
 		// Provide a practical way to modify permissions.
+		$permissions_by_scope['membergroup'] = &$permissions_by_scope['global'];
 		IntegrationHook::call('integrate_load_permissions', [&self::$permission_groups, &$permissions_by_scope, &self::$left_permission_groups, &$hidden_permissions, &$relabel_permissions]);
 
 		// If the hook made changes, sync them back to our master list.
