022932444a02ef1dfc5cd9258e7d55dbe8c3110e
John Rayes <live627@gmail.com>
2024-02-20 03:08:16 -0700

    Adding font awesome for real this time
    Co-authored-by: Diego Andrés <diegoandres_cortes@outlook.com>
    
    Signed-off-by: John Rayes <live627@gmail.com>

--- Themes/default/index.template.php
+++ Themes/default/index.template.php
@@ -90,7 +90,6 @@ function template_init()
  */
 function template_html_above()
 {
-	Theme::loadCSSFile('https://use.fontawesome.com/releases/v6.1.2/css/all.css', array('external' => true));
 	// Show right to left, the language code, and the character set for ease of translating.
 	echo '<!DOCTYPE html>
 <html', Utils::$context['right_to_left'] ? ' dir="rtl"' : '', !empty(Lang::getTxt('lang_locale', file: 'General')) ? ' lang="' . str_replace("_", "-", substr(Lang::getTxt('lang_locale', file: 'General'), 0, strcspn(Lang::getTxt('lang_locale', file: 'General'), "."))) . '"' : '', '>
