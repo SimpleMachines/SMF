bf0bbcc8823bf104b1179b2d92c7f05f70ca5ae6
Diego Andrés <diegoandres_cortes@outlook.com>
2024-03-10 18:20:11 -0600

    use correct method for time
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

--- Themes/default/Profile.template.php
+++ Themes/default/Profile.template.php
@@ -10,11 +10,11 @@
  * @version 3.0 Alpha 3
  */
 
-use SMF\BrowserDetector;
 use SMF\Config;
 use SMF\Lang;
 use SMF\Security;
 use SMF\Theme;
+use SMF\Time;
 use SMF\Utils;
 use SMF\User;
 
@@ -59,7 +59,7 @@ function template_profile_popup()
 					<a href="', Config::$scripturl, '?action=profile;u=', User::$me->id, '">', User::$me->name, '</a>
 				</span>
 				<span class="profile_group">', Utils::$context['member']['group'], '</span>
-				<time datetime="', smf_gmstrftime('%FT%TZ'), '">', Utils::$context['current_time'], '</time>
+				<time datetime="', Time::gmstrftime('%FT%TZ'), '">', Utils::$context['current_time'], '</time>
 			</div>
 		</div>
 		<div class="body">
