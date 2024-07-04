f5c1854ced8670daccfe36c099f333f02966865a
Michael Eshom <oldiesmann@gmail.com>
2024-07-04 14:29:18 -0400

    Add a key to the insert because the code has a meltdown otherwise...

--- Sources/Actions/Admin/Reactions.php
+++ Sources/Actions/Admin/Reactions.php
@@ -215,7 +215,7 @@ class Reactions implements ActionInterface
 						$do_update = true;
 
 						// Insert the new reactions
-						Db::$db->insert('insert', '{db_pref}reactions', ['name'], $add, []);
+						Db::$db->insert('', '{db_pref}reactions', ['name'], $add, ['id_reaction']);
 					}
 				}
 
