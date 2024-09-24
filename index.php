50d23645aea91754117633ab472396f393a6eed6
Michel Mendiola <suki@missallsunday.com>
2024-09-24 10:38:36 -0600

    feat: move autoload functionality to its own file

--- index.php
+++ index.php
@@ -137,32 +137,8 @@ if (SMF === 1) {
 /*
  * 3. Load some other essential includes.
  */
-$loader = require SMF\Config::$vendordir . '/autoload.php';
-$third_party_mappers = [];
 
-// Ensure $sourcedir is set to something valid.
-if (class_exists(Config::class, false) && isset(Config::$sourcedir)) {
-	$sourcedir = Config::$sourcedir;
-}
-
-if (empty($sourcedir) || !is_dir($sourcedir)) {
-	$sourcedir = __DIR__;
-}
-
-// Do any third-party scripts want in on the fun?
-if (!defined('SMF_INSTALLING') && class_exists(Config::class, false)) {
-	if (!class_exists(IntegrationHook::class, false) && is_file($sourcedir . '/IntegrationHook.php')) {
-		require_once $sourcedir . '/IntegrationHook.php';
-	}
-
-	if (class_exists(IntegrationHook::class, false)) {
-		IntegrationHook::call('integrate_autoload', [&$third_party_mappers]);
-	}
-}
-
-foreach ($third_party_mappers as $prefix => $dirname) {
-	$loader->addPsr4($prefix, $dirname);
-}
+require_once SMF\Config::$sourcedir . '/Autoloader.php';
 
 // Ensure we don't trip over disabled internal functions
 require_once SMF\Config::$sourcedir . '/Subs-Compat.php';
