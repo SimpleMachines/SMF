2be5143f143f1e99f1276508a6073097c4b5f83f
Diego Andrés <diegoandres_cortes@outlook.com>
2024-10-11 20:15:27 -0600

    fix buttonlist dropmenu items
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

--- Themes/default/scripts/script.js
+++ Themes/default/scripts/script.js
@@ -1702,8 +1702,10 @@ document.addEventListener('DOMContentLoaded', () => {
 			item.classList.add('visible');
 		});
 
-		document.addEventListener('click', () => {
-			item.classList.remove('visible');
+		document.addEventListener('click', e => {
+			if (!item.contains(e.target)) {
+				item.classList.remove('visible');
+			}
 		});
 	}
 
