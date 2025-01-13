8a3de8f38fbe4ff2e4d5be72559891a16a44bbad
Shawn Bulen <bulens@pacbell.net>
2025-01-13 12:19:26 -0800

    Don't write sessions when cookies are not present Signed by Shawn Bulen, bulens@pacbell.net

--- Sources/Session.php
+++ Sources/Session.php
@@ -97,7 +97,7 @@ class Session implements \SessionHandlerInterface
 	public function write(string $session_id, string $data): bool
 	{
 
-		// Don't bother writing the session if cookies are diabled
+		// Don't bother writing the session if cookies are disabled
 		if (empty($_COOKIE))
 			return true;
 
