481c56b2b94b8e1fb32d951c4761f7058e68539c
Jon Stovell <jonstovell@gmail.com>
2025-02-06 20:09:39 -0700

    Don't skip character 0xFF in source charset when converting to UTF-8
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

--- other/upgrade.php
+++ other/upgrade.php
@@ -3803,7 +3803,7 @@ function ConvertUtf8(): bool
 							$replace = 'REPLACE(' . $replace . ', ' . $from . ', ' . $to . ')';
 						}
 					} else {
-						for ($i = 0; $i < 0xFF; $i++) {
+						for ($i = 0; $i <= 0xFF; $i++) {
 							$from = '0x' . strtoupper(dechex($i));
 							$to = '0x' . strtoupper(bin2hex(mb_convert_encoding(chr($i), 'UTF-8', $from_charset)));
 
