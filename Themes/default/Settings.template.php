150d2ecd7f7b0d7d38a86cc266ee6de89e7026bb
Jon Stovell <jonstovell@gmail.com>
2025-02-19 16:49:19 -0700

    Hide calendar heading in theme options when calendar is disabled
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

--- Themes/default/Settings.template.php
+++ Themes/default/Settings.template.php
@@ -134,7 +134,7 @@ function template_options()
 			'label' => Lang::$txt['pm_remove_inbox_label'],
 			'default' => true,
 		),
-		Lang::$txt['theme_opt_calendar'],
+		!empty(Config::$modSettings['cal_enabled']) ? Lang::$txt['theme_opt_calendar'] : '',
 		array(
 			'id' => 'calendar_default_view',
 			'label' => Lang::$txt['calendar_default_view'],
