201b1d5af1609aa233cf318b412ec00557d71d44
Jon Stovell <jonstovell@gmail.com>
2025-02-28 21:41:38 -0700

    Fixes bootstrapping issue when calling Forum::getCurrentAction()
    It is called during Theme::load(), before Forum::$current_action has been set.
    
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

--- Sources/Forum.php
+++ Sources/Forum.php
@@ -447,22 +447,18 @@ class Forum
 	{
 		$this->init();
 
-		$requested_action = $_REQUEST['action'] ?? null;
-		$current_action = $this->findAction($requested_action);
+		self::getCurrentAction();
 
-		if (is_a($current_action, ActionInterface::class, true)) {
-			$action = call_user_func([$current_action, 'load']);
-			self::$current_action = $action;
-
-			// Perform operations on the action object before execute() is called.
-			IntegrationHook::call('integrate_init_action', [$action]);
+		// Perform operations on the action object before its execute() is called.
+		if (isset(self::$current_action)) {
+			IntegrationHook::call('integrate_init_action', [self::$current_action]);
 		}
 
 		$this->preflight();
 
-		if (isset($action)) {
-			$action->execute();
-		} elseif (is_callable($current_action)) {
+		if (isset(self::$current_action)) {
+			self::$current_action->execute();
+		} elseif (is_callable($current_action = self::findAction($_REQUEST['action'] ?? null))) {
 			call_user_func($current_action);
 		} else {
 			ErrorHandler::fatalLang('not_found', false, [], 404);
@@ -511,6 +507,14 @@ class Forum
 	 */
 	public static function getCurrentAction(): ?ActionInterface
 	{
+		if (!isset(self::$current_action)) {
+			$current_action = self::findAction($_REQUEST['action'] ?? null);
+
+			if (is_a($current_action, ActionInterface::class, true)) {
+				self::$current_action = call_user_func([$current_action, 'load']);
+			}
+		}
+
 		return self::$current_action;
 	}
 
@@ -684,6 +688,10 @@ class Forum
 		}
 	}
 
+	/*************************
+	 * Internal static methods
+	 *************************/
+
 	/**
 	 * Resolves the appropriate action to execute based on the current request context.
 	 *
@@ -691,7 +699,7 @@ class Forum
 	 *  - A string representing a class implementing ActionInterface.
 	 *  - A callable string representing a static method (e.g., `'Class::method'`).
 	 */
-	protected function findAction(?string $action): string|callable|false
+	protected static function findAction(?string $action): string|callable|false
 	{
 		// If no action was supplied, is there an implied action?
 		if (empty($action)) {
