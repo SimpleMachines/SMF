44ca77d7680a268eee7b5adbca3d2cdd0a78fdae
Jon Stovell <jonstovell@gmail.com>
2025-03-31 19:45:10 -0600

    Fixes a misnamed function
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

--- Themes/default/Post.template.php
+++ Themes/default/Post.template.php
@@ -282,7 +282,7 @@ function template_main()
 
 				if (!empty(Config::$modSettings['attachmentShowImages']))
 				{
-					if (str_begins_with($attachment['mime_type'], 'image'))
+					if (str_starts_with($attachment['mime_type'], 'image'))
 						$src = Config::$scripturl . '?action=dlattach;attach=' . (!empty($attachment['thumb']) ? $attachment['thumb'] : $attachment['attachID']) . ';preview;image';
 					else
 						$src = Theme::$current->settings['images_url'] . '/generic_attach.png';
