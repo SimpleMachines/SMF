2bfe6400dbceb3eae52b80fa6e5d5e3888414afd
Jon Stovell <jonstovell@gmail.com>
2025-04-19 19:04:02 -0600

    Fixes another type error in Msg::modify()
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

--- Sources/Msg.php
+++ Sources/Msg.php
@@ -1854,7 +1854,7 @@ class Msg implements \ArrayAccess, Routable
 
 		// Finally, if we are setting the approved state we need to do much more work :(
 		if (Config::$modSettings['postmod_active'] && isset($msgOptions['approved'])) {
-			Msg::approve($msgOptions['id'], $msgOptions['approved']);
+			Msg::approve($msgOptions['id'], !empty($msgOptions['approved']));
 		}
 
 		return true;
