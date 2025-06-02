1367f09a76726b0d6e6a9fa53ac3f0c896a1db3f
Jon Stovell <jonstovell@gmail.com>
2025-06-02 16:27:55 -0600

    Adds missing SMF\Db\Schema\v2_1\Settings class
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

---
+++ Sources/Db/Schema/v2_1/Settings.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Simple Machines Forum (SMF)
+ *
+ * @package SMF
+ * @author Simple Machines https://www.simplemachines.org
+ * @copyright 2023 Simple Machines and individual contributors
+ * @license https://www.simplemachines.org/about/smf/license.php BSD
+ *
+ * @version 3.0 Alpha 3
+ */
+
+declare(strict_types=1);
+
+namespace SMF\Db\Schema\v2_1;
+
+use SMF\Db\Schema\Column;
+use SMF\Db\Schema\DbIndex;
+use SMF\Db\Schema\Table;
+
+/**
+ * Defines all the properties for a database table.
+ */
+class Settings extends Table
+{
+	/****************
+	 * Public methods
+	 ****************/
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct()
+	{
+		$this->name = 'settings';
+
+		$this->columns = [
+			'variable' => new Column(
+				name: 'variable',
+				type: 'varchar',
+				size: 255,
+				default: '',
+			),
+			'value' => new Column(
+				name: 'value',
+				type: 'text',
+				not_null: true,
+			),
+		];
+
+		$this->indexes = [
+			'primary' => new DbIndex(
+				type: 'primary',
+				columns: [
+					'variable(30)',
+				],
+			),
+		];
+
+		parent::__construct();
+	}
+}
