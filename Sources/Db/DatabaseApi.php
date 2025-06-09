c2060495440e19434d4c200f1799739493759912
jdarwood007 <unmonitored+github@sleepycode.com>
2025-06-08 18:23:36 -0700

    CS fixer

--- Sources/Db/DatabaseApi.php
+++ Sources/Db/DatabaseApi.php
@@ -631,8 +631,8 @@ abstract class DatabaseApi
 		}
 
 		// Parameters reorded.
-		Utils::$smcFunc['db_query'] = fn(string $identifier = null, string $db_string, array $db_values = [], ?object $connection = null) => $this->db_query($db_string, $db_values, $connection, $identifier);
-		Utils::$smcFunc['db_search_query'] = fn(string $identifier = null, string $db_string, array $db_values = [], ?object $connection = null) => $this->db_search_query($db_string, $db_values, $connection, $identifier);
+		Utils::$smcFunc['db_query'] = fn(?string $identifier = null, string $db_string, array $db_values = [], ?object $connection = null) => $this->db_query($db_string, $db_values, $connection, $identifier);
+		Utils::$smcFunc['db_search_query'] = fn(?string $identifier = null, string $db_string, array $db_values = [], ?object $connection = null) => $this->db_search_query($db_string, $db_values, $connection, $identifier);
 
 	}
 }
