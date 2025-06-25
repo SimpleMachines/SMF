23bb03557441269ced465b3be56f79c82694e295
Jon Stovell <jonstovell@gmail.com>
2025-06-25 17:57:37 -0600

    Updates version to 2.1.6
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

--- other/install.php
+++ other/install.php
@@ -8,10 +8,10 @@
  * @copyright 2025 Simple Machines and individual contributors
  * @license https://www.simplemachines.org/about/smf/license.php BSD
  *
- * @version 2.1.5
+ * @version 2.1.6
  */
 
-define('SMF_VERSION', '2.1.5');
+define('SMF_VERSION', '2.1.6');
 define('SMF_FULL_VERSION', 'SMF ' . SMF_VERSION);
 define('SMF_SOFTWARE_YEAR', '2025');
 define('DB_SCRIPT_VERSION', '2-1');
