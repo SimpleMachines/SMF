22139ec3768541613c50e6106e5d705d8f82d4c4
jdarwood007 <unmonitored+github@sleepycode.com>
2025-07-05 08:35:51 -0700

    Meant for another PR

--- Sources/Config.php
+++ Sources/Config.php
@@ -2772,7 +2772,7 @@ class Config
 		}
 
 		if (!isset($db_last_error)) {
-			self::updateDbLastError(0);
+			self::updateDbLastError(0, true);
 		} else {
 			self::$db_last_error = (int) $db_last_error;
 		}
