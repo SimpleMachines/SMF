9e2b42ab0f4de655b51a0204c86a5ce307f69741
Shawn Bulen <bulens@pacbell.net>
2025-07-13 20:49:16 -0700

    Cant view likes without cookies
    Signed-off-by: Shawn Bulen <bulens@pacbell.net>

--- Themes/default/Display.template.php
+++ Themes/default/Display.template.php
@@ -813,9 +813,19 @@ function template_single_post($message)
 				$count--;
 			}
 
+
+			// Remove link if no cookies; session reference won't work
+			if (empty($_COOKIE)) {
+				$like_text = Lang::getTxt($base, ['url' => '', 'num' => $count], file: 'General');
+				$like_text = strtr($like_text, ['<a href="">' => '', '</a>' => '']);
+			}
+			else {
+				$like_text = Lang::getTxt($base, ['url' => Config::$scripturl . '?action=likes;sa=view;ltype=msg;like=' . $message['id'] . ';' . Utils::$context['session_var'] . '=' . Utils::$context['session_id'], 'num' => $count], file: 'General');
+			}
+
 			echo '
 									<li class="like_count smalltext">
-										', Lang::getTxt($base, ['url' => Config::$scripturl . '?action=likes;sa=view;ltype=msg;like=' . $message['id'] . ';' . Utils::$context['session_var'] . '=' . Utils::$context['session_id'], 'num' => $count], file: 'General'), '
+										', $like_text, '
 									</li>';
 		}
 
