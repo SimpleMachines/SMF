01031dc7678972a3466b4e9ab9eb0949d029c808
jdarwood007 <unmonitored+github@sleepycode.com>
2025-08-08 11:05:49 -0700

    [3.0] Fatal error due to class not matching interface
    Also ensure we return as the return type specifies.

--- Sources/Db/APIs/MySQL.php
+++ Sources/Db/APIs/MySQL.php
@@ -1932,7 +1932,7 @@ class MySQL extends DatabaseApi implements DatabaseApiInterface
 	/**
 	 *
 	 */
-	public function rename_table(string $old_name, string $new_name, bool $allowed_reserved = false): bool
+	public function rename_table(string $old_name, string $new_name, bool $allowed_reserved = false, string $error = 'fatal'): bool
 	{
 		// After stripping away the database name, this is what's left.
 		$real_prefix = preg_match('~^(`?)(.+?)\\1\\.(.*?)$~', $this->prefix, $match) === 1 ? $match[3] : $this->prefix;
@@ -1972,6 +1972,8 @@ class MySQL extends DatabaseApi implements DatabaseApiInterface
 				'security_override' => true,
 			],
 		);
+
+		return true;
 	}
 
 	/**
