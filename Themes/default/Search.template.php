588d99d63a06cee37420fb9651117574997b06f2
John Rayes <live627@gmail.com>
2025-08-26 03:07:48 -0700

    Remove string evaluations

--- Themes/default/Search.template.php
+++ Themes/default/Search.template.php
@@ -123,7 +123,6 @@ function template_main()
 				</dd>
 			</dl>
 			<script>
-				createEventListener(window);
 				window.addEventListener("load", initSearch, false);
 			</script>
 			<input type="hidden" name="advanced" value="1">';
@@ -251,12 +250,10 @@ function template_main()
 	</form>
 	<script>
 		var oAddMemberSuggest = new smc_AutoSuggest({
-			sSelf: \'oAddMemberSuggest\',
 			sSessionId: smf_session_id,
 			sSessionVar: smf_session_var,
 			sControlId: \'userspec\',
-			sSearchType: \'member\',
-			bItemList: false
+			sSearchType: \'member\'
 		});
 	</script>';
 }
