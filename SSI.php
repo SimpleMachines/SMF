113e6a4a510a2857accd69c5b7c0b64e77eb09bb
Jon Stovell <jonstovell@gmail.com>
2025-08-31 22:51:59 -0600

    Avoid polluting the global namespace
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

--- SSI.php
+++ SSI.php
@@ -38,5 +38,4 @@ if (!defined('SMF')) {
 // Initialize.
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'index.php';
 
-$ssi = new SMF\ServerSideIncludes();
-$ssi->execute();
+(new SMF\ServerSideIncludes())->execute();
