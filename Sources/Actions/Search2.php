d591cd63f561a2462e0e3acaf5427a4893568c09
John Rayes <live627@gmail.com>
2025-09-13 14:24:04 -0700

    Move both IconList and JumpTo to topic.js

--- Sources/Actions/Search2.php
+++ Sources/Actions/Search2.php
@@ -432,6 +432,8 @@ class Search2 implements ActionInterface, Routable
 			}
 		}
 
+		Theme::loadJavaScriptFile('topic.js', ['defer' => false, 'minimize' => true], 'smf_topic');
+
 		SearchApi::$loadedApi->resultsContext();
 	}
 
