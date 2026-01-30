a8ba9915fa2166edc9c75f2848f8d4a8e348e8e6
Michel Mendiola <suki@missallsunday.com>
2026-01-30 09:48:49 -0600

    feat: Add League/Container 4.0 to the list of dependencies and have a way to initialize the container

---
+++ Sources/Container.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Simple Machines Forum (SMF)
+ *
+ * @package SMF
+ * @author Simple Machines https://www.simplemachines.org
+ * @copyright 2026 Simple Machines and individual contributors
+ * @license https://www.simplemachines.org/about/smf/license.php BSD
+ *
+ * @version 3.0 Alpha 4
+ */
+
+declare(strict_types=1);
+
+namespace SMF;
+
+use League\Container\Container as LeagueContainer;
+
+/**
+ * A wrapper for the League\Container dependency injection container.
+ */
+class Container
+{
+	/**
+	 * @var LeagueContainer
+	 */
+	private static LeagueContainer $instance;
+
+	/**
+	 * Initialize the container.
+	 */
+	public static function init(): void
+	{
+		if (!isset(self::$instance)) {
+			self::$instance = new LeagueContainer();
+		}
+	}
+
+	/**
+	 * Get the container instance.
+	 *
+	 * @return LeagueContainer
+	 */
+	public static function getInstance(): LeagueContainer
+	{
+		if (!isset(self::$instance)) {
+			self::init();
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Proxy static calls to the container instance.
+	 *
+	 * @param string $name
+	 * @param array $arguments
+	 * @return mixed
+	 */
+	public static function __callStatic(string $name, array $arguments)
+	{
+		return self::getInstance()->$name(...$arguments);
+	}
+}
