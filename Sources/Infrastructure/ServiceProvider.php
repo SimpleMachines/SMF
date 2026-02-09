6f135303ea223864035f72ac39072886766e0737
Michel Mendiola <suki@missallsunday.com>
2026-02-09 09:49:26 -0600

    feat: first attempt at creating a serviceProvider and serviceList files for services

---
+++ Sources/Infrastructure/ServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+namespace SMF\Infrastructure;
+
+use League\Container\ServiceProvider\AbstractServiceProvider;
+
+class ServiceProvider extends AbstractServiceProvider
+{
+	private array $services;
+
+	public function __construct(array $services = [])
+	{
+		$coreServices = require 'ServicesList.php';
+		$this->services = array_filter(array_merge($coreServices, $services));
+	}
+	public function provides(string $id): bool
+	{
+		return array_key_exists($id, $this->services);
+	}
+
+	public function register(): void
+	{
+		$container = $this->getContainer();
+		foreach ($this->services as $id => $config) {
+			$method = ($config['shared'] ?? false) ? 'addShared' : 'add';
+
+			$container->$method($id)
+				->addArguments($config['arguments'] ?? []);
+		}
+	}
+}
---
+++ Sources/Infrastructure/ServicesList.php
@@ -0,0 +1,11 @@
+<?php
+
+
+// List of all services registered for SMF, example:
+//'db' => [
+//	'arguments' => [$db_server, $db_user],
+//	'shared' => true  // false will create a new instance everytime
+//],
+return [
+
+];
