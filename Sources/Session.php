0d545787bd97cb541bb9f37bb3f2a932a13da88b
Shawn Bulen <bulens@pacbell.net>
2026-02-23 12:35:48 -0800

    Minimize unnecessary session writes
    Signed-off-by: Shawn Bulen <bulens@pacbell.net>

--- Sources/Session.php
+++ Sources/Session.php
@@ -172,10 +172,16 @@ class SmfSessionHandler extends SessionHandler implements SessionHandlerInterfac
 	#[\ReturnTypeWillChange]
 	public function write(/*PHP 8.0 string*/$id,/*PHP 8.0 string */ $data): bool
 	{
-		global $smcFunc;
+		global $smcFunc, $scripturl;
 
 		// Don't bother writing the session if cookies are disabled; no way to retrieve it later
 		if (empty($_COOKIE))
+			return true;
+
+		// Don't bother writing the session for users just browsing
+		// Any action that is not dependent on data within the session may be added to this array
+		static $no_writes = array('dlattach');
+		if ((empty($_REQUEST['action']) || in_array($_REQUEST['action'], $no_writes, true)) && !empty($scripturl))
 			return true;
 
 		if (!$this->isValidSessionID($id))
