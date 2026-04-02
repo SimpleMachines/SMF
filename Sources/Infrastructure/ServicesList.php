dc326202a37e174840c4bee82fc8bbe43bbcdee3
Michel Mendiola <suki@missallsunday.com>
2026-04-02 09:33:53 -0600

    chore: Split settings and modSettings to be independent of Config.php

--- Sources/Infrastructure/ServicesList.php
+++ Sources/Infrastructure/ServicesList.php
@@ -1,6 +1,8 @@
 <?php
 
 use SMF\Services\ErrorHandlerService;
+use SMF\Services\ModSettingsService;
+use SMF\Services\SettingsService;
 
 // List of all services registered for SMF, example:
 //'db' => [
@@ -8,6 +10,15 @@ use SMF\Services\ErrorHandlerService;
 //	'shared' => true  // false will create a new instance everytime
 //],
 return [
+	// Settings.php configuration service
+	SettingsService::class => [
+		'shared' => true,
+	],
+	// Database settings service
+	ModSettingsService::class => [
+		'shared' => true,
+	],
+	// Error handler service
 	ErrorHandlerService::class => [
 		'shared' => true,
 	],
