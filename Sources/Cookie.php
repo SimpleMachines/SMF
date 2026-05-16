bfb03cc97f26b04f1d5f68fb81b8c61549c812ba
jdarwood007 <unmonitored+github@sleepycode.com>
2026-05-16 16:30:25 -0700

    Try to delete the old session

--- Sources/Cookie.php
+++ Sources/Cookie.php
@@ -434,12 +434,8 @@ class Cookie
 
 			// Recreate and restore the new session.
 			Session::load();
-
-			// @todo should we use session_regenerate_id(true); now that we are 5.1+
-			session_regenerate_id();
-
+			session_regenerate_id(true);
 			$_SESSION = $oldSessionData;
-
 			$_SESSION['login_' . Config::$cookiename] = $_COOKIE[Config::$cookiename];
 		}
 	}
