79c8f64bc1d66ae28ac39142f66a4b2f7f81929e
jdarwood007 <unmonitored+github@sleepycode.com>
2026-05-16 16:56:02 -0700

    Use hash_equals on proxy validation

--- proxy.php
+++ proxy.php
@@ -144,7 +144,7 @@ class ProxyServer
 			return false;
 		}
 
-		if (hash_hmac('sha1', $request, $this->secret) != $hash)
+		if (!hash_equals(hash_hmac('sha1', $request, $this->secret), $hash))
 			return false;
 
 		// Ensure any non-ASCII characters in the URL are encoded correctly
