8cf625c874e8151e60cc534a00f1e6f8b62cb312
Jon Stovell <jonstovell@gmail.com>
2026-08-13 23:14:54 -0600

    Casts to correct types when calling Actions\TopicRestore::mergePosts()
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

--- Sources/Actions/TopicRestore.php
+++ Sources/Actions/TopicRestore.php
@@ -166,7 +166,7 @@ class TopicRestore implements ActionInterface, Routable
 
 				// Move the posts back then!
 				if (isset($previous_topics[$topic])) {
-					self::mergePosts(array_keys($data['msgs']), $data['current_topic'], $topic);
+					self::mergePosts(array_keys($data['msgs']), (int) $data['current_topic'], (int) $topic);
 					// Log em.
 					Logging::logAction('restore_posts', ['topic' => $topic, 'subject' => $previous_topics[$topic]['subject'], 'board' => empty($data['previous_board']) ? $data['possible_prev_board'] : $data['previous_board']]);
 					$messages = array_merge(array_keys($data['msgs']), $messages);
