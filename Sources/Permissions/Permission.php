ce43114724c6e2c6209b597f037b064cc89ba534
oldiesmann <oldiesmann@gmail.com>
2026-08-31 20:14:23 -0400

    Fix unknown permission and a few other things
    Signed-off-by: oldiesmann <oldiesmann@gmail.com>

--- Sources/Permissions/Permission.php
+++ Sources/Permissions/Permission.php
@@ -357,12 +357,6 @@ class Permission implements \ArrayAccess
 			'group_level' => self::GROUP_LEVEL_MODERATOR,
 			'never_guests' => true,
 		],
-		'likes_like' => [
-			'view_group' => 'likes',
-			'scope' => 'global',
-			'group_level' => self::GROUP_LEVEL_STANDARD,
-			'never_guests' => true,
-		],
 		'lock_own' => [
 			'generic_name' => 'lock',
 			'own_any' => 'own',
@@ -876,6 +870,12 @@ class Permission implements \ArrayAccess
 			'group_level' => self::GROUP_LEVEL_MAINTENANCE,
 			'never_guests' => true,
 		],
+		'reactions_react' => [
+			'view_group' => 'reactions',
+			'scope' => 'global',
+			'group_level' => self::GROUP_LEVEL_STANDARD,
+			'never_guests' => true,
+		],
 		'remove_own' => [
 			'generic_name' => 'remove',
 			'own_any' => 'own',
@@ -1290,9 +1290,9 @@ class Permission implements \ArrayAccess
 			self::$permissions['post_attachment']['hidden'] = true;
 		}
 
-		// If likes are disabled, disable the related permission.
-		if (empty(Config::$modSettings['enable_likes'])) {
-			self::$permissions['likes_like']['hidden'] = true;
+		// If reactions are disabled, disable the related permission.
+		if (empty(Config::$modSettings['enable_reacts'])) {
+			self::$permissions['reactions_react']['hidden'] = true;
 		}
 
 		// If mentions are disabled, disable the related permission.
