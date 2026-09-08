58f1a040f9b0af7199d6a38396a79e348dd84da0
Jon Stovell <jonstovell@gmail.com>
2026-09-08 10:07:05 -0600

    Resolves class correctly

--- Sources/Maintenance/Tools/Upgrade.php
+++ Sources/Maintenance/Tools/Upgrade.php
@@ -1226,7 +1226,7 @@ class Upgrade extends ToolsBase implements ToolsInterface
 			],
 			[
 				[
-					SMF\Tasks\FetchSMFiles::class,
+					\SMF\Tasks\FetchSMFiles::class,
 					'',
 					0,
 				],
