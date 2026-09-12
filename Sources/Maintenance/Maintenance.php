eea4da07e9daffaf5b732eb48425eb5e69492c07
Jon Stovell <jonstovell@gmail.com>
2026-09-12 00:51:46 -0600

    Fixes broken regex in Maintenance::checkManifest()
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

--- Sources/Maintenance/Maintenance.php
+++ Sources/Maintenance/Maintenance.php
@@ -462,7 +462,7 @@ class Maintenance
 					foreach (Tools\Upgrade::MIGRATIONS[$ns] as $class) {
 						if (!class_exists($class)) {
 							$missing_or_corrupt_files[] = preg_replace(
-								['/^SMF\b/', '/\\/'],
+								['/^SMF\b/', '/\\\\/'],
 								['Sources', DIRECTORY_SEPARATOR],
 								$class,
 							);
@@ -472,7 +472,7 @@ class Maintenance
 					foreach (Tools\Upgrade::CLEANUPS[$ns] as $class) {
 						if (!class_exists($class)) {
 							$missing_or_corrupt_files[] = preg_replace(
-								['/^SMF\b/', '/\\/'],
+								['/^SMF\b/', '/\\\\/'],
 								['Sources', DIRECTORY_SEPARATOR],
 								$class,
 							);
